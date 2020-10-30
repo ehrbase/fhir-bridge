619fc6c774bbef8b4d8f40273ad251a7f9751b89
Renaud Subiger <renaud@subiger.com>
2020-10-30 15:54:44 +0100

    Exclude ThymeleafAutoConfiguration

--- src/main/java/org/ehrbase/fhirbridge/FhirBridgeApplication.java
+++ src/main/java/org/ehrbase/fhirbridge/FhirBridgeApplication.java
@@ -3,9 +3,11 @@ package org.ehrbase.fhirbridge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-        RestClientAutoConfiguration.class
+        RestClientAutoConfiguration.class,
+        ThymeleafAutoConfiguration.class
 })
 public class FhirBridgeApplication {
 
