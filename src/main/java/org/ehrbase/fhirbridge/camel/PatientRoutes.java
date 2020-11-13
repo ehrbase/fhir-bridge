8327498cf84f59cabfdc0edb92c14acd78e84afe
Renaud Subiger <renaud@subiger.com>
2020-11-13 17:20:35 +0100

    Add integration tests

--- src/main/java/org/ehrbase/fhirbridge/camel/PatientRoutes.java
+++ src/main/java/org/ehrbase/fhirbridge/camel/PatientRoutes.java
@@ -1,15 +1,20 @@
 package org.ehrbase.fhirbridge.camel;
 
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import org.apache.camel.builder.RouteBuilder;
 import org.ehrbase.fhirbridge.camel.processor.DefaultCreateResourceRequestValidator;
+import org.hl7.fhir.r4.model.Patient;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PatientRoutes extends RouteBuilder {
 
+    private final IFhirResourceDao<Patient> patientDao;
+
     private final DefaultCreateResourceRequestValidator requestValidator;
 
-    public PatientRoutes(DefaultCreateResourceRequestValidator requestValidator) {
+    public PatientRoutes(IFhirResourceDao<Patient> patientDao, DefaultCreateResourceRequestValidator requestValidator) {
+        this.patientDao = patientDao;
         this.requestValidator = requestValidator;
     }
 
@@ -19,6 +24,7 @@ public class PatientRoutes extends RouteBuilder {
         from("pt-create:/service?audit=false&fhirContext=#fhirContext")
             .routeId("create-patient")
             .process(requestValidator)
+            .bean(patientDao, "create(${body})")
             .to("log:create-patient?showAll=true");
         // @formatter:on
     }
