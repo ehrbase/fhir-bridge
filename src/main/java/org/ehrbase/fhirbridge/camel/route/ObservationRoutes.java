965db18244228fe741ebd2b9f3d3c9fa9d9cbccd
Renaud Subiger <renaud@subiger.com>
2020-11-20 13:22:44 +0100

    Integrate CoronavirusNachweisTest profile

--- src/main/java/org/ehrbase/fhirbridge/camel/route/ObservationRoutes.java
+++ src/main/java/org/ehrbase/fhirbridge/camel/route/ObservationRoutes.java
@@ -1,14 +1,51 @@
 package org.ehrbase.fhirbridge.camel.route;
 
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import org.apache.camel.builder.RouteBuilder;
+import org.ehrbase.fhirbridge.FhirBridgeException;
+import org.ehrbase.fhirbridge.camel.DefaultCreateResourceRequestValidator;
+import org.ehrbase.fhirbridge.camel.FhirBridgeConstants;
+import org.ehrbase.fhirbridge.camel.PatientIdProcessor;
+import org.ehrbase.fhirbridge.camel.component.ehr.composition.CompositionConstants;
+import org.ehrbase.fhirbridge.ehr.converter.KennzeichnungErregernachweisSARSCoV2CompositionConverter;
+import org.ehrbase.fhirbridge.fhir.common.Profile;
+import org.hl7.fhir.r4.model.Observation;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ObservationRoutes extends RouteBuilder {
 
+    private final IFhirResourceDao<Observation> observationDao;
+
+    private final DefaultCreateResourceRequestValidator requestValidator;
+
+    private final PatientIdProcessor patientIdProcessor;
+
+    public ObservationRoutes(IFhirResourceDao<Observation> observationDao, DefaultCreateResourceRequestValidator requestValidator, PatientIdProcessor patientIdProcessor) {
+        this.observationDao = observationDao;
+        this.requestValidator = requestValidator;
+        this.patientIdProcessor = patientIdProcessor;
+    }
+
     @Override
     public void configure() {
         // @formatter:off
+        from("fhir-create-observation:/service?audit=false&fhirContext=#fhirContext")
+            .process(requestValidator)
+            .bean(observationDao, "create(${body})")
+            .setHeader(FhirBridgeConstants.METHOD_OUTCOME, body())
+            .setBody(simple("${body.resource}"))
+            .process(patientIdProcessor)
+            .choice()
+                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.CORONARIRUS_NACHWEIS_TEST))
+                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new KennzeichnungErregernachweisSARSCoV2CompositionConverter()))
+                .otherwise()
+                    .process(exchange -> {
+                        throw new FhirBridgeException("Unsupported profile / Not yet implemented");
+                    })
+            .end()
+            .to("ehr-composition:compositionProducer?operation=mergeCompositionEntity")
+            .setBody(header(FhirBridgeConstants.METHOD_OUTCOME));
         // @formatter:on
     }
 }
