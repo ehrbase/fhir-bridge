9ae96807f4f3cb54d09d93d3e120008cfc0a5565
Renaud Subiger <renaud@subiger.com>
2020-11-20 12:03:01 +0100

    Fix Checkstyle issues and add IT test for Condition

--- src/test/java/org/ehrbase/fhirbridge/fhir/ConditionIT.java
+++ src/test/java/org/ehrbase/fhirbridge/fhir/ConditionIT.java
@@ -3,7 +3,10 @@ package org.ehrbase.fhirbridge.fhir;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.gclient.ICreateTyped;
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 
@@ -26,5 +29,14 @@ class ConditionIT extends AbstractSetupIT {
 
         assertNotNull(outcome.getId());
         assertEquals(true, outcome.getCreated());
+    }
+
+    @Test
+    void createWithInvalidSubject() throws IOException {
+        String resource = IOUtils.toString(new ClassPathResource("Condition/create-with-invalid-subject.json").getInputStream(), StandardCharsets.UTF_8);
+        ICreateTyped createTyped = client.create().resource(resource);
+        Exception exception = Assertions.assertThrows(UnprocessableEntityException.class, createTyped::execute);
+
+        assertEquals("HTTP 422 : Subject identifier is required", exception.getMessage());
     }
 }
