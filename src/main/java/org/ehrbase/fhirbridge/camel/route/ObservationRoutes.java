af78c6632f350fb0163daf152a84ed11aa0c39b9
Renaud Subiger <renaud@subiger.com>
2020-11-24 14:21:55 +0100

    Remove resolution of CompositionConverter from the Camel route into CompositionConverterResolver

--- src/main/java/org/ehrbase/fhirbridge/camel/route/ObservationRoutes.java
+++ src/main/java/org/ehrbase/fhirbridge/camel/route/ObservationRoutes.java
@@ -2,26 +2,11 @@ package org.ehrbase.fhirbridge.camel.route;
 
 import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import org.apache.camel.builder.RouteBuilder;
-import org.ehrbase.fhirbridge.FhirBridgeException;
 import org.ehrbase.fhirbridge.camel.DefaultCreateResourceRequestValidator;
 import org.ehrbase.fhirbridge.camel.FhirBridgeConstants;
 import org.ehrbase.fhirbridge.camel.PatientIdProcessor;
 import org.ehrbase.fhirbridge.camel.component.ehr.composition.CompositionConstants;
-import org.ehrbase.fhirbridge.ehr.converter.BloodPressureCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.BodyHeightCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.BodyTemperatureCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.BodyWeightCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.ClinicalFrailtyScaleScoreCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.CoronavirusNachweisTestCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.FiO2CompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.HeartRateCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.ObservationLabCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.PatientInIcuCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.PregnancyStatusCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.RespiratoryRateCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.SmokingStatusCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.SofaScoreCompositionConverter;
-import org.ehrbase.fhirbridge.fhir.common.Profile;
+import org.ehrbase.fhirbridge.ehr.converter.CompositionConverterResolver;
 import org.hl7.fhir.r4.model.Observation;
 import org.springframework.stereotype.Component;
 
@@ -34,10 +19,15 @@ public class ObservationRoutes extends RouteBuilder {
 
     private final PatientIdProcessor patientIdProcessor;
 
-    public ObservationRoutes(IFhirResourceDao<Observation> observationDao, DefaultCreateResourceRequestValidator requestValidator, PatientIdProcessor patientIdProcessor) {
+    private final CompositionConverterResolver compositionConverterResolver;
+
+    public ObservationRoutes(IFhirResourceDao<Observation> observationDao,
+                             DefaultCreateResourceRequestValidator requestValidator,
+                             PatientIdProcessor patientIdProcessor, CompositionConverterResolver compositionConverterResolver) {
         this.observationDao = observationDao;
         this.requestValidator = requestValidator;
         this.patientIdProcessor = patientIdProcessor;
+        this.compositionConverterResolver = compositionConverterResolver;
     }
 
     @Override
@@ -49,40 +39,7 @@ public class ObservationRoutes extends RouteBuilder {
             .setHeader(FhirBridgeConstants.METHOD_OUTCOME, body())
             .setBody(simple("${body.resource}"))
             .process(patientIdProcessor)
-            .choice()
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.BLOOD_PRESSURE))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new BloodPressureCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.BODY_HEIGHT))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new BodyHeightCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.BODY_TEMP))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new BodyTemperatureCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.BODY_WEIGHT))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new BodyWeightCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.CLINICAL_FRAILTY_SCALE))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new ClinicalFrailtyScaleScoreCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.CORONARIRUS_NACHWEIS_TEST))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new CoronavirusNachweisTestCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.FIO2))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new FiO2CompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.HEART_RATE))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new HeartRateCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.OBSERVATION_LAB))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new ObservationLabCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.PATIENT_IN_ICU))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new PatientInIcuCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.PREGNANCY_STATUS))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new PregnancyStatusCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.RESPIRATORY_RATE))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new RespiratoryRateCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.SMOKING_STATUS))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new SmokingStatusCompositionConverter()))
-                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.SOFA_SCORE))
-                    .setHeader(CompositionConstants.COMPOSITION_CONVERTER, constant(new SofaScoreCompositionConverter()))
-                .otherwise()
-                    .process(exchange -> {
-                        throw new FhirBridgeException("Unsupported profile / Not yet implemented");
-                    })
-            .end()
+            .setHeader(CompositionConstants.COMPOSITION_CONVERTER, method(compositionConverterResolver, "resolve(${header.CamelFhirBridgeProfile})"))
             .to("ehr-composition:compositionProducer?operation=mergeCompositionEntity")
             .setBody(header(FhirBridgeConstants.METHOD_OUTCOME));
         // @formatter:on
