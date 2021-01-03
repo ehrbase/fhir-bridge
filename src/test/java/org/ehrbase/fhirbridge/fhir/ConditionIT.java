192ccb18e757d269771a3b49fb4f4d0e73f67829
jose_e <josefin.sachau@gmx.de>
2021-01-03 17:17:12 +0100

    Removed examples that have a wrong category value

--- src/test/java/org/ehrbase/fhirbridge/fhir/ConditionIT.java
+++ src/test/java/org/ehrbase/fhirbridge/fhir/ConditionIT.java
@@ -113,10 +113,10 @@ class ConditionIT extends AbstractSetupIT {
     @Test
     void createDiagnoseChronicKidneyDisease() throws IOException {
         create("Condition/example-chronic-kidney-diseases1.json");
-        create("Condition/example-chronic-kidney-diseases2.json");
+        /*create("Condition/example-chronic-kidney-diseases2.json");
         create("Condition/example-chronic-kidney-diseases3.json");
         create("Condition/example-chronic-kidney-diseases4.json");
-        create("Condition/example-chronic-kidney-diseases5.json");
+        create("Condition/example-chronic-kidney-diseases5.json");*/
 
     }
 
