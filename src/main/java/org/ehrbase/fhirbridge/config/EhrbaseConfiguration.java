e6d5cedbece577301ffa1a35d9f2e2b56c59cfe6
Dave <dave.petzold@appsfactory.de>
2021-01-12 15:28:59 +0100

    removed empty line

--- src/main/java/org/ehrbase/fhirbridge/config/EhrbaseConfiguration.java
+++ src/main/java/org/ehrbase/fhirbridge/config/EhrbaseConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-
 import java.net.URI;
 import java.net.URISyntaxException;
 
