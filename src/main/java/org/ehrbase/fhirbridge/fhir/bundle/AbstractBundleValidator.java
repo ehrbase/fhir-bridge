17bce7a760e39540c1ff378ee8bf2db7f98fb67e
SevKohler <kohlerseverin@posteo.de>
2021-02-04 16:24:40 +0100

    Bundle Validator added

--- src/main/java/org/ehrbase/fhirbridge/fhir/bundle/AbstractBundleValidator.java
+++ src/main/java/org/ehrbase/fhirbridge/fhir/bundle/AbstractBundleValidator.java
@@ -1,37 +1,159 @@
 package org.ehrbase.fhirbridge.fhir.bundle;
 
+import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
 import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Resource;
 import org.openehealth.ipf.commons.ihe.fhir.FhirTransactionValidator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 public abstract class AbstractBundleValidator implements FhirTransactionValidator {
 
+
+    class MemberValidator {
+        List<String> fullUrlList = new ArrayList<>();
+        List<String> hasMembersList = new ArrayList<>();
+
+        private void addFullUrls(String fullUrl) {
+            addListEntry(this.fullUrlList, fullUrl);
+        }
+
+        private void deleteFullUrl(String fullUrl) {
+            fullUrlList.remove(fullUrl);
+        }
+
+        void addListEntry(List<String> list, String entry){
+            if(entry.startsWith("urn:uuid:")){
+                list.add(entry);
+            }else{
+                throw new UnprocessableEntityException(entry + " is not a valid format. The fullUrl has to be an UUID and start with: urn:uuid:, example urn:uuid:04121321-4af5-424c-a0e1-ed3aab1c349d");
+            }
+        }
+
+        public void setHasMembersList(List<Reference> hasMembersList) {
+            if (memberListNotAlreadySet()) {
+                for (Reference reference : hasMembersList) {
+                    addListEntry(this.hasMembersList, reference.getReference());
+                }
+            } else {
+                throw new UnprocessableEntityException("More then one Observation has the hasMember Attribute, this is not supported by the Fhir-bridge");
+
+            }
+        }
+
+        boolean memberListNotAlreadySet() {
+            return this.hasMembersList.size() == 0;
+        }
+
+        public void validateMembers() {
+            if (!fullUrlList.equals(hasMembersList)) {
+                System.out.println(fullUrlList);
+                System.out.println(hasMembersList);
+                throw new UnprocessableEntityException("The urls of the entries within the Bundle are not matching the hasMember.references. Make sure that every Url contained in the observation with hasMembers is contained and correct.");
+            }
+        }
+    }
+
     @Override
     public void validateRequest(Object payload, Map<String, Object> parameters) {
         Bundle bundle = (Bundle) payload;
 
-        if (bundle.getType() != Bundle.BundleType.TRANSACTION) {
-            throw new UnprocessableEntityException(); // TODO
-        }
+        validateBundleType(bundle);
+        MemberValidator memberValidator = new MemberValidator();
 
         for (Bundle.BundleEntryComponent entry : bundle.getEntry()) {
-            Bundle.BundleEntryRequestComponent request = entry.getRequest();
-            if (request.getMethod() != Bundle.HTTPVerb.POST) {
-                throw new UnprocessableEntityException(); // TODO
-            }
-            Resource resource = entry.getResource();
-            if (!Objects.equals(request.getUrl(), resource.getResourceType().name())) {
-                throw new UnprocessableEntityException(); // TODO
+            setUrlMemberLists(entry, memberValidator);
+            validateRequestElement(entry);
+        }
+        memberValidator.validateMembers();
+    }
+
+    void setUrlMemberLists(Bundle.BundleEntryComponent entry, MemberValidator memberValidator){
+        memberValidator.addFullUrls(entry.getFullUrl());
+        if (isObservation(entry)) {
+            Observation observation = (Observation) entry.getResource();
+            if (observation.getHasMember().size() > 0) {
+                memberValidator.setHasMembersList(observation.getHasMember());
+                memberValidator.deleteFullUrl(entry.getFullUrl()); //Since the Observation is not a Member of itself
             }
         }
     }
+
+    boolean isObservation(Bundle.BundleEntryComponent entry) {
+        return entry.getResource().getResourceType().toString().equals("Observation");
+    }
+
+    void validateBundleType(Bundle bundle) {
+        if (bundle.getType() != Bundle.BundleType.TRANSACTION) {
+            throw new UnprocessableEntityException("The Fhir bridge does currently support only Bundles of the type transaction");
+        }
+    }
+
+    void validateRequestElement(Bundle.BundleEntryComponent entry) {
+        Bundle.BundleEntryRequestComponent request = entry.getRequest();
+        validateHttpVerb(request);
+        validateUrl(request, entry);
+    }
+
+    void validateHttpVerb(Bundle.BundleEntryRequestComponent request) {
+        if (request.getMethod() != Bundle.HTTPVerb.POST) {
+            throw new UnprocessableEntityException("Only the request method POST is supported !");
+        }
+    }
+
+    void validateUrl(Bundle.BundleEntryRequestComponent request, Bundle.BundleEntryComponent entry) {
+        Resource resource = entry.getResource();
+
+        if (!Objects.equals(request.getUrl(), resource.getResourceType().name())) {
+            if (request.getUrl().contains("/")) {
+                throw new UnprocessableEntityException("The Fhir bridge does not supports id assignment from the client side, please delete the ids, request url should looke like this example: \"url\":\"Observation\"");
+            } else {
+                throw new UnprocessableEntityException("The request.url and the resourceType of the entry have to be the same !");
+            }
+        }
+    }
+
 
     @Override
     public void validateResponse(Object payload, Map<String, Object> parameters) {
         throw new UnsupportedOperationException("Response validation is not supported");
     }
+
+
+    private String getEhrUID(Bundle bundle) {
+        String ehrUID = "";
+        for (Bundle.BundleEntryComponent bundleEntry : bundle.getEntry()) {
+            checkIfPatientRessourceNotPresent(bundleEntry);
+            Observation observation = (Observation) bundleEntry.getResource();
+            ehrUID = getEntryEhrUID(ehrUID, observation);
+
+        }
+        return ehrUID;
+    }
+
+    private String getEntryEhrUID(String ehrUID, Observation observation) {
+        if (ehrUID.equals("") || referencesSameEhrUID(ehrUID, observation)) {
+            return observation.getSubject().getReference().split(":")[2];
+        }
+        throw new InternalErrorException("The subject Ids of the profile within the Bundle reference different Patient. They must refer to one identical Patient!");
+
+    }
+
+    private boolean referencesSameEhrUID(String ehrUID, Observation observation) {
+        return ehrUID.equals(observation.getSubject().getReference().split(":")[2]);
+    }
+
+    private void checkIfPatientRessourceNotPresent(Bundle.BundleEntryComponent bundle) {
+        if (bundle.getResource().getResourceType().toString().equals("Patient")) {
+            throw new InternalErrorException("Patient as a resource is not allowed to be contained !");
+        }
+    }
+
+
 }
--- src/main/java/org/ehrbase/fhirbridge/fhir/bundle/BloodGasPanelBundleValidator.java
+++ src/main/java/org/ehrbase/fhirbridge/fhir/bundle/BloodGasPanelBundleValidator.java
@@ -1,15 +1,173 @@
 package org.ehrbase.fhirbridge.fhir.bundle;
 
+import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Resource;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class BloodGasPanelBundleValidator extends AbstractBundleValidator {
+    private static final String bloodGasUrl = "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/blood-gas-panel";
+    private static final String pHUrl = "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/pH";
+    private static final String carbonDioxidePartialPressureUrl = "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/carbon-dioxide-partial-pressure";
+    private static final String oxygenPartialPressureUrl = "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/oxygen-partial-pressure";
+    private static final String oxygenSaturationUrl = "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/oxygen-saturation";
+
+    private Optional<Observation> bloodGasPanel = Optional.empty();
+    private Optional<Observation> pH = Optional.empty();
+    private Optional<Observation> carbonDioxidePartialPressure = Optional.empty();
+    private Optional<Observation> oxygenPartialPressure = Optional.empty();
+    private Optional<Observation> oxygenSaturation = Optional.empty();
+
+    private int bloodGasProfilesContained = 0;
+    private int phProfilesContained = 0;
+    private int carbonDioxideProfilesContained = 0;
+    private int oxygenPartialProfilesContained = 0;
+    private int oxygenSaturationProfilesContained = 0;
+
+    private Bundle bundle;
 
     @Override
     public void validateRequest(Object payload, Map<String, Object> parameters) {
         super.validateRequest(payload, parameters);
-
-        Bundle bundle = (Bundle) payload;
+        this.bundle = (Bundle) payload;
+        validateObservations();
     }
+
+    private void validateObservations() {
+        resetAttributes();
+        for (Bundle.BundleEntryComponent entry : bundle.getEntry()) {
+            validateProfiles(entry);
+        }
+        checkIfObservationsComplete();
+    }
+
+
+    private void resetAttributes() {
+        bloodGasPanel = Optional.empty();
+        pH = Optional.empty();
+        carbonDioxidePartialPressure = Optional.empty();
+        oxygenPartialPressure = Optional.empty();
+        oxygenSaturation = Optional.empty();
+        bloodGasProfilesContained = 0;
+        phProfilesContained = 0;
+        carbonDioxideProfilesContained = 0;
+        oxygenPartialProfilesContained = 0;
+        oxygenSaturationProfilesContained = 0;
+    }
+
+
+    private void validateProfiles(Bundle.BundleEntryComponent entry) {
+        try {
+            String profileUrl = entry.getResource().getMeta().getProfile().get(0).getValue();
+            switch (profileUrl) {
+                case bloodGasUrl:
+                    setBloodGasPanel(entry.getResource());
+                    break;
+                case pHUrl:
+                    setpH(entry.getResource());
+                    break;
+                case carbonDioxidePartialPressureUrl:
+                    setCarbonDioxidePartialPressure(entry.getResource());
+                    break;
+                case oxygenPartialPressureUrl:
+                    setOxygenPartialPressure(entry.getResource());
+                    break;
+                case oxygenSaturationUrl:
+                    setOxygenSaturation(entry.getResource());
+                    break;
+                default:
+                    throw new InternalErrorException("Blood gas panel bundle needs to contain only the profiles for the blood gas panel. Please delete profile " + profileUrl + " from the Bundle.");
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+            throw new UnprocessableEntityException("Make sure only the for Blood Gas Panel supported Profiles are contained in the Bundle these are: blood gas panel, oxygen saturation, carbon dioxide saturation, ph, oxygen partaial pressure");
+        }
+    }
+
+
+    private void checkIfObservationsComplete() {
+        if (!checkIfOneProfileIsPresent()) {
+            throw new UnprocessableEntityException("Bundle Blood gas panel needs to contain at least one of the following profiles: oxygen partial pressure, carbon dioxide partial pressure" +
+                    ", ph or oxygen saturation");
+        }
+    }
+
+    private boolean checkIfOneProfileIsPresent() {
+        return oxygenPartialPressure.isPresent() || carbonDioxidePartialPressure.isPresent() || pH.isPresent() || oxygenSaturation.isPresent();
+    }
+
+    private void setBloodGasPanel(Resource resource) {
+        bloodGasProfilesContained += 1;
+        if (bloodGasProfilesContained == 1) {
+            this.bloodGasPanel = Optional.of((Observation) resource);
+        } else {
+            throw new InternalErrorException("Blood gas Panel profile is duplicated within the bundle, please delete one of them");
+        }
+
+    }
+
+    private void setpH(Resource resource) {
+        phProfilesContained += 1;
+        if (phProfilesContained == 1) {
+            this.pH = Optional.of((Observation) resource);
+        } else {
+            throw new InternalErrorException("PH profile is duplicated within the bundle, please delete one of them");
+        }
+    }
+
+    private void setCarbonDioxidePartialPressure(Resource resource) {
+        carbonDioxideProfilesContained += 1;
+        if (carbonDioxideProfilesContained == 1) {
+            this.carbonDioxidePartialPressure = Optional.of((Observation) resource);
+        } else {
+            throw new InternalErrorException("Carbon Dioxide Partial Pressure profile is duplicated within the bundle, please delete one of them");
+        }
+
+    }
+
+    private void setOxygenPartialPressure(Resource resource) {
+        oxygenPartialProfilesContained += 1;
+        if (oxygenPartialProfilesContained == 1) {
+            this.oxygenPartialPressure = Optional.of((Observation) resource);
+        } else {
+            throw new InternalErrorException("Oxygen partial pressure profile is duplicated within the bundle, please delete one of them");
+        }
+
+    }
+
+    private void setOxygenSaturation(Resource resource) {
+        oxygenSaturationProfilesContained += 1;
+        if (oxygenSaturationProfilesContained == 1) {
+            this.oxygenSaturation = Optional.of((Observation) resource);
+        } else {
+            throw new InternalErrorException("Oxygen saturation profile is duplicated within the bundle, please delete one of them");
+
+        }
+    }
+
+    public Observation getBloodGasPanel() {
+        return bloodGasPanel.get();
+    }
+
+    public Optional<Observation> getpH() {
+        return pH;
+    }
+
+    public Optional<Observation> getCarbonDioxidePartialPressure() {
+        return carbonDioxidePartialPressure;
+    }
+
+    public Optional<Observation> getOxygenPartialPressure() {
+        return oxygenPartialPressure;
+    }
+
+    public Optional<Observation> getOxygenSaturation() {
+        return oxygenSaturation;
+    }
+
 }
+
