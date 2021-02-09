e3b5f54dbf66a6a6b96f973952a3e0a6682c8234
Birgit Dell13Zoll <birgit.saalfeld@plri.de>
2021-02-09 10:05:03 +0100

    created mapping

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/TravelHistoryCompositionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/TravelHistoryCompositionConverter.java
@@ -10,18 +10,12 @@ import org.ehrbase.fhirbridge.ehr.opt.intensivmedizinischesmonitoringkorpertempe
 import org.ehrbase.fhirbridge.ehr.opt.intensivmedizinischesmonitoringkorpertemperaturcomposition.definition.KorpertemperaturBeliebigesEreignisPointEvent;
 import org.ehrbase.fhirbridge.ehr.opt.intensivmedizinischesmonitoringkorpertemperaturcomposition.definition.KorpertemperaturObservation;
 import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.ReisehistorieComposition;
-import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.AussageUeberDenAusschlussDefiningCode;
-import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.AussageUeberDieFehlendeInformationDefiningCode;
-import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.KeineReisehistorieEvaluation;
-import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.ProblemDiagnoseDefiningCode;
-import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.ReiseAngetretenDefiningCode;
-import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.ReisehistorieAdminEntry;
-import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.ReisehistorieBestimmtesReisezielCluster;
-import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.UnbekannteReisehistorieEvaluation;
+import org.ehrbase.fhirbridge.ehr.opt.reisehistoriecomposition.definition.*;
 import org.ehrbase.fhirbridge.ehr.opt.shareddefinition.CategoryDefiningcode;
 import org.ehrbase.fhirbridge.ehr.opt.shareddefinition.Language;
 import org.ehrbase.fhirbridge.ehr.opt.shareddefinition.SettingDefiningcode;
 import org.ehrbase.fhirbridge.ehr.opt.shareddefinition.Territory;
+import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Quantity;
@@ -29,11 +23,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.temporal.TemporalAccessor;
+import java.util.*;
 
 public class TravelHistoryCompositionConverter implements CompositionConverter<ReisehistorieComposition, Observation> {
     private static final Logger LOG = LoggerFactory.getLogger(TravelHistoryCompositionConverter.class);
+
+    private static final Map<String, String> loincTravelCodesMap = new HashMap<>();
+    private static final Map<String, LandDefiningCode> countryMap = new HashMap<>();
+    private static final Map<String, BundeslandRegionDefiningCode> regionMap = new HashMap<>();
+
+    private static String SNOMED_SYSTEM = "http://snomed.info/sct";
+    private static String LOINC_SYSTEM = "http://loinc.org";
+
+
+    private static String LOINC_DATE_TRAVEL_STARTED = "82752-7";
+    private static String LOINC_DATE_OF_DEPARTURE_FROM_TRAVEL_DESTINATION = "91560-3";
+    private static String LOINC_CITY_OF_TRAVEL= "94653-3";
+    private static String LOINC_STATE_OF_TRAVEL="82754-3";
+    private static String LOINC_COUNTRY_OF_TRAVEL="94651-7";
+
+    static {
+
+        for (LandDefiningCode item : LandDefiningCode.values()) {
+            countryMap.put(item.getCode(), item);
+        }
+
+        for (BundeslandRegionDefiningCode item : BundeslandRegionDefiningCode.values()) {
+            regionMap.put(item.getCode(), item);
+        }
+    }
 
     @Override
     public Observation fromComposition(ReisehistorieComposition composition) throws CompositionConversionException {
@@ -45,117 +64,59 @@ public class TravelHistoryCompositionConverter implements CompositionConverter<R
     @Override
     public ReisehistorieComposition toComposition(Observation observation) throws CompositionConversionException {
 
-
-        IntensivmedizinischesMonitoringKorpertemperaturComposition result = new IntensivmedizinischesMonitoringKorpertemperaturComposition();
-
-        // ========================================================================================
-        // value quantity is expected
-        Quantity fhirValue = null;
-        BigDecimal fhirValueNumeric = null;
-        DateTimeType fhirEffectiveDateTime = observation.getEffectiveDateTimeType();
-
-
-        try {
-            fhirValue = observation.getValueQuantity();
-            fhirValueNumeric = fhirValue.getValue();
-            LOG.debug("Value numeric: {}", fhirValueNumeric);
-        } catch (Exception e) {
-            throw new UnprocessableEntityException(e.getMessage());
+        if (null == observation) {
+            throw new UnprocessableEntityException("Observation is null. Couldn't proceed.");
         }
 
-        if (fhirValueNumeric == null) {
-            throw new UnprocessableEntityException("Value is required in FHIR Observation and should be Quantity");
-        }
-
-        // mapping to openEHR
-        KorpertemperaturBeliebigesEreignisPointEvent tempEvent = new KorpertemperaturBeliebigesEreignisPointEvent();
-        tempEvent.setTimeValue(fhirEffectiveDateTime.getValueAsCalendar().toZonedDateTime()); // mandatory
-        tempEvent.setTemperaturMagnitude(fhirValueNumeric.doubleValue());
-        tempEvent.setTemperaturUnits(fhirValue.getUnit());
-
-
-        KorpertemperaturObservation tempObs = new KorpertemperaturObservation();
-        List<KorpertemperaturBeliebigesEreignisChoice> events = new ArrayList<>();
-        events.add(tempEvent);
-        tempObs.setBeliebigesEreignis(events);
-        tempObs.setOriginValue(fhirEffectiveDateTime.getValueAsCalendar().toZonedDateTime()); // mandatory
-        tempObs.setLanguage(Language.EN); // FIXME: we need to grab the language from the template
-        tempObs.setSubject(new PartySelf());
-
-        List<KorpertemperaturObservation> observations = new ArrayList<>();
-        observations.add(tempObs);
-        result.setKorpertemperatur(observations);
-
-
-        //####################################################################
-
-        // copied from BodTemperature Example
-        if (observation == null) {
-            return null;
-        }
-
-        // set feeder audit
-        FeederAudit fa = CommonData.constructFeederAudit(observation);
-        result.setFeederAudit(fa);
-
-        //###############################################################
+        ReisehistorieComposition composition;
 
         String code = getSnomedCodeObservation(observation);
         // check for general travel state
         try {
-            if (code.equals(snomed_yes)) {
-                return map_yes(observation, ReiseAngetretenDefiningCode.YES_QUALIFIER_VALUE);
-            } else if (code.equals(snomed_no)) {
-                return map_no(observation, AussageUeberDenAusschlussDefiningCode.NO_QUALIFIER_VALUE);
-            } else if (code.equals(snomed_unknown)) {
-                return map_unknown(observation, AussageUeberDieFehlendeInformationDefiningCode.UNKNOWN_QUALIFIER_VALUE);
-            } else {
+            if (code.equals(ReiseAngetretenDefiningCode.YES_QUALIFIER_VALUE.getCode())) {
+                composition = map_yes(observation, ReiseAngetretenDefiningCode.YES_QUALIFIER_VALUE);
+            } else if (code.equals(AussageUeberDenAusschlussDefiningCode.NO_QUALIFIER_VALUE.getCode())) {
+                composition = map_no(observation, AussageUeberDenAusschlussDefiningCode.NO_QUALIFIER_VALUE);
+            } else if (code.equals(AussageUeberDieFehlendeInformationDefiningCode.UNKNOWN_QUALIFIER_VALUE.getCode())) {
+                composition = map_unknown(observation, AussageUeberDieFehlendeInformationDefiningCode.UNKNOWN_QUALIFIER_VALUE);
+            } else
                 throw new UnprocessableEntityException("Expected snomed-code for history of travel, but got '" + code + "' instead ");
-            }
         } catch (Exception e) {
             e.printStackTrace();
             throw new UnprocessableEntityException(e.getMessage());
         }
-    }
-    //###################################################################################
 
-    //source http://fhir.ch/ig/ch-ems/ValueSet-yes-no-unknown.html
-    private static final String snomed_yes = "373066001";
-    private static final String snomed_no = "373067005";
-    private static final String snomed_unknown = "261665006";
-
-    private static String getSnomedCodeObservation(Observation fhirObservation) {
-        return fhirObservation.getValueCodeableConcept().getCoding().get(0).getCode();
+        // BSa: What about system (loinc, snomed)
+        // BSa: What about nullFlavour?
+        // BSa: What about identifier?
+        return (composition);
     }
-    //###################################################################################
 
 
     private ReisehistorieComposition createCompositionAndSetDefaults(Observation observation) {
         ReisehistorieComposition composition = new ReisehistorieComposition();
         // ======================================================================================
-        // @severin: do we need all this stuff?
         // Required fields by API
         composition.setLanguage(org.ehrbase.client.classgenerator.shareddefinition.Language.EN); // FIXME: we need to grab the language from the template
         composition.setLocation("test");
-        //composition.setSettingDefiningCode(SettingDefiningcode.SECONDARY_MEDICAL_CARE);// woher weiß ich das?
+        composition.setSettingDefiningCode(SettingDefiningcode.SECONDARY_MEDICAL_CARE);
         composition.setTerritory(org.ehrbase.client.classgenerator.shareddefinition.Territory.DE);
-        //composition.setCategoryDefiningCode(CategoryDefiningcode.EVENT);
-        // @severin: is contained in bodytemperature, but can't find it in the "history of travel" fhir structure
+        composition.setCategoryDefiningCode(CategoryDefiningcode.EVENT);
+
         DateTimeType fhirEffectiveDateTime = observation.getEffectiveDateTimeType();
         composition.setStartTimeValue(fhirEffectiveDateTime.getValueAsCalendar().toZonedDateTime());
 
-        // FIXME: https://github.com/ehrbase/ehrbase_client_library/issues/31
-        //        PartyProxy composer = new PartyIdentified();
-        //        result.setComposer(composer);
-
         composition.setComposer(new PartySelf());
-        //#################################################################
+
+        // set feeder audit
+        FeederAudit fa = CommonData.constructFeederAudit(observation);
+        composition.setFeederAudit(fa);
 
         return composition;
     }
 
-    //###################################################################################
     private ReisehistorieComposition map_yes(Observation observation, ReiseAngetretenDefiningCode reiseCode) {
+
         ReisehistorieComposition composition = createCompositionAndSetDefaults(observation);
         ReisehistorieAdminEntry adminentry = new ReisehistorieAdminEntry();
 
@@ -169,63 +130,87 @@ public class TravelHistoryCompositionConverter implements CompositionConverter<R
 
     private ReisehistorieAdminEntry mapInternalEvents(ReisehistorieAdminEntry adminentry, Observation observation) {
 
-//        for (int i = 0; i < observation.getComponent().size(); i++) {
-//            Observation.ObservationComponentComponent codingObject = getCodingObject(observation, i);
-//            //adminentry = mapTravelDestination(adminentry, codingObject);
-//        }
+        ReisehistorieBestimmtesReisezielCluster travel = null;
 
         for (Observation.ObservationComponentComponent observationComponent
                 : observation.getComponent()) {
-            observationComponent.getName();
+
+            Coding coding = observationComponent.getCode().getCoding().get(0);
+            String system = coding.getSystem();
+            String code =   coding.getCode();
+
+            checkForSnomedSystem(system);
+
+            if (code.equals(LOINC_DATE_TRAVEL_STARTED)) {
+                travel.setEinreisedatumValue(getDate(observationComponent));
+            } else if (code.equals(LOINC_DATE_OF_DEPARTURE_FROM_TRAVEL_DESTINATION)) {
+                travel.setAbfahrtsdatumValue(getDate(observationComponent));
+            } else if (code.equals(LOINC_CITY_OF_TRAVEL)) {
+                travel.setStadtValue(getCity(observationComponent));
+            } else if (code.equals(LOINC_STATE_OF_TRAVEL)) {
+                travel.setBundeslandRegionDefiningCode(getBundeslandRegion(observationComponent));
+            } else if (code.equals(LOINC_COUNTRY_OF_TRAVEL)) {
+                travel.setLandDefiningCode(getLand(observationComponent));
+            } else {
+                throw new UnprocessableEntityException("Expected loinc-code for history of travel, but got '" + code + "' instead ");
+            }
         }
-        optional oben als member und rein packen
+        adminentry.setBestimmtesReiseziel(List.of(travel));
 
         return adminentry;
     }
 
-    private final String LOINC_URL = "http://loinc.org";
-    private final String LOINC_DATE_TRAVEL_STARTED = "82752-7";
-    private final String LOINC_DATE_OF_DEPARTURE_FROM_TRAVEL_DESTINATION = "91560-3";
-    private final String LOINC_CITY_OF_TRAVEL = "94653-3";
-    private final String LOINC_STATE_OF_TRAVEL = "82754-3";
-    private final String LOINC_COUNTRY_OF_TRAVEL = "94651-7";
+    private TemporalAccessor getDate(Observation.ObservationComponentComponent observationComponent) {
 
-    private ReisehistorieAdminEntry mapTravelDestination(ReisehistorieAdminEntry adminentry, Observation.ObservationComponentComponent codingObject) {
-        String codeOfConcept = getCodeOfConcept(codingObject);
+        Coding coding = observationComponent.getCode().getCoding().get(0);
+        checkForLoincSystem(coding.getSystem());
+        TemporalAccessor date = observationComponent.getValueDateTimeType().getValueAsCalendar().toZonedDateTime();
+        return date;
+    }
 
-        //Birgit Continue -> code worked in old fhir-bridge, but not longer
+    private String getCity(Observation.ObservationComponentComponent observationComponent) {
+        Coding coding = observationComponent.getCode().getCoding().get(0);
+        checkForLoincSystem(coding.getSystem());
+        String city = coding.getCode();
+        return city;
+    }
 
-        // testen: 168 https://github.com/ehrbase/fhir-bridge-old/blob/feature/num_82_JunitTestsPrototype/src/test/java/org/ehrbase/fhirbridge/FhirBridgeApplicationIT.java
+    private LandDefiningCode getLand(Observation.ObservationComponentComponent observationComponent) {
+        Coding coding = observationComponent.getValueCodeableConcept().getCoding().get(0);
+        checkForLoincSystem(coding.getSystem());
+        LandDefiningCode country = countryMap.get(coding.getCode());
+        return country;
+    }
 
-        adminentry.setBestimmtesReiseziel();
+    private BundeslandRegionDefiningCode getBundeslandRegion(Observation.ObservationComponentComponent observationComponent) {
+        Coding coding = observationComponent.getValueCodeableConcept().getCoding().get(0);
+        checkForLoincSystem(coding.getSystem());
+        BundeslandRegionDefiningCode region = regionMap.get(coding.getCode());
+        return region;
+    }
 
-        List<ReisehistorieBestimmtesReisezielCluster> travelDestinations = new
-
-                //travelDestinations.
-
-                ReisehistorieBestimmtesReisezielCluster a;
-
-        a.setEinreisedatumValue();
-
-        if (codeOfConcept.equals(LOINC_DATE_TRAVEL_STARTED)) {
-            adminentry.setAbreisedatumValue(getDateTime(codingObject));
-        } else if (codeOfConcept.equals(LOINC_DATE_OF_DEPARTURE_FROM_TRAVEL_DESTINATION)) {
-            adminentry.setRuckreisedatumValue(getDateTime(codingObject));
-        } else if (codeOfConcept.equals(LOINC_CITY_OF_TRAVEL)) {
-            adminentry.setStadtValue(getString(codingObject));
-        } else if (codeOfConcept.equals(LOINC_STATE_OF_TRAVEL)) {
-            adminentry.setBundeslandRegionDefiningcode(getBundeslandByCode(codingObject));
-        } else if (codeOfConcept.equals(LOINC_COUNTRY_OF_TRAVEL)) {
-            adminentry.setLandDefiningcode(getLandByCode(codingObject));
-        } else {
-            throw new UnprocessableEntityException("Expected loinc-code for history of travel, but got '" + codeOfConcept + "' instead ");
+    private void checkForLoincSystem(String systemCode) {
+        if(!LOINC_SYSTEM.equals(systemCode))
+        {
+            throw new UnprocessableEntityException("The system is not correct. "+
+                    "It should be '"+LOINC_SYSTEM+"', but it was '"+systemCode+"'.");
         }
+    }
 
-        travelDestinations.add(a);
-        adminentry.setBestimmtesReiseziel(List.of(travelDestinations, anders, anders));
+    private void checkForSnomedSystem(String systemCode) {
+        if(!SNOMED_SYSTEM.equals(systemCode))
+        {
+            throw new UnprocessableEntityException("The system is not correct. "+
+                    "It should be '"+SNOMED_SYSTEM+"', but it was '"+systemCode+"'.");
+        }
+    }
 
+    private String getSnomedCodeObservation(Observation fhirObservation) {
 
-        return adminentry;
+        Coding code = fhirObservation.getValueCodeableConcept().getCoding().get(0);
+        checkForSnomedSystem(code.getSystem());
+
+        return code.getCode();
     }
 
     private String getCodeOfConcept(Observation.ObservationComponentComponent codingObject) {
@@ -244,10 +229,15 @@ public class TravelHistoryCompositionConverter implements CompositionConverter<R
         ReisehistorieComposition composition = createCompositionAndSetDefaults(observation);
         KeineReisehistorieEvaluation evaluation = new KeineReisehistorieEvaluation();
 
+
         evaluation.setProblemDiagnoseDefiningCode(ProblemDiagnoseDefiningCode.HISTORY_OF_TRAVEL);
         evaluation.setAussageUeberDenAusschlussDefiningCode(reiseCode);
 
         composition.setKeineReisehistorie(evaluation);
+
+        //optional
+        observation.getDataAbsentReason();
+
 
         return composition;
     }
@@ -265,6 +255,4 @@ public class TravelHistoryCompositionConverter implements CompositionConverter<R
 
         return composition;
     }
-
-    //@Severin: was ist mit dem feeder audit? Setzen?
 }
