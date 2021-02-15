f37b88e9f5fd5b43c5efad4a7fc2030760af57bc
SevKohler <kohlerseverin@posteo.de>
2021-02-15 16:20:39 +0100

    loinc code changed

---
+++ src/test/java/org/ehrbase/fhirbridge/fhir/observation/PulseOximetryIT.java
@@ -0,0 +1,67 @@
+package org.ehrbase.fhirbridge.fhir.observation;
+
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
+import org.ehrbase.fhirbridge.comparators.CustomTemporalAcessorComparator;
+import org.ehrbase.fhirbridge.ehr.converter.pulseoximetry.PulseOximetryConverter;
+import org.ehrbase.fhirbridge.ehr.converter.radiologischerBefund.RadiologischerBefundConverter;
+import org.ehrbase.fhirbridge.ehr.opt.d4lquestionnairecomposition.definition.SchwangerschaftsstatusObservation;
+import org.ehrbase.fhirbridge.ehr.opt.geccoradiologischerbefundcomposition.GECCORadiologischerBefundComposition;
+import org.ehrbase.fhirbridge.ehr.opt.pulsoxymetriecomposition.PulsoxymetrieComposition;
+import org.ehrbase.fhirbridge.ehr.opt.pulsoxymetriecomposition.definition.PulsoxymetrieObservation;
+import org.ehrbase.fhirbridge.fhir.AbstractMappingTestSetupIT;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.DiagnosticReport;
+import org.hl7.fhir.r4.model.Observation;
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.core.diff.Diff;
+import org.javers.core.metamodel.clazz.ValueObjectDefinition;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.temporal.TemporalAccessor;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Integration tests for {@link Observation Observation} resource.
+ */
+class PulseOximetryIT extends AbstractMappingTestSetupIT {
+
+    public PulseOximetryIT() {
+        super("Observation/PulseOximetry/", Observation.class);
+    }
+
+    @Test
+    void createPulseOxymetry() throws IOException {
+        create("create-pulse-oximetry.json");
+    }
+
+    @Test
+    void mappingNormalFinding() throws IOException {
+        Observation observation = (Observation) super.testFileLoader.loadResource("create-pulse-oximetry.json");
+        PulseOximetryConverter pulseOximetryConverter = new PulseOximetryConverter();
+        PulsoxymetrieComposition mappedPulsoxymetrieComposition = pulseOximetryConverter.toComposition(observation);
+        Diff diff = compareCompositions(getJavers(), "paragon-create-pulse-oximetry.json", mappedPulsoxymetrieComposition);
+
+        assertEquals(diff.getChanges().size(), 0);
+    }
+
+    @Override
+    public Exception executeMappingUnprocessableEntityException(IBaseResource baseResource) {
+        return assertThrows(UnprocessableEntityException.class, () -> {
+             new PulseOximetryConverter().toComposition(((Observation) baseResource));
+        });
+    }
+
+
+    @Override
+    public Javers getJavers() {
+        return JaversBuilder.javers()
+                .registerValue(TemporalAccessor.class, new CustomTemporalAcessorComparator())
+                .registerValueObject(new ValueObjectDefinition(PulsoxymetrieComposition.class, List.of("location")))
+                .registerValueObject(PulsoxymetrieObservation.class)
+                .build();
+    }
+}
