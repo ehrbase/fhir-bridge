c62f7e59352338cc43e087b9827e5af9327d4c70
Wlad <64201310+wlad@users.noreply.github.com>
2021-02-18 14:19:04 +0100

    Enhances and cleans up suite configuration

--- tests/robot/_resources/variables/sut_config.py
+++ tests/robot/_resources/variables/sut_config.py
@@ -19,97 +19,100 @@ import os
 import base64
 from requests import request
 
-# ENVIRONMENT VARS
-DEV_FHIRBRIDGE_URL = os.getenv('DEV_FHIRBRIDGE_URL')
-DEV_EHRBASE_URL = os.getenv('DEV_EHRBASE_URL')
-DEV_EHRBASE_USER = os.getenv('DEV_EHRBASE_USER')
-DEV_EHRBASE_PASS = os.getenv('DEV_EHRBASE_PASS')
-
-X = f"{DEV_EHRBASE_USER}:{DEV_EHRBASE_PASS}"
-Xb64 = base64.b64encode(X.encode("utf-8"))
-XS = str(Xb64, "utf-8")
-DEV_AUTH = f"Basic {XS}"
-
-STAGING_FHIRBRIDGE_URL = os.getenv('STAGING_FHIRBRIDGE_URL')
-STAGING_EHRBASE_URL = os.getenv('STAGING_EHRBASE_URL')
-STAGING_EHRBASE_USER = os.getenv('STAGING_EHRBASE_USER')
-STAGING_EHRBASE_PASS = os.getenv('STAGING_EHRBASE_PASS')
-
-Y = f"{STAGING_EHRBASE_USER}:{STAGING_EHRBASE_PASS}"
-Yb64 = base64.b64encode(X.encode("utf-8"))
-YS = str(Yb64, "utf-8")
-STAGING_AUTH = f"Basic {YS}"
 
 
+def get_variables(sut="LOCAL", auth_type="BASIC"):
 
+    #////////////////////////////////////////////////
+    #// CENTRAL RESEARCH REPOSITORY              ///
+    #//////////////////////////////////////////////
 
-# SUT CONFIGURATIONS
-"""
-CONFIG              SUT STARTUP AUTOMATED?      COMMENT
-------              ----------------------      -------
+    # get urls and credentials of remote environments from ENVs
+    if sut != "LOCAL":
+        FHIRBRIDGE_URL = os.getenv('FHIRBRIDGE_URL')
+        EHRBASE_URL = os.getenv('EHRBASE_URL')
+        EHRBASE_USER = os.getenv('EHRBASE_USER')
+        EHRBASE_PASS = os.getenv('EHRBASE_PASS')
 
-CI == LOCAL
-LOCAL
-DEV (GWDG)            
-TEST (GWDG)
-"""
+        userpass = f"{EHRBASE_USER}:{EHRBASE_PASS}"
+        userpassb64 = base64.b64encode(userpass.encode("utf-8"))
+        auth = str(userpassb64, "utf-8")
 
-# local environment: for local execution
-# the same is used for continuous integration on CircleCI
-LOCAL_CONFIG = {
-    "SUT": "LOCAL",
-    "BASE_URL": "http://localhost:8888/fhir-bridge/fhir",
-    "EHRBASE_URL": "http://localhost:8080/ehrbase/rest/openehr/v1",
-    "HEARTBEAT_URL": "http://localhost:8080/ehrbase/",
-    "CREDENTIALS": ["ehrbase-user", "SuperSecretPassword"],
-    "SECURITY_AUTHTYPE": "BASIC",
-    "AUTHORIZATION": {
-        "Authorization": "Basic ZWhyYmFzZS11c2VyOlN1cGVyU2VjcmV0UGFzc3dvcmQ="
-    },
-    "NODENAME": "local.execution.org",
-    "CONTROL_MODE": "robot"
-}
-
-DEV_CONFIG = {
-    "SUT": "DEV",
-    "BASE_URL": DEV_FHIRBRIDGE_URL + '/fhir',
-    "EHRBASE_URL": DEV_EHRBASE_URL + '/ehrbase/rest/openehr/v1',
-    "HEARTBEAT_FHIRBRIDGE": DEV_FHIRBRIDGE_URL,
-    "HEARTBEAT_EHRBASE": DEV_EHRBASE_URL,
-    "CREDENTIALS": [DEV_EHRBASE_USER, DEV_EHRBASE_PASS],
-    "SECURITY_AUTHTYPE": "BASIC",
-    "AUTHORIZATION": {
-        "Authorization": DEV_AUTH
-    },
-    "NODENAME": "crr_dev.execution.org",
-    "CONTROL_MODE": "robot"
-}
-
-STAGING_CONFIG = {
-    "SUT": "STAGING",
-    "BASE_URL": STAGING_FHIRBRIDGE_URL + '/fhir',
-    "EHRBASE_URL": STAGING_EHRBASE_URL + '/ehrbase/rest/openehr/v1',
-    "HEARTBEAT_FHIRBRIDGE": STAGING_FHIRBRIDGE_URL,
-    "HEARTBEAT_EHRBASE": STAGING_EHRBASE_URL,
-    "CREDENTIALS": [STAGING_EHRBASE_USER, STAGING_EHRBASE_PASS],
-    "SECURITY_AUTHTYPE": "BASIC",
-    "AUTHORIZATION": {
-        "Authorization": STAGING_AUTH
-    },
-    "NODENAME": "crr_staging.execution.org",
-    "CONTROL_MODE": "robot"
-}
-
-
-def get_variables(sut="LOCAL", auth_type="BASIC", nodocker="NEIN!"):
     # LOCAL CONFIG W/ BASIC AUTH
     if sut == "LOCAL":
+        # local environment: is used per default
+        # the same is also used for continuous integration on CircleCI
+        LOCAL_CONFIG = {
+            "SUT": "LOCAL",
+            "BASE_URL": "http://localhost:8888/fhir-bridge/fhir",
+            "EHRBASE_URL": "http://localhost:8080/ehrbase/rest/openehr/v1",
+            "HEARTBEAT_URL": "http://localhost:8080/ehrbase/",
+            "CREDENTIALS": ["ehrbase-user", "SuperSecretPassword"],
+            "SECURITY_AUTHTYPE": "BASIC",
+            "AUTHORIZATION": {
+                "Authorization": "Basic ZWhyYmFzZS11c2VyOlN1cGVyU2VjcmV0UGFzc3dvcmQ="
+            },
+            "NODENAME": "local.execution.org",
+            "CONTROL_MODE": "robot"
+        }
         return LOCAL_CONFIG
     
     # DEV (GWDG) CONFIG W/ BASIC AUTH
     if sut == "DEV":
+        DEV_CONFIG = {
+            "SUT": "DEV",
+            "BASE_URL": FHIRBRIDGE_URL + '/fhir',
+            "EHRBASE_URL": EHRBASE_URL + '/ehrbase/rest/openehr/v1',
+            "HEARTBEAT_FHIRBRIDGE": FHIRBRIDGE_URL,
+            "HEARTBEAT_EHRBASE": EHRBASE_URL,
+            "CREDENTIALS": [EHRBASE_USER, EHRBASE_PASS],
+            "SECURITY_AUTHTYPE": "BASIC",
+            "AUTHORIZATION": {
+                "Authorization": f"Basic {auth}"
+            },
+            "NODENAME": "crr_dev.execution.org",
+            "CONTROL_MODE": "robot"
+        }
         return DEV_CONFIG
     
     # STAGING (GWDG) CONFIG W/ BASIC AUTH
     if sut == "STAGING":
+        STAGING_CONFIG = {
+            "SUT": "STAGING",
+            "BASE_URL": FHIRBRIDGE_URL + '/fhir',
+            "EHRBASE_URL": EHRBASE_URL + '/ehrbase/rest/openehr/v1',
+            "HEARTBEAT_FHIRBRIDGE": FHIRBRIDGE_URL,
+            "HEARTBEAT_EHRBASE": EHRBASE_URL,
+            "CREDENTIALS": [EHRBASE_USER, EHRBASE_PASS],
+            "SECURITY_AUTHTYPE": "BASIC",
+            "AUTHORIZATION": {
+                "Authorization": f"Basic {auth}"
+            },
+            "NODENAME": "crr_staging.execution.org",
+            "CONTROL_MODE": "robot"
+        }
         return STAGING_CONFIG
+    
+    # PREPROD (GWDG) CONFIG W/ BASIC AUTH
+    if sut == "PREPROD":
+        PREPROD_CONFIG = {
+            "SUT": "PREPROD",
+            "BASE_URL": FHIRBRIDGE_URL + '/fhir',
+            "EHRBASE_URL": EHRBASE_URL + '/ehrbase/rest/openehr/v1',
+            "HEARTBEAT_FHIRBRIDGE": FHIRBRIDGE_URL,
+            "HEARTBEAT_EHRBASE": EHRBASE_URL,
+            "CREDENTIALS": [EHRBASE_USER, EHRBASE_PASS],
+            "SECURITY_AUTHTYPE": "BASIC",
+            "AUTHORIZATION": {
+                "Authorization": f"Basic {auth}"
+            },
+            "NODENAME": "crr_preprod.execution.org",
+            "CONTROL_MODE": "robot"
+        }
+        return PREPROD_CONFIG
+
+    #/////////////////////////////////////////////////////
+    #// CENTRAL TRANSACTIONAL REPOSITORY              ///
+    #///////////////////////////////////////////////////
+
+    # TODO: Add configs for 'transactional repo' environments
