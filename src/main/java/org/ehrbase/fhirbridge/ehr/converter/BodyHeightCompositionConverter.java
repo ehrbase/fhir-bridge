b4aec401db6a2413cbdcc7042e266076a57160aa
Birgit Dell13Zoll <birgit.saalfeld@plri.de>
2021-02-28 14:42:43 +0100

    refactor

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/BodyHeightCompositionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/BodyHeightCompositionConverter.java
@@ -9,7 +9,6 @@ import org.ehrbase.fhirbridge.ehr.opt.shareddefinition.CategoryDefiningcode;
 import org.ehrbase.fhirbridge.ehr.opt.shareddefinition.Language;
 import org.ehrbase.fhirbridge.ehr.opt.shareddefinition.SettingDefiningcode;
 import org.ehrbase.fhirbridge.ehr.opt.shareddefinition.Territory;
-import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.Observation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,51 +33,63 @@ public class BodyHeightCompositionConverter implements CompositionConverter<Korp
             return null;
         }
 
-        KorpergrosseComposition result = new KorpergrosseComposition();
-        GrosseLangeObservation grosseLangeObservation = new GrosseLangeObservation();
-
-
         ZonedDateTime fhirEffectiveDateTime = null;
-        try {
+        fhirEffectiveDateTime = getDateTime(observation);
 
-            if(observation.hasEffectiveDateTimeType()) {
+        GrosseLangeObservation grosseLangeObservation = new GrosseLangeObservation();
+        setDateTime(grosseLangeObservation, fhirEffectiveDateTime);
+        setDefault(grosseLangeObservation);
+        setMappingContent(observation, grosseLangeObservation);
 
-                // default for every observation
-                fhirEffectiveDateTime = observation.getEffectiveDateTimeType().getValueAsCalendar().toZonedDateTime();
-                grosseLangeObservation.setTimeValue(fhirEffectiveDateTime);
-                grosseLangeObservation.setOriginValue(fhirEffectiveDateTime);
+        KorpergrosseComposition composition = createComposition(fhirEffectiveDateTime, grosseLangeObservation);
 
-            } else if(observation.hasEffectivePeriod()) {
-                fhirEffectiveDateTime = observation.getEffectivePeriod().getStart().toInstant().atZone(ZoneId.systemDefault());
-                grosseLangeObservation.setTimeValue(fhirEffectiveDateTime);
-                grosseLangeObservation.setOriginValue(fhirEffectiveDateTime);
-            } else {
-                throw new UnprocessableEntityException("No time is set");
-            }
+        return composition;
+    }
 
-            grosseLangeObservation.setLanguage(Language.DE); // FIXME: we need to grab the language from the template
-            grosseLangeObservation.setSubject(new PartySelf());
-
-            // special mapping content
-            grosseLangeObservation.setGrosseLangeUnits(observation.getValueQuantity().getCode());
-
-            grosseLangeObservation.setGrosseLangeMagnitude(observation.getValueQuantity().getValue().doubleValue());
-
-        } catch (Exception e) {
-            throw new UnprocessableEntityException(e.getMessage());
-        }
-
-        result.setGrosseLange(grosseLangeObservation);
-
+    private KorpergrosseComposition createComposition(ZonedDateTime fhirEffectiveDateTime, GrosseLangeObservation grosseLangeObservation) {
+        KorpergrosseComposition composition = new KorpergrosseComposition();
+        composition.setGrosseLange(grosseLangeObservation);
         // Required fields by API
-        result.setLanguage(Language.DE); // FIXME: we need to grab the language from the template
-        result.setLocation("test");
-        result.setSettingDefiningcode(SettingDefiningcode.SECONDARY_MEDICAL_CARE);
-        result.setTerritory(Territory.DE);
-        result.setCategoryDefiningcode(CategoryDefiningcode.EVENT);
-        result.setStartTimeValue(fhirEffectiveDateTime);
-        result.setComposer(new PartySelf()); // FIXME: id ausdenken oder weglassen?
+        composition.setLanguage(Language.DE); // FIXME: we need to grab the language from the template
+        composition.setLocation("test");
+        composition.setSettingDefiningcode(SettingDefiningcode.SECONDARY_MEDICAL_CARE);
+        composition.setTerritory(Territory.DE);
+        composition.setCategoryDefiningcode(CategoryDefiningcode.EVENT);
+        composition.setStartTimeValue(fhirEffectiveDateTime);
+        composition.setComposer(new PartySelf()); // FIXME: id ausdenken oder weglassen?
 
-        return result;
+        return (composition);
+    }
+
+    private ZonedDateTime getDateTime(Observation observation) {
+        ZonedDateTime fhirEffectiveDateTime;
+        if(observation.hasEffectiveDateTimeType()) {
+
+            // default for every observation
+            fhirEffectiveDateTime = observation.getEffectiveDateTimeType().getValueAsCalendar().toZonedDateTime();
+
+
+        } else if(observation.hasEffectivePeriod()) {
+            fhirEffectiveDateTime = observation.getEffectivePeriod().getStart().toInstant().atZone(ZoneId.systemDefault());
+
+        } else {
+            throw new UnprocessableEntityException("No time is set");
+        }
+        return fhirEffectiveDateTime;
+    }
+
+    private void setDateTime(GrosseLangeObservation grosseLangeObservation, ZonedDateTime fhirEffectiveDateTime) {
+        grosseLangeObservation.setTimeValue(fhirEffectiveDateTime);
+        grosseLangeObservation.setOriginValue(fhirEffectiveDateTime);
+    }
+
+    private void setMappingContent(Observation observation, GrosseLangeObservation grosseLangeObservation) {
+        grosseLangeObservation.setGrosseLangeUnits(observation.getValueQuantity().getCode());
+        grosseLangeObservation.setGrosseLangeMagnitude(observation.getValueQuantity().getValue().doubleValue());
+    }
+
+    private void setDefault(GrosseLangeObservation grosseLangeObservation) {
+        grosseLangeObservation.setLanguage(Language.DE); // FIXME: we need to grab the language from the template
+        grosseLangeObservation.setSubject(new PartySelf());
     }
 }
