841e5164ecdbe76aa19c57df8e4204ffbc2988db
SevKohler <kohlerseverin@posteo.de>
2021-03-04 12:46:57 +0100

    generated opt classes

---
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/geccoserologischerbefundcomposition/GECCOSerologischerBefundComposition.java
@@ -0,0 +1,306 @@
+package org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition;
+
+import com.nedap.archie.rm.archetyped.FeederAudit;
+import com.nedap.archie.rm.datastructures.Cluster;
+import com.nedap.archie.rm.generic.Participation;
+import com.nedap.archie.rm.generic.PartyIdentified;
+import com.nedap.archie.rm.generic.PartyProxy;
+import java.lang.String;
+import java.time.temporal.TemporalAccessor;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.ehrbase.client.annotations.Archetype;
+import org.ehrbase.client.annotations.Entity;
+import org.ehrbase.client.annotations.Id;
+import org.ehrbase.client.annotations.Path;
+import org.ehrbase.client.annotations.Template;
+import org.ehrbase.client.classgenerator.interfaces.CompositionEntity;
+import org.ehrbase.client.classgenerator.shareddefinition.Category;
+import org.ehrbase.client.classgenerator.shareddefinition.Language;
+import org.ehrbase.client.classgenerator.shareddefinition.NullFlavour;
+import org.ehrbase.client.classgenerator.shareddefinition.Setting;
+import org.ehrbase.client.classgenerator.shareddefinition.Territory;
+import org.ehrbase.client.openehrclient.VersionUid;
+import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.BefundObservation;
+import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.GeccoSerologischerBefundKategorieLoincElement;
+import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.KategorieDefiningCode;
+import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.StatusDefiningCode;
+
+@Entity
+@Archetype("openEHR-EHR-COMPOSITION.registereintrag.v1")
+@Generated(
+    value = "org.ehrbase.client.classgenerator.ClassGenerator",
+    date = "2021-03-04T12:46:24.471683+01:00",
+    comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.0.0"
+)
+@Template("GECCO_Serologischer Befund")
+public class GECCOSerologischerBefundComposition implements CompositionEntity {
+  /**
+   * Path: GECCO_Serologischer Befund/category
+   */
+  @Path("/category|defining_code")
+  private Category categoryDefiningCode;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/Erweiterung
+   * Description: Ergänzende Angaben zum Registereintrag.
+   */
+  @Path("/context/other_context[at0001 and name/value='Tree']/items[at0002]")
+  private List<Cluster> erweiterung;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/Status
+   * Description: Status der gelieferten Daten für den Registereintrag. Hinweis: Dies ist nicht der Status einzelner Komponenten.
+   */
+  @Path("/context/other_context[at0001 and name/value='Tree']/items[at0004]/value|defining_code")
+  private StatusDefiningCode statusDefiningCode;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/Tree/Status/null_flavour
+   */
+  @Path("/context/other_context[at0001 and name/value='Tree']/items[at0004]/null_flavour|defining_code")
+  private NullFlavour statusNullFlavourDefiningCode;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/Kategorie
+   * Description: Die Klassifikation des Registereintrags (z.B. Typ der Observation des FHIR-Profils).
+   */
+  @Path("/context/other_context[at0001 and name/value='Tree']/items[at0005 and name/value='Kategorie']/value|defining_code")
+  private KategorieDefiningCode kategorieDefiningCode;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/Tree/Kategorie/null_flavour
+   */
+  @Path("/context/other_context[at0001 and name/value='Tree']/items[at0005 and name/value='Kategorie']/null_flavour|defining_code")
+  private NullFlavour kategorieNullFlavourDefiningCode;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/Kategorie (LOINC)
+   * Description: Die Klassifikation des Registereintrags (z.B. Typ der Observation des FHIR-Profils).
+   */
+  @Path("/context/other_context[at0001 and name/value='Tree']/items[at0005 and name/value='Kategorie (LOINC)']")
+  private List<GeccoSerologischerBefundKategorieLoincElement> kategorieLoinc;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/start_time
+   */
+  @Path("/context/start_time|value")
+  private TemporalAccessor startTimeValue;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/participations
+   */
+  @Path("/context/participations")
+  private List<Participation> participations;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/end_time
+   */
+  @Path("/context/end_time|value")
+  private TemporalAccessor endTimeValue;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/location
+   */
+  @Path("/context/location")
+  private String location;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/health_care_facility
+   */
+  @Path("/context/health_care_facility")
+  private PartyIdentified healthCareFacility;
+
+  /**
+   * Path: GECCO_Serologischer Befund/context/setting
+   */
+  @Path("/context/setting|defining_code")
+  private Setting settingDefiningCode;
+
+  /**
+   * Path: GECCO_Serologischer Befund/Befund
+   * Description: Das Ergebnis - einschließlich der Befunde und der Interpretation des Labors - einer Untersuchung, die an Proben durchgeführt wurde, die von einer Einzelperson stammen oder mit dieser Person zusammenhängen.
+   */
+  @Path("/content[openEHR-EHR-OBSERVATION.laboratory_test_result.v1 and name/value='Befund']")
+  private BefundObservation befund;
+
+  /**
+   * Path: GECCO_Serologischer Befund/composer
+   */
+  @Path("/composer")
+  private PartyProxy composer;
+
+  /**
+   * Path: GECCO_Serologischer Befund/language
+   */
+  @Path("/language")
+  private Language language;
+
+  /**
+   * Path: GECCO_Serologischer Befund/feeder_audit
+   */
+  @Path("/feeder_audit")
+  private FeederAudit feederAudit;
+
+  /**
+   * Path: GECCO_Serologischer Befund/territory
+   */
+  @Path("/territory")
+  private Territory territory;
+
+  @Id
+  private VersionUid versionUid;
+
+  public void setCategoryDefiningCode(Category categoryDefiningCode) {
+     this.categoryDefiningCode = categoryDefiningCode;
+  }
+
+  public Category getCategoryDefiningCode() {
+     return this.categoryDefiningCode ;
+  }
+
+  public void setErweiterung(List<Cluster> erweiterung) {
+     this.erweiterung = erweiterung;
+  }
+
+  public List<Cluster> getErweiterung() {
+     return this.erweiterung ;
+  }
+
+  public void setStatusDefiningCode(StatusDefiningCode statusDefiningCode) {
+     this.statusDefiningCode = statusDefiningCode;
+  }
+
+  public StatusDefiningCode getStatusDefiningCode() {
+     return this.statusDefiningCode ;
+  }
+
+  public void setStatusNullFlavourDefiningCode(NullFlavour statusNullFlavourDefiningCode) {
+     this.statusNullFlavourDefiningCode = statusNullFlavourDefiningCode;
+  }
+
+  public NullFlavour getStatusNullFlavourDefiningCode() {
+     return this.statusNullFlavourDefiningCode ;
+  }
+
+  public void setKategorieDefiningCode(KategorieDefiningCode kategorieDefiningCode) {
+     this.kategorieDefiningCode = kategorieDefiningCode;
+  }
+
+  public KategorieDefiningCode getKategorieDefiningCode() {
+     return this.kategorieDefiningCode ;
+  }
+
+  public void setKategorieNullFlavourDefiningCode(NullFlavour kategorieNullFlavourDefiningCode) {
+     this.kategorieNullFlavourDefiningCode = kategorieNullFlavourDefiningCode;
+  }
+
+  public NullFlavour getKategorieNullFlavourDefiningCode() {
+     return this.kategorieNullFlavourDefiningCode ;
+  }
+
+  public void setKategorieLoinc(
+      List<GeccoSerologischerBefundKategorieLoincElement> kategorieLoinc) {
+     this.kategorieLoinc = kategorieLoinc;
+  }
+
+  public List<GeccoSerologischerBefundKategorieLoincElement> getKategorieLoinc() {
+     return this.kategorieLoinc ;
+  }
+
+  public void setStartTimeValue(TemporalAccessor startTimeValue) {
+     this.startTimeValue = startTimeValue;
+  }
+
+  public TemporalAccessor getStartTimeValue() {
+     return this.startTimeValue ;
+  }
+
+  public void setParticipations(List<Participation> participations) {
+     this.participations = participations;
+  }
+
+  public List<Participation> getParticipations() {
+     return this.participations ;
+  }
+
+  public void setEndTimeValue(TemporalAccessor endTimeValue) {
+     this.endTimeValue = endTimeValue;
+  }
+
+  public TemporalAccessor getEndTimeValue() {
+     return this.endTimeValue ;
+  }
+
+  public void setLocation(String location) {
+     this.location = location;
+  }
+
+  public String getLocation() {
+     return this.location ;
+  }
+
+  public void setHealthCareFacility(PartyIdentified healthCareFacility) {
+     this.healthCareFacility = healthCareFacility;
+  }
+
+  public PartyIdentified getHealthCareFacility() {
+     return this.healthCareFacility ;
+  }
+
+  public void setSettingDefiningCode(Setting settingDefiningCode) {
+     this.settingDefiningCode = settingDefiningCode;
+  }
+
+  public Setting getSettingDefiningCode() {
+     return this.settingDefiningCode ;
+  }
+
+  public void setBefund(BefundObservation befund) {
+     this.befund = befund;
+  }
+
+  public BefundObservation getBefund() {
+     return this.befund ;
+  }
+
+  public void setComposer(PartyProxy composer) {
+     this.composer = composer;
+  }
+
+  public PartyProxy getComposer() {
+     return this.composer ;
+  }
+
+  public void setLanguage(Language language) {
+     this.language = language;
+  }
+
+  public Language getLanguage() {
+     return this.language ;
+  }
+
+  public void setFeederAudit(FeederAudit feederAudit) {
+     this.feederAudit = feederAudit;
+  }
+
+  public FeederAudit getFeederAudit() {
+     return this.feederAudit ;
+  }
+
+  public void setTerritory(Territory territory) {
+     this.territory = territory;
+  }
+
+  public Territory getTerritory() {
+     return this.territory ;
+  }
+
+  public VersionUid getVersionUid() {
+     return this.versionUid ;
+  }
+
+  public void setVersionUid(VersionUid versionUid) {
+     this.versionUid = versionUid;
+  }
+}
