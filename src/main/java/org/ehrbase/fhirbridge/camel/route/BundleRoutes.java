cbbe8ec5f8d195285dfc2acdf092a03abcff9fa0
SevKohler <kohlerseverin@posteo.de>
2021-03-04 14:00:28 +0100

    add AntibodyPanel to route etc

--- src/main/java/org/ehrbase/fhirbridge/camel/route/BundleRoutes.java
+++ src/main/java/org/ehrbase/fhirbridge/camel/route/BundleRoutes.java
@@ -4,8 +4,10 @@ import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import org.apache.camel.builder.RouteBuilder;
 import org.ehrbase.fhirbridge.camel.FhirBridgeConstants;
 import org.ehrbase.fhirbridge.camel.processor.BundleResponseProcessor;
+import org.ehrbase.fhirbridge.ehr.converter.antibodypanel.AntiBodyPanelConverter;
 import org.ehrbase.fhirbridge.fhir.bundle.DiagnosticReportLabConverter;
 import org.ehrbase.fhirbridge.fhir.bundle.converter.BloodGasPanelConverter;
+import org.ehrbase.fhirbridge.fhir.bundle.validator.AntiBodyPanelBundleValidator;
 import org.ehrbase.fhirbridge.fhir.bundle.validator.BloodGasPanelBundleValidator;
 import org.ehrbase.fhirbridge.fhir.bundle.validator.DiagnosticReportLabBundleValidator;
 import org.ehrbase.fhirbridge.fhir.common.Profile;
@@ -28,9 +30,17 @@ public class BundleRoutes extends RouteBuilder {
                 .to("direct:process-blood-gas-panel-bundle")
                 .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.DIAGNOSTIC_REPORT_LAB))
                 .to("direct:process-diagnostic-report-lab-bundle")
+                .when(header(FhirBridgeConstants.PROFILE).isEqualTo(Profile.ANTI_BODY_PANEL))
+                .to("direct:process-anti-body-panel-bundle")
                 .otherwise()
                 .throwException(new UnprocessableEntityException("Unsupported transaction: provided Bundle should have a resource that " +
                         "uses on of the following profiles: " + Profile.BLOOD_GAS_PANEL.getUri() + ", " + Profile.DIAGNOSTIC_REPORT_LAB.getUri()));
+
+        from("direct:process-anti-body-panel-bundle")
+                .bean(AntiBodyPanelBundleValidator.class)
+                .bean(AntiBodyPanelConverter.class, "convert")
+                .to("direct:process-observation")
+                .process(bundleResponseProcessor);
 
         from("direct:process-blood-gas-panel-bundle")
                 .bean(BloodGasPanelBundleValidator.class)
