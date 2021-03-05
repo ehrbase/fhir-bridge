4bf1bb6791115a0c6f06740d9542b9d190a12c96
Renaud Subiger <renaud@subiger.com>
2021-03-05 10:56:55 +0100

    Add configuration for references and placeholders

--- src/main/java/org/ehrbase/fhirbridge/config/FhirJpaConfiguration.java
+++ src/main/java/org/ehrbase/fhirbridge/config/FhirJpaConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ehrbase.fhirbridge.config;
 
 import ca.uhn.fhir.jpa.api.config.DaoConfig;
@@ -43,7 +59,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 /**
- * {@link Configuration Configuration} for HAPI FHIR JPA server.
+ * {@link Configuration Configuration} for HAPI FHIR JPA Server module.
+ *
+ * @since 1.0.0
  */
 @Configuration
 @EnableConfigurationProperties(FhirJpaProperties.class)
@@ -63,7 +81,11 @@ public class FhirJpaConfiguration extends BaseR4Config {
 
     @Bean
     public DaoConfig daoConfig() {
-        return new DaoConfig();
+        DaoConfig daoConfig = new DaoConfig();
+        daoConfig.setAllowInlineMatchUrlReferences(fhirJpaProperties.isAllowInlineMatchUrlReferences());
+        daoConfig.setAutoCreatePlaceholderReferenceTargets(fhirJpaProperties.isAutoCreatePlaceholderReferences());
+        daoConfig.setPopulateIdentifierInAutoCreatedPlaceholderReferenceTargets(fhirJpaProperties.isPopulateIdentifierInAutoCreatedPlaceholderReferences());
+        return daoConfig;
     }
 
     @Bean
@@ -85,7 +107,11 @@ public class FhirJpaConfiguration extends BaseR4Config {
         entityManagerFactory.setPersistenceUnitName("HAPI_PU");
         entityManagerFactory.setDataSource(dataSource);
         entityManagerFactory.setJpaPropertyMap(jpaProperties.getProperties());
-        entityManagerFactory.setPackagesToScan("ca.uhn.fhir.jpa.model.entity", "ca.uhn.fhir.jpa.entity", "org.ehrbase.fhirbridge.fhir.support");
+        entityManagerFactory.setPackagesToScan(
+                "ca.uhn.fhir.jpa.model.entity",
+                "ca.uhn.fhir.jpa.entity",
+                "org.ehrbase.fhirbridge.fhir.support"
+        );
         return entityManagerFactory;
     }
 
