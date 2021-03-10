c74f97b202b5b1023ce6915cd8115e5054564f7e
Birgit Dell13Zoll <birgit.saalfeld@plri.de>
2021-03-10 11:48:20 +0100

    add test for invalid state

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/knownexposure/KnownExposureConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/knownexposure/KnownExposureConverter.java
@@ -1,5 +1,6 @@
 package org.ehrbase.fhirbridge.ehr.converter.knownexposure;
 
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import com.nedap.archie.rm.generic.PartySelf;
 import org.ehrbase.client.classgenerator.shareddefinition.Category;
 import org.ehrbase.client.classgenerator.shareddefinition.Language;
@@ -7,9 +8,8 @@ import org.ehrbase.client.classgenerator.shareddefinition.Setting;
 import org.ehrbase.client.classgenerator.shareddefinition.Territory;
 import org.ehrbase.fhirbridge.camel.component.ehr.composition.CompositionConversionException;
 import org.ehrbase.fhirbridge.camel.component.ehr.composition.CompositionConverter;
-import org.ehrbase.fhirbridge.ehr.converter.radiologischerBefund.BildgebendesUntersuchungsergebnisConverter;
-import org.ehrbase.fhirbridge.ehr.opt.geccoradiologischerbefundcomposition.GECCORadiologischerBefundComposition;
 import org.ehrbase.fhirbridge.ehr.opt.sarscov2expositioncomposition.SARSCoV2ExpositionComposition;
+import org.ehrbase.fhirbridge.ehr.opt.sarscov2expositioncomposition.definition.StatusDefiningCode;
 import org.hl7.fhir.r4.model.Observation;
 
 public class KnownExposureConverter  implements CompositionConverter<SARSCoV2ExpositionComposition, Observation> {
@@ -41,5 +41,20 @@ public class KnownExposureConverter  implements CompositionConverter<SARSCoV2Exp
         composition.setTerritory(Territory.DE);
         composition.setSettingDefiningCode(Setting.SECONDARY_MEDICAL_CARE);
         return (composition);
+    }
+
+    private void mapStatus(SARSCoV2ExpositionComposition composition, Observation obs) {
+        String status = obs.getStatusElement().getCode();
+        if (status.equals(StatusDefiningCode.FINAL.getValue())) {
+            composition.setStatusDefiningCode(StatusDefiningCode.FINAL);
+        } else if (status.equals(StatusDefiningCode.GEAENDERT.getValue())) {
+            composition.setStatusDefiningCode(StatusDefiningCode.GEAENDERT);
+        } else if (status.equals(StatusDefiningCode.REGISTRIERT.getValue())) {
+            composition.setStatusDefiningCode(StatusDefiningCode.REGISTRIERT);
+        } else if (status.equals(StatusDefiningCode.VORLAEUFIG.getValue())) {
+            composition.setStatusDefiningCode(StatusDefiningCode.VORLAEUFIG);
+        } else {
+            throw new UnprocessableEntityException("The status " + obs.getStatus().toString() + " is not valid for known exposure.");
+        }
     }
 }
