79efdb87118b751d2c4cbe4a5d439c167b06fbdc
SevKohler <kohlerseverin@posteo.de>
2021-03-16 15:43:42 +0100

    reflection added

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/generic/ObservationToObservationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/generic/ObservationToObservationConverter.java
@@ -4,11 +4,29 @@ import org.ehrbase.client.classgenerator.interfaces.EntryEntity;
 import org.hl7.fhir.r4.model.Observation;
 import org.springframework.lang.NonNull;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.temporal.TemporalAccessor;
+
 
 public abstract class ObservationToObservationConverter <E extends EntryEntity> extends EntryEntityConverter<Observation, E> {
+
+
     @Override
     public E convert(@NonNull Observation resource) {
-        //TODO setOrigin and  setTimeValue
-        return super.convert(resource);
+        E entryEntity = super.convert(resource);
+        invokeTimeValues(entryEntity, resource);
+        return entryEntity;
+    }
+
+    protected void invokeTimeValues(E entryEntity, Observation resource) {
+        try {
+            Method setOriginValue = entryEntity.getClass().getMethod("setOriginValue", TemporalAccessor.class);
+            Method setTimeValue = entryEntity.getClass().getMethod("setTimeValue", TemporalAccessor.class);
+            setOriginValue.invoke(entryEntity, resource.getEffectiveDateTimeType().getValueAsCalendar().toZonedDateTime());
+            setTimeValue.invoke(entryEntity, resource.getEffectiveDateTimeType().getValueAsCalendar().toZonedDateTime());
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException noSuchMethodException) {
+            noSuchMethodException.printStackTrace();
+        }
     }
 }
