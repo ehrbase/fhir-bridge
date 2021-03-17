05f5a3c0df490e7d84145596a1732ef0bef2ca13
Birgit Dell13Zoll <birgit.saalfeld@plri.de>
2021-03-17 14:33:42 +0100

    add optional for valueCodeableConcept

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/knownexposure/SarsCov2ExpositionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/knownexposure/SarsCov2ExpositionConverter.java
@@ -3,9 +3,13 @@ package org.ehrbase.fhirbridge.ehr.converter.knownexposure;
 import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import com.nedap.archie.rm.generic.PartySelf;
 import org.ehrbase.client.classgenerator.shareddefinition.Language;
+import org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition.definition.VorliegendeDiagnoseEvaluation;
 import org.ehrbase.fhirbridge.ehr.opt.sarscov2expositioncomposition.definition.ExpositionVorhandenDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.sarscov2expositioncomposition.definition.SarsCov2ExpositionEvaluation;
+import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Observation;
+
+import java.util.Optional;
 
 public class SarsCov2ExpositionConverter {
 
@@ -32,15 +36,25 @@ public class SarsCov2ExpositionConverter {
     }
 
     private void mapExpositionVorhanden(SarsCov2ExpositionEvaluation eval, Observation fhirObserv) {
+
+        //BSa valueCodeableConcept can be not existing -> how to deal with that?
+        Optional<CodeableConcept> valueCodeableConcept;
+        valueCodeableConcept = Optional.ofNullable(fhirObserv.getValueCodeableConcept());
+
+        if (valueCodeableConcept.isPresent()) {
+            return;
+        }
+
         String code = fhirObserv.getValueCodeableConcept().getCoding().get(0).getCode();
-        if(code.equals(KnownExposureCode.KNOWN_EXPOSURE.getCode())){
+
+        if (code.equals(KnownExposureCode.KNOWN_EXPOSURE.getCode())) {
             eval.setExpositionVorhandenDefiningCode(ExpositionVorhandenDefiningCode.EXPOSURE_TO_SEVERE_ACUTE_RESPIRATORY_SYNDROME_CORONAVIRUS2_EVENT);
-        }else if (code.equals(KnownExposureCode.NO_EXPOSURE.getCode())){
+        } else if (code.equals(KnownExposureCode.NO_EXPOSURE.getCode())) {
             eval.setExpositionVorhandenDefiningCode(ExpositionVorhandenDefiningCode.NO_QUALIFIER_VALUE);
-        }else if (code.equals(KnownExposureCode.UNKNOWN_EXPOSURE.getCode())){
+        } else if (code.equals(KnownExposureCode.UNKNOWN_EXPOSURE.getCode())) {
             eval.setExpositionVorhandenDefiningCode(ExpositionVorhandenDefiningCode.UNKNOWN);
         } else {
-            throw new UnprocessableEntityException("The SNOMED code "+code+" is not supported for known SarsCov2ExpositionConverter !");
+            throw new UnprocessableEntityException("The SNOMED code " + code + " is not supported for known SarsCov2ExpositionConverter !");
         }
     }
 
