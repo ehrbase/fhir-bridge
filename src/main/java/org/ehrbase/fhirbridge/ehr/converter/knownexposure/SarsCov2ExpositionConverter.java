91edc6002cdae7f79731226976a37b9256c046a1
Birgit Dell13Zoll <birgit.saalfeld@plri.de>
2021-03-17 10:40:08 +0100

    save before erik

---
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/knownexposure/SarsCov2ExpositionConverter.java
@@ -0,0 +1,62 @@
+package org.ehrbase.fhirbridge.ehr.converter.knownexposure;
+
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
+import com.nedap.archie.rm.generic.PartySelf;
+import org.ehrbase.client.classgenerator.shareddefinition.Language;
+import org.ehrbase.fhirbridge.ehr.opt.geccoradiologischerbefundcomposition.definition.BefundeDefiningCode;
+import org.ehrbase.fhirbridge.ehr.opt.geccoradiologischerbefundcomposition.definition.BildgebendesUntersuchungsergebnisObservation;
+import org.ehrbase.fhirbridge.ehr.opt.geccoradiologischerbefundcomposition.definition.NameDerUntersuchungDefiningCode;
+import org.ehrbase.fhirbridge.ehr.opt.sarscov2expositioncomposition.definition.ExpositionVorhandenDefiningCode;
+import org.ehrbase.fhirbridge.ehr.opt.sarscov2expositioncomposition.definition.SarsCov2ExpositionEvaluation;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.DiagnosticReport;
+import org.hl7.fhir.r4.model.Observation;
+
+import java.util.List;
+
+public class SarsCov2ExpositionConverter {
+    
+    public SarsCov2ExpositionEvaluation  map(Observation fhirObserv) {
+        SarsCov2ExpositionEvaluation eval = new SarsCov2ExpositionEvaluation();
+
+        //? Wieso habe ich kein Time hier? braucht evaluation das nicht?
+        eval.setLanguage(Language.DE);
+        eval.setSubject(new PartySelf());
+
+        mapInfektionserreger(eval, fhirObserv);
+        mapExpositionVorhanden(eval, fhirObserv);
+        mapSpezifischeDetailsZurExposition(eval, fhirObserv);
+
+        return eval;
+    }
+
+    private void mapSpezifischeDetailsZurExposition(SarsCov2ExpositionEvaluation eval, Observation fhirObserv) {
+        eval.setBeschreibungDerExpositionValue();
+        eval.setDatumUhrzeitDerExpositionValue();
+    }
+
+    private void mapInfektionserreger(SarsCov2ExpositionEvaluation eval, Observation fhirObserv) {
+        //? Woher kenne ich genau den Pfad in Fhir? Der ist so "generisch" beschrieben -> ich debugge und hole mir den code, aber wo ist string?
+        String code = fhirObserv.
+        if(coding.get(0).getCode().equals("???")){
+            eval.setInfektionserregerValue(String);
+        }else{
+            throw new UnprocessableEntityException("The Loinc code "+code()+" is not supported for SarsCov2ExpositionConverter !");
+        }
+    }
+
+    private void mapExpositionVorhanden(SarsCov2ExpositionEvaluation eval, Observation fhirObserv) {
+        String code = fhirObserv.getValueCodeableConcept().getCoding().get(0).getCode();
+        if(code.equals(KnownExposureCode.KNOWN_EXPOSURE.getCode())){
+            eval.setExpositionVorhandenDefiningCode(ExpositionVorhandenDefiningCode.EXPOSURE_TO_SEVERE_ACUTE_RESPIRATORY_SYNDROME_CORONAVIRUS2_EVENT);
+        }else if (code.equals(KnownExposureCode.NO_EXPOSURE.getCode())){
+            eval.setExpositionVorhandenDefiningCode(ExpositionVorhandenDefiningCode.NO_QUALIFIER_VALUE);
+        }else if (code.equals(KnownExposureCode.UNKNOWN_EXPOSURE.getCode())){
+            eval.setExpositionVorhandenDefiningCode(ExpositionVorhandenDefiningCode.UNKNOWN);
+        } else {
+            throw new UnprocessableEntityException("The SNOMED code "+code+" is not supported for known SarsCov2ExpositionConverter !");
+        }
+    }
+
+}
