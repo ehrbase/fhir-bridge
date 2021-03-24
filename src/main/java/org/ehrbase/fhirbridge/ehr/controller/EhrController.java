46f77306eaabf6f94042ba71eaf90185306dd2d0
serefarikan <serefarikan@gmail.com>
2021-03-24 14:43:59 +0000

    add error handling to Ehr controller and support for returning http 500 when exception occurs.

--- src/main/java/org/ehrbase/fhirbridge/ehr/controller/EhrController.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/controller/EhrController.java
@@ -16,6 +16,7 @@ import org.ehrbase.client.openehrclient.OpenEhrClientConfig;
 import org.ehrbase.client.openehrclient.defaultrestclient.DefaultRestClient;
 import org.ehrbase.fhirbridge.ehr.ResourceTemplateProvider;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -27,35 +28,41 @@ public class EhrController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public  String createEHR(@RequestParam String pPatientId) throws URISyntaxException {
-        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("myuser", "myPassword432"));
+    public ResponseEntity<String> createEHR(@RequestParam String pPatientId) throws URISyntaxException {
+        try {
+            CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("myuser", "myPassword432"));
 
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-            .setDefaultCredentialsProvider(credentialsProvider)
-            .build();
+            CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultCredentialsProvider(credentialsProvider)
+                .build();
 
-        DefaultRestClient client = new DefaultRestClient(
-            new OpenEhrClientConfig(new URI("http://localhost:8091/ehrbase/rest/openehr/v1/")),
-            new ResourceTemplateProvider("classpath:/opt/*.opt"),
-            httpClient);
+            DefaultRestClient client = new DefaultRestClient(
+                new OpenEhrClientConfig(new URI("http://localhost:8091/ehrbase/rest/openehr/v1/")),
+                new ResourceTemplateProvider("classpath:/opt/*.opt"),
+                httpClient);
 
-//        EhrStatus ehrStatus = new EhrStatus();
-//        ehrStatus.setSubject(new PartySelf(new PartyRef(new HierObjectId(pPatientId), "demographic", "PERSON")));
-//        ehrStatus.setArchetypeNodeId("openEHR-EHR-EHR_STATUS.generic.v1");
-//        ehrStatus.setName(new DvText("Integration tests status"));
-        PartySelf subject = new PartySelf();
-        PartyRef externalRef = new PartyRef();
-        externalRef.setType("PARTY_REF");
-        externalRef.setNamespace("patients");
-        GenericId genericId = new GenericId();
-        genericId.setScheme("id_scheme");
-        genericId.setValue(pPatientId);
-        externalRef.setId(genericId);
-        subject.setExternalRef(externalRef);
-        DvText dvText = new DvText("any EHR status");
-        EhrStatus ehrStatus = new EhrStatus("openEHR-EHR-ITEM_TREE.generic.v1", dvText, subject ,true, true, null);
-        
-        return client.ehrEndpoint().createEhr(ehrStatus).toString();
+            //taken from EhrBase tests
+            PartySelf subject = new PartySelf();
+            PartyRef externalRef = new PartyRef();
+            externalRef.setType("PARTY_REF");
+            externalRef.setNamespace("patients");
+            GenericId genericId = new GenericId();
+            genericId.setScheme("id_scheme");
+            genericId.setValue(pPatientId);
+            externalRef.setId(genericId);
+            subject.setExternalRef(externalRef);
+            DvText dvText = new DvText("any EHR status");
+            EhrStatus ehrStatus = new EhrStatus("openEHR-EHR-ITEM_TREE.generic.v1", dvText, subject, true, true, null);
+
+            return new ResponseEntity<>(
+                client.ehrEndpoint().createEhr(ehrStatus).toString(),
+                HttpStatus.OK
+            );
+        } catch (Exception ex) {
+            return new ResponseEntity<>(
+                "Error while creating an EHR for patient id " + pPatientId,
+                HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
