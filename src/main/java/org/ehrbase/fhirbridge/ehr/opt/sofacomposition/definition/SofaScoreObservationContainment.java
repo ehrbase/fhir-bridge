a425499c4c9f68edbd5a6febcbaad02deda7c521
SevKohler <kohlerseverin@posteo.de>
2021-04-01 12:09:11 +0200

    merged with develop

--- src/main/java/org/ehrbase/fhirbridge/ehr/opt/sofacomposition/definition/SofaScoreObservationContainment.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/sofacomposition/definition/SofaScoreObservationContainment.java
@@ -2,7 +2,6 @@ package org.ehrbase.fhirbridge.ehr.opt.sofacomposition.definition;
 
 import com.nedap.archie.rm.archetyped.FeederAudit;
 import com.nedap.archie.rm.datastructures.Cluster;
-import com.nedap.archie.rm.datastructures.ItemTree;
 import com.nedap.archie.rm.datavalues.quantity.DvOrdinal;
 import com.nedap.archie.rm.generic.PartyProxy;
 import java.lang.Long;
@@ -18,41 +17,39 @@ import org.ehrbase.client.classgenerator.shareddefinition.NullFlavour;
 public class SofaScoreObservationContainment extends Containment {
   public SelectAqlField<SofaScoreObservation> SOFA_SCORE_OBSERVATION = new AqlFieldImp<SofaScoreObservation>(SofaScoreObservation.class, "", "SofaScoreObservation", SofaScoreObservation.class, this);
 
-  public SelectAqlField<DvOrdinal> ATEMTAETIGKEIT = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value", "atemtaetigkeit", DvOrdinal.class, this);
+  public SelectAqlField<DvOrdinal> RESPIRATION = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value", "respiration", DvOrdinal.class, this);
 
-  public SelectAqlField<NullFlavour> ATEMTAETIGKEIT_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0004]/null_flavour|defining_code", "atemtaetigkeitNullFlavourDefiningCode", NullFlavour.class, this);
+  public SelectAqlField<NullFlavour> RESPIRATION_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0004]/null_flavour|defining_code", "respirationNullFlavourDefiningCode", NullFlavour.class, this);
 
-  public SelectAqlField<DvOrdinal> ZENTRALES_NERVENSYSTEM = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0005]/value", "zentralesNervensystem", DvOrdinal.class, this);
+  public SelectAqlField<DvOrdinal> KARDIOVASKULAERES_SYSTEM = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0010]/value", "kardiovaskulaeresSystem", DvOrdinal.class, this);
 
-  public SelectAqlField<NullFlavour> ZENTRALES_NERVENSYSTEM_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0005]/null_flavour|defining_code", "zentralesNervensystemNullFlavourDefiningCode", NullFlavour.class, this);
+  public SelectAqlField<NullFlavour> KARDIOVASKULAERES_SYSTEM_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0010]/null_flavour|defining_code", "kardiovaskulaeresSystemNullFlavourDefiningCode", NullFlavour.class, this);
 
-  public SelectAqlField<DvOrdinal> HERZ_KREISLAUF_SYSTEM = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0006]/value", "herzKreislaufSystem", DvOrdinal.class, this);
+  public SelectAqlField<DvOrdinal> ZENTRALES_NERVENSYSTEM = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0016]/value", "zentralesNervensystem", DvOrdinal.class, this);
 
-  public SelectAqlField<NullFlavour> HERZ_KREISLAUF_SYSTEM_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0006]/null_flavour|defining_code", "herzKreislaufSystemNullFlavourDefiningCode", NullFlavour.class, this);
+  public SelectAqlField<NullFlavour> ZENTRALES_NERVENSYSTEM_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0016]/null_flavour|defining_code", "zentralesNervensystemNullFlavourDefiningCode", NullFlavour.class, this);
 
-  public SelectAqlField<DvOrdinal> LEBERFUNKTION = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0007]/value", "leberfunktion", DvOrdinal.class, this);
+  public SelectAqlField<DvOrdinal> NIERENFUNKTION = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0022]/value", "nierenfunktion", DvOrdinal.class, this);
 
-  public SelectAqlField<NullFlavour> LEBERFUNKTION_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0007]/null_flavour|defining_code", "leberfunktionNullFlavourDefiningCode", NullFlavour.class, this);
+  public SelectAqlField<NullFlavour> NIERENFUNKTION_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0022]/null_flavour|defining_code", "nierenfunktionNullFlavourDefiningCode", NullFlavour.class, this);
 
-  public SelectAqlField<DvOrdinal> BLUTGERINNUNG = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0008]/value", "blutgerinnung", DvOrdinal.class, this);
+  public SelectAqlField<DvOrdinal> LEBERFUNKTION = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0028]/value", "leberfunktion", DvOrdinal.class, this);
 
-  public SelectAqlField<NullFlavour> BLUTGERINNUNG_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0008]/null_flavour|defining_code", "blutgerinnungNullFlavourDefiningCode", NullFlavour.class, this);
+  public SelectAqlField<NullFlavour> LEBERFUNKTION_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0028]/null_flavour|defining_code", "leberfunktionNullFlavourDefiningCode", NullFlavour.class, this);
 
-  public SelectAqlField<DvOrdinal> NIERENFUNKTION = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0009]/value", "nierenfunktion", DvOrdinal.class, this);
+  public SelectAqlField<DvOrdinal> BLUTGERINNUNG = new AqlFieldImp<DvOrdinal>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0034]/value", "blutgerinnung", DvOrdinal.class, this);
 
-  public SelectAqlField<NullFlavour> NIERENFUNKTION_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0009]/null_flavour|defining_code", "nierenfunktionNullFlavourDefiningCode", NullFlavour.class, this);
+  public SelectAqlField<NullFlavour> BLUTGERINNUNG_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0034]/null_flavour|defining_code", "blutgerinnungNullFlavourDefiningCode", NullFlavour.class, this);
 
-  public SelectAqlField<Long> SOFA_SCORE_MAGNITUDE = new AqlFieldImp<Long>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0010]/value|magnitude", "sofaScoreMagnitude", Long.class, this);
+  public SelectAqlField<Long> GESAMTERGEBNIS_MAGNITUDE = new AqlFieldImp<Long>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0041]/value|magnitude", "gesamtergebnisMagnitude", Long.class, this);
 
-  public SelectAqlField<NullFlavour> SOFA_SCORE_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0010]/null_flavour|defining_code", "sofaScoreNullFlavourDefiningCode", NullFlavour.class, this);
-
-  public SelectAqlField<ItemTree> BAUM = new AqlFieldImp<ItemTree>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/state[at0013]", "baum", ItemTree.class, this);
+  public SelectAqlField<NullFlavour> GESAMTERGEBNIS_NULL_FLAVOUR_DEFINING_CODE = new AqlFieldImp<NullFlavour>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/data[at0003]/items[at0041]/null_flavour|defining_code", "gesamtergebnisNullFlavourDefiningCode", NullFlavour.class, this);
 
   public SelectAqlField<TemporalAccessor> TIME_VALUE = new AqlFieldImp<TemporalAccessor>(SofaScoreObservation.class, "/data[at0001]/events[at0002]/time|value", "timeValue", TemporalAccessor.class, this);
 
   public SelectAqlField<TemporalAccessor> ORIGIN_VALUE = new AqlFieldImp<TemporalAccessor>(SofaScoreObservation.class, "/data[at0001]/origin|value", "originValue", TemporalAccessor.class, this);
 
-  public ListSelectAqlField<Cluster> ERWEITERUNG = new ListAqlFieldImp<Cluster>(SofaScoreObservation.class, "/protocol[at0014]/items[at0015]", "erweiterung", Cluster.class, this);
+  public ListSelectAqlField<Cluster> ERWEITERUNG = new ListAqlFieldImp<Cluster>(SofaScoreObservation.class, "/protocol[at0043]/items[at0044]", "erweiterung", Cluster.class, this);
 
   public SelectAqlField<PartyProxy> SUBJECT = new AqlFieldImp<PartyProxy>(SofaScoreObservation.class, "/subject", "subject", PartyProxy.class, this);
 
