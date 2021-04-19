e229138db5a16e3fed241e2461d2445c3c8232aa
Erik Tute <eriktute@mx.de>
2021-04-19 12:46:56 +0200

    Just pushing current state - I know it's not working...

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/generic/ConsentToCompositionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/generic/ConsentToCompositionConverter.java
@@ -2,20 +2,16 @@ package org.ehrbase.fhirbridge.ehr.converter.generic;
 
 import org.ehrbase.client.classgenerator.interfaces.CompositionEntity;
 import org.hl7.fhir.r4.model.Consent;
-import org.hl7.fhir.r4.model.DateTimeType;
-import org.hl7.fhir.r4.model.Extension;
-import org.hl7.fhir.r4.model.Patient;
 import org.springframework.lang.NonNull;
 
 import java.time.Instant;
-import java.time.ZonedDateTime;
 
 public abstract class ConsentToCompositionConverter<C extends CompositionEntity> extends CompositionConverter<Consent, C> {
 
     @Override
     public C convert(@NonNull Consent resource) {
         C composition = super.convert(resource);
-        composition.setStartTimeValue(Instant.now());
+        //composition.setStartTimeValue(Instant.now());
         return composition;
     }
 }
