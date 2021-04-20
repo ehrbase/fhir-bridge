ddaafa48588b11826c88202d6c863921236e8d37
Renaud Subiger <renaud@subiger.com>
2021-04-20 18:30:08 +0200

    Minor changes

--- src/main/java/org/ehrbase/fhirbridge/ehr/web/EhrController.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/web/EhrController.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.ehrbase.fhirbridge.ehr.web;
 
 import com.nedap.archie.rm.datavalues.DvText;
