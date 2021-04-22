416ec4e06568bbec47496da4d6cfef5343879bf0
uakoenig <urs.a.koenig@t-online.de>
2021-04-22 22:09:45 +0200

    Mapping mit createPatientDischarge-Fehler

---
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/patientdischarge/PatientDischargeCompositionConverter.java
@@ -0,0 +1,40 @@
+package org.ehrbase.fhirbridge.ehr.converter.specific.patientdischarge;
+
+import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
+import org.ehrbase.fhirbridge.ehr.converter.generic.ObservationToCompositionConverter;
+import org.ehrbase.fhirbridge.ehr.opt.geccoentlassungsdatencomposition.GECCOEntlassungsdatenComposition;
+import org.ehrbase.fhirbridge.ehr.opt.geccoentlassungsdatencomposition.definition.StatusDefiningCode;
+import org.hl7.fhir.r4.model.Observation;
+import org.springframework.lang.NonNull;
+
+import static org.ehrbase.fhirbridge.ehr.converter.specific.CodeSystem.SNOMED;
+
+public class PatientDischargeCompositionConverter extends ObservationToCompositionConverter<GECCOEntlassungsdatenComposition> {
+
+    @Override
+    public GECCOEntlassungsdatenComposition convertInternal(@NonNull Observation resource) {
+
+        GECCOEntlassungsdatenComposition composition = new GECCOEntlassungsdatenComposition();
+
+        mapStatus(composition, resource);
+
+        composition.setEntlassungsart(new PatientDischargeAdminEntryConverter().convertInternal(resource));
+
+        return composition;
+    }
+
+    private void mapStatus(GECCOEntlassungsdatenComposition composition, Observation obs) {
+        String status = obs.getStatusElement().getCode();
+        if (status.equals(StatusDefiningCode.FINAL.getValue())) {
+            composition.setStatusDefiningCode(StatusDefiningCode.FINAL);
+        } else if (status.equals(StatusDefiningCode.GEAENDERT.getValue())) {
+            composition.setStatusDefiningCode(StatusDefiningCode.GEAENDERT);
+        } else if (status.equals(StatusDefiningCode.REGISTRIERT.getValue())) {
+            composition.setStatusDefiningCode(StatusDefiningCode.REGISTRIERT);
+        } else if (status.equals(StatusDefiningCode.VORLAEUFIG.getValue())) {
+            composition.setStatusDefiningCode(StatusDefiningCode.VORLAEUFIG);
+        } else {
+            throw new ConversionException("The status " + obs.getStatus().toString() + " is not valid for known exposure.");
+        }
+    }
+}
---
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/geccoentlassungsdatencomposition/definition/EntlassungsartAdminEntry.java
@@ -0,0 +1,126 @@
+package org.ehrbase.fhirbridge.ehr.opt.geccoentlassungsdatencomposition.definition;
+
+import com.nedap.archie.rm.archetyped.FeederAudit;
+import com.nedap.archie.rm.datastructures.Cluster;
+import com.nedap.archie.rm.datavalues.DvCodedText;
+import com.nedap.archie.rm.generic.PartyProxy;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.ehrbase.client.annotations.Archetype;
+import org.ehrbase.client.annotations.Entity;
+import org.ehrbase.client.annotations.Path;
+import org.ehrbase.client.classgenerator.interfaces.EntryEntity;
+import org.ehrbase.client.classgenerator.shareddefinition.Language;
+import org.ehrbase.client.classgenerator.shareddefinition.NullFlavour;
+//import org.ehrbase.validation.terminology.validator.DvCodedText;
+
+@Entity
+@Archetype("openEHR-EHR-ADMIN_ENTRY.discharge_summary.v0")
+@Generated(
+    value = "org.ehrbase.client.classgenerator.ClassGenerator",
+    date = "2021-04-22T10:43:11.563378400+02:00",
+    comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.4.0"
+)
+public class EntlassungsartAdminEntry implements EntryEntity {
+  /**
+   * Path: Entlassungsdaten/Entlassungsart/Art der Entlassung
+   * Description: Grund der Entlassung
+   */
+  @Path("/data[at0001]/items[at0040]/value")
+  private DvCodedText artDerEntlassung;
+
+  /**
+   * Path: Entlassungsdaten/Entlassungsart/Tree/Art der Entlassung/null_flavour
+   */
+  @Path("/data[at0001]/items[at0040]/null_flavour|defining_code")
+  private NullFlavour artDerEntlassungNullFlavourDefiningCode;
+
+  /**
+   * Path: Entlassungsdaten/Entlassungsart/Letzter Patientenstandort
+   * Description: *
+   */
+  @Path("/data[at0001]/items[at0066]")
+  private List<Cluster> letzterPatientenstandort;
+
+  /**
+   * Path: Entlassungsdaten/Entlassungsart/Zugewiesener Patientenstandort
+   * Description: Für die lokale Verwendung enthält dieses Feld den Typ der Organisationseinheit oder der klinischen Einheit.
+   */
+  @Path("/data[at0001]/items[at0067]")
+  private List<Cluster> zugewiesenerPatientenstandort;
+
+  /**
+   * Path: Entlassungsdaten/Entlassungsart/subject
+   */
+  @Path("/subject")
+  private PartyProxy subject;
+
+  /**
+   * Path: Entlassungsdaten/Entlassungsart/language
+   */
+  @Path("/language")
+  private Language language;
+
+  /**
+   * Path: Entlassungsdaten/Entlassungsart/feeder_audit
+   */
+  @Path("/feeder_audit")
+  private FeederAudit feederAudit;
+
+  public void setArtDerEntlassung(DvCodedText artDerEntlassung) {
+     this.artDerEntlassung = artDerEntlassung;
+  }
+
+  public DvCodedText getArtDerEntlassung() {
+     return this.artDerEntlassung ;
+  }
+
+  public void setArtDerEntlassungNullFlavourDefiningCode(
+      NullFlavour artDerEntlassungNullFlavourDefiningCode) {
+     this.artDerEntlassungNullFlavourDefiningCode = artDerEntlassungNullFlavourDefiningCode;
+  }
+
+  public NullFlavour getArtDerEntlassungNullFlavourDefiningCode() {
+     return this.artDerEntlassungNullFlavourDefiningCode ;
+  }
+
+  public void setLetzterPatientenstandort(List<Cluster> letzterPatientenstandort) {
+     this.letzterPatientenstandort = letzterPatientenstandort;
+  }
+
+  public List<Cluster> getLetzterPatientenstandort() {
+     return this.letzterPatientenstandort ;
+  }
+
+  public void setZugewiesenerPatientenstandort(List<Cluster> zugewiesenerPatientenstandort) {
+     this.zugewiesenerPatientenstandort = zugewiesenerPatientenstandort;
+  }
+
+  public List<Cluster> getZugewiesenerPatientenstandort() {
+     return this.zugewiesenerPatientenstandort ;
+  }
+
+  public void setSubject(PartyProxy subject) {
+     this.subject = subject;
+  }
+
+  public PartyProxy getSubject() {
+     return this.subject ;
+  }
+
+  public void setLanguage(Language language) {
+     this.language = language;
+  }
+
+  public Language getLanguage() {
+     return this.language ;
+  }
+
+  public void setFeederAudit(FeederAudit feederAudit) {
+     this.feederAudit = feederAudit;
+  }
+
+  public FeederAudit getFeederAudit() {
+     return this.feederAudit ;
+  }
+}
