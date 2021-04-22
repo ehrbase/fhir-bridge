a4e31d87bb6421383c8116f8b48624c2cff7bf30
SevKohler <kohlerseverin@posteo.de>
2021-04-22 15:47:12 +0200

    fixed validator

--- src/main/java/org/ehrbase/fhirbridge/fhir/bundle/validator/AntiBodyPanelBundleValidator.java
+++ src/main/java/org/ehrbase/fhirbridge/fhir/bundle/validator/AntiBodyPanelBundleValidator.java
@@ -1,17 +1,10 @@
 package org.ehrbase.fhirbridge.fhir.bundle.validator;
 
-import org.hl7.fhir.r4.model.Bundle;
-
 import java.util.Map;
 
-public class AntiBodyPanelBundleValidator extends AbstractBundleValidator{
-    
+public class AntiBodyPanelBundleValidator extends AbstractBundleValidator {
     @Override
     public void validateRequest(Object bundle, Map<String, Object> parameters) {
         super.validateRequest(bundle, parameters);
-        validateAntiBodyPanel((Bundle) bundle);
-    }
-
-    private void validateAntiBodyPanel(Bundle bundle) {
     }
 }
