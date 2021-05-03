3e406ce88fb68bb809f054dbf95f500722a8b5ca
SevKohler <kohlerseverin@posteo.de>
2021-05-03 11:59:14 +0200

    finished mapping

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/medication/GeccoMedikationObservationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/medication/GeccoMedikationObservationConverter.java
@@ -22,7 +22,6 @@ public abstract class GeccoMedikationObservationConverter<E extends EntryEntity>
     public E convert(@NonNull MedicationStatement resource) {
         E entryEntity = super.convert(resource);
         invokeStatus(entryEntity, resource);
-        invokeGrund(entryEntity, resource);
         return entryEntity;
     }
 
@@ -41,69 +40,25 @@ public abstract class GeccoMedikationObservationConverter<E extends EntryEntity>
     protected StatusCluster mapStatus(MedicationStatement resource) {
         StatusCluster statusCluster = new StatusCluster();
         String fhirStatus = resource.getStatusElement().getCode();
-        if (fhirStatus.equals(StatusDefiningCode2.ABGESCHLOSSEN.getValue())) {
+        if (fhirStatus.equals(StatusDefiningCode2.ABGESCHLOSSEN.getValue()) || fhirStatus.equals("completed")) {
             statusCluster.setStatusDefiningCode(StatusDefiningCode2.ABGESCHLOSSEN);
-        } else if (fhirStatus.equals(StatusDefiningCode2.AKTIV.getValue())) {
+        } else if (fhirStatus.equals(StatusDefiningCode2.AKTIV.getValue()) || fhirStatus.equals("active")) {
             statusCluster.setStatusDefiningCode(StatusDefiningCode2.AKTIV);
-        } else if (fhirStatus.equals(StatusDefiningCode2.ANGEHALTEN.getValue())) {
+        } else if (fhirStatus.equals(StatusDefiningCode2.ANGEHALTEN.getValue()) || fhirStatus.equals("on-hold")) {
             statusCluster.setStatusDefiningCode(StatusDefiningCode2.ANGEHALTEN);
-        } else if (fhirStatus.equals(StatusDefiningCode2.BEABSICHTIGT.getValue())) {
+        } else if (fhirStatus.equals(StatusDefiningCode2.BEABSICHTIGT.getValue()) || fhirStatus.equals("intended")) {
             statusCluster.setStatusDefiningCode(StatusDefiningCode2.BEABSICHTIGT);
-        } else if (fhirStatus.equals(StatusDefiningCode2.FEHLERHAFTE_ANWENDUNG.getValue())) {
+        } else if (fhirStatus.equals(StatusDefiningCode2.FEHLERHAFTE_ANWENDUNG.getValue()) || fhirStatus.equals("entered-in-error")) {
             statusCluster.setStatusDefiningCode(StatusDefiningCode2.FEHLERHAFTE_ANWENDUNG);
-        } else if (fhirStatus.equals(StatusDefiningCode2.GESTOPPT.getValue())) {
+        } else if (fhirStatus.equals(StatusDefiningCode2.GESTOPPT.getValue()) || fhirStatus.equals("stopped")) {
             statusCluster.setStatusDefiningCode(StatusDefiningCode2.GESTOPPT);
-        } else if (fhirStatus.equals(StatusDefiningCode2.NICHT_GENOMMEN.getValue())) {
+        } else if (fhirStatus.equals(StatusDefiningCode2.NICHT_GENOMMEN.getValue()) || fhirStatus.equals("not-taken")) {
             statusCluster.setStatusDefiningCode(StatusDefiningCode2.NICHT_GENOMMEN);
-        } else if (fhirStatus.equals(StatusDefiningCode2.UNBEKANNT.getValue())) {
+        } else if (fhirStatus.equals(StatusDefiningCode2.UNBEKANNT.getValue()) || fhirStatus.equals("unknown")) {
             statusCluster.setStatusDefiningCode(StatusDefiningCode2.UNBEKANNT);
         } else {
             throw new UnprocessableEntityException("Invalid Status code " + fhirStatus);
         }
         return statusCluster;
     }
-
-
-    public void invokeGrund(E eventEntity, MedicationStatement resource) {
-        try {
-            Method setGrundDefiningCode = eventEntity.getClass().getMethod("setGrundDefiningCode", GrundDefiningCode.class);
-            if(getGrundDefiningCode(resource).isPresent()){
-                setGrundDefiningCode.invoke(eventEntity, getGrundDefiningCode(resource).get());
-            }
-        } catch (IllegalAccessException | InvocationTargetException exception) {
-            exception.printStackTrace();
-        } catch (NoSuchMethodException ignored) {
-            //ignored
-        }
-    }
-
-    protected Optional<GrundDefiningCode> getGrundDefiningCode(MedicationStatement resource) {
-        for (Coding coding : resource.getMedicationCodeableConcept().getCoding()) {
-            if (coding.hasSystem() && coding.getSystem().equals(CodeSystem.SNOMED.getUrl())) {
-                return mapGrundDefiningCode(coding);
-            }
-        }
-        return Optional.empty();
-    }
-
-    protected Optional<GrundDefiningCode> mapGrundDefiningCode(Coding coding) {
-        String snomedCode = coding.getCode();
-        if (snomedCode.equals(GrundDefiningCode.ADJUNCT_INTENT_QUALIFIER_VALUE.getCode())) {
-            return Optional.of(GrundDefiningCode.ADJUNCT_INTENT_QUALIFIER_VALUE);
-        } else if (snomedCode.equals(GrundDefiningCode.ADJUVANT_INTENT_QUALIFIER_VALUE.getCode())) {
-            return Optional.of(GrundDefiningCode.ADJUVANT_INTENT_QUALIFIER_VALUE);
-        } else if (snomedCode.equals(GrundDefiningCode.CURATIVE_PROCEDURE_INTENT_QUALIFIER_VALUE.getCode())) {
-            return Optional.of(GrundDefiningCode.CURATIVE_PROCEDURE_INTENT_QUALIFIER_VALUE);
-        } else if (snomedCode.equals(GrundDefiningCode.NEO_ADJUVANT_INTENT_QUALIFIER_VALUE.getCode())) {
-            return Optional.of(GrundDefiningCode.NEO_ADJUVANT_INTENT_QUALIFIER_VALUE);
-        } else if (snomedCode.equals(GrundDefiningCode.PROPHYLAXIS_PROCEDURE_INTENT_QUALIFIER_VALUE.getCode())) {
-            return Optional.of(GrundDefiningCode.PROPHYLAXIS_PROCEDURE_INTENT_QUALIFIER_VALUE);
-        } else if (snomedCode.equals(GrundDefiningCode.SUPPORTIVE_PROCEDURE_INTENT_QUALIFIER_VALUE.getCode())) {
-            return Optional.of(GrundDefiningCode.SUPPORTIVE_PROCEDURE_INTENT_QUALIFIER_VALUE);
-        } else {
-            throw new UnprocessableEntityException("Invalid Status code " + snomedCode);
-        }
-    }
-
-
 }
--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/medication/observations/AntikoagulanzienObservationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/medication/observations/AntikoagulanzienObservationConverter.java
@@ -5,10 +5,12 @@ import org.ehrbase.fhirbridge.ehr.converter.specific.CodeSystem;
 import org.ehrbase.fhirbridge.ehr.converter.specific.medication.GeccoMedikationObservationConverter;
 import org.ehrbase.fhirbridge.ehr.opt.geccomedikationcomposition.definition.AntikoagulanzienObservation;
 import org.ehrbase.fhirbridge.ehr.opt.geccomedikationcomposition.definition.ArzneimittelNameDefiningCode4;
+import org.ehrbase.fhirbridge.ehr.opt.geccomedikationcomposition.definition.GrundDefiningCode;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.MedicationStatement;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class AntikoagulanzienObservationConverter extends GeccoMedikationObservationConverter<AntikoagulanzienObservation> {
 
@@ -16,6 +18,7 @@ public class AntikoagulanzienObservationConverter extends GeccoMedikationObserva
     protected AntikoagulanzienObservation convertInternal(MedicationStatement resource) {
         AntikoagulanzienObservation antikoagulanzienObservation = new AntikoagulanzienObservation();
         antikoagulanzienObservation.setArzneimittelNameDefiningCode(getArzneimittelName(resource));
+        getGrundDefiningCode(resource).ifPresent(antikoagulanzienObservation::setGrundDefiningCode);
         return antikoagulanzienObservation;
     }
 
@@ -34,5 +37,33 @@ public class AntikoagulanzienObservationConverter extends GeccoMedikationObserva
             return arzneimittelNameDefiningCodeMap.get(coding.getCode());
         }
         throw new UnprocessableEntityException("Invalid Arzneimittel code  " + coding.getCode());
+    }
+
+    protected Optional<GrundDefiningCode> getGrundDefiningCode(MedicationStatement resource) {
+        for (Coding coding : resource.getReasonCode().get(0).getCoding()) {
+            if (coding.hasSystem() && coding.getSystem().equals(CodeSystem.SNOMED.getUrl())) {
+                return mapGrundDefiningCode(coding);
+            }
+        }
+        return Optional.empty();
+    }
+
+    protected Optional<GrundDefiningCode> mapGrundDefiningCode(Coding coding) {
+        String snomedCode = coding.getCode();
+        if (snomedCode.equals(GrundDefiningCode.ADJUNCT_INTENT_QUALIFIER_VALUE.getCode())) {
+            return Optional.of(GrundDefiningCode.ADJUNCT_INTENT_QUALIFIER_VALUE);
+        } else if (snomedCode.equals(GrundDefiningCode.ADJUVANT_INTENT_QUALIFIER_VALUE.getCode())) {
+            return Optional.of(GrundDefiningCode.ADJUVANT_INTENT_QUALIFIER_VALUE);
+        } else if (snomedCode.equals(GrundDefiningCode.CURATIVE_PROCEDURE_INTENT_QUALIFIER_VALUE.getCode())) {
+            return Optional.of(GrundDefiningCode.CURATIVE_PROCEDURE_INTENT_QUALIFIER_VALUE);
+        } else if (snomedCode.equals(GrundDefiningCode.NEO_ADJUVANT_INTENT_QUALIFIER_VALUE.getCode())) {
+            return Optional.of(GrundDefiningCode.NEO_ADJUVANT_INTENT_QUALIFIER_VALUE);
+        } else if (snomedCode.equals(GrundDefiningCode.PROPHYLAXIS_PROCEDURE_INTENT_QUALIFIER_VALUE.getCode())) {
+            return Optional.of(GrundDefiningCode.PROPHYLAXIS_PROCEDURE_INTENT_QUALIFIER_VALUE);
+        } else if (snomedCode.equals(GrundDefiningCode.SUPPORTIVE_PROCEDURE_INTENT_QUALIFIER_VALUE.getCode())) {
+            return Optional.of(GrundDefiningCode.SUPPORTIVE_PROCEDURE_INTENT_QUALIFIER_VALUE);
+        } else {
+          throw new UnprocessableEntityException("The reasonCode "+snomedCode+" is invalid !");
+        }
     }
 }
