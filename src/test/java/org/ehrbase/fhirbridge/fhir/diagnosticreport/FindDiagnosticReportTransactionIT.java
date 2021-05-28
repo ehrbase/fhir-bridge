30cd6fabccc00eab385814b3dd93f2cfc0b2b55a
Renaud Subiger <renaud@subiger.com>
2021-05-28 11:48:29 +0200

    Add integration tests for DiagnosticReport and update Postman collection

---
+++ src/test/java/org/ehrbase/fhirbridge/fhir/diagnosticreport/FindDiagnosticReportTransactionIT.java
@@ -0,0 +1,57 @@
+package org.ehrbase.fhirbridge.fhir.diagnosticreport;
+
+import org.ehrbase.fhirbridge.fhir.AbstractTransactionIT;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.DiagnosticReport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+/**
+ * Integration Tests that validate "Find DiagnosticReport" transaction.
+ */
+class FindDiagnosticReportTransactionIT extends AbstractTransactionIT {
+
+    @Test
+    void findDiagnosticReportRead() throws Exception {
+        var outcome = create("DiagnosticReport/transactions/provide-diagnostic-report-create.json");
+        var id = outcome.getId();
+
+        var diagnosticReport = read(id.getIdPart(), DiagnosticReport.class);
+
+        Assertions.assertNotNull(diagnosticReport);
+        Assertions.assertNotNull(diagnosticReport.getId(), id.getIdPart());
+        Assertions.assertEquals(PATIENT_ID, diagnosticReport.getSubject().getIdentifier().getValue());
+    }
+
+    @Test
+    void findDiagnosticReportVRead() throws Exception {
+        var outcome = create("DiagnosticReport/transactions/provide-diagnostic-report-create.json");
+        var id = outcome.getId();
+
+        var diagnosticReport = vread(id.getIdPart(), id.getVersionIdPart(), DiagnosticReport.class);
+        Assertions.assertNotNull(diagnosticReport);
+        Assertions.assertNotNull(diagnosticReport.getId(), id.getIdPart());
+        Assertions.assertNotNull(diagnosticReport.getMeta().getVersionId(), id.getVersionIdPart());
+        Assertions.assertEquals(PATIENT_ID, diagnosticReport.getSubject().getIdentifier().getValue());
+    }
+
+    @Test
+    void findDiagnosticReportSearch() throws IOException {
+        create("DiagnosticReport/transactions/find-diagnostic-report-search.json");
+        create("DiagnosticReport/transactions/find-diagnostic-report-search.json");
+        create("DiagnosticReport/transactions/find-diagnostic-report-search.json");
+
+        Bundle bundle = search("DiagnosticReport?subject.identifier=" + PATIENT_ID + "&status=registered");
+
+        Assertions.assertEquals(3, bundle.getTotal());
+
+        bundle.getEntry().forEach(entry -> {
+            var diagnosticReport = (DiagnosticReport) entry.getResource();
+
+            Assertions.assertEquals(PATIENT_ID, diagnosticReport.getSubject().getIdentifier().getValue());
+            Assertions.assertEquals(DiagnosticReport.DiagnosticReportStatus.REGISTERED, diagnosticReport.getStatus());
+        });
+    }
+}
---
+++ src/test/java/org/ehrbase/fhirbridge/fhir/diagnosticreport/ProvideDiagnosticReportTransactionIT.java
@@ -0,0 +1,40 @@
+package org.ehrbase.fhirbridge.fhir.diagnosticreport;
+
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import org.ehrbase.fhirbridge.fhir.AbstractTransactionIT;
+import org.hl7.fhir.r4.model.DiagnosticReport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Integration Tests that validate "Provide DiagnosticReport" transaction.
+ */
+class ProvideDiagnosticReportTransactionIT extends AbstractTransactionIT {
+
+    @Test
+    void provideDiagnosticReportCreate() throws Exception {
+        var outcome = create("DiagnosticReport/transactions/provide-diagnostic-report-create.json");
+
+        Assertions.assertEquals(true, outcome.getCreated());
+        Assertions.assertNotNull(outcome.getId().getValue());
+    }
+
+    @Test
+    void provideDiagnosticReportConditionalUpdate() throws Exception {
+        MethodOutcome outcome;
+
+        outcome = create("DiagnosticReport/transactions/provide-diagnostic-report-create.json");
+        var id = outcome.getId();
+
+        outcome = update("DiagnosticReport/transactions/provide-diagnostic-report-update.json", "DiagnosticReport?_id=" + id.getIdPart() + "&subject.identifier=" + PATIENT_ID);
+
+        Assertions.assertEquals(id.getIdPart(), outcome.getId().getIdPart());
+        Assertions.assertEquals(id.getVersionIdPartAsLong() + 1, outcome.getId().getVersionIdPartAsLong());
+
+        var diagnosticReport = (DiagnosticReport) outcome.getResource();
+
+        Assertions.assertEquals(PATIENT_ID, diagnosticReport.getSubject().getIdentifier().getValue());
+        Assertions.assertEquals("http://external.fhir.server/ServiceRequest/987", diagnosticReport.getBasedOn().get(0).getReference());
+        Assertions.assertEquals(DiagnosticReport.DiagnosticReportStatus.CORRECTED, diagnosticReport.getStatus());
+    }
+}
