8cd1b6fa13972ab5f79a9210a21d08ad2de1e3e2
Renaud Subiger <renaud@subiger.com>
2021-05-28 10:26:25 +0200

    Add integration tests for Consent and update Postman collection

---
+++ src/test/java/org/ehrbase/fhirbridge/fhir/consent/ProvideConsentTransactionIT.java
@@ -0,0 +1,44 @@
+package org.ehrbase.fhirbridge.fhir.consent;
+
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import org.ehrbase.fhirbridge.fhir.AbstractTransactionIT;
+import org.hl7.fhir.r4.model.Consent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Integration Tests that validate "Provide Consent" transaction.
+ */
+class ProvideConsentTransactionIT extends AbstractTransactionIT {
+
+    @Disabled("Converter not yet implemented")
+    @Test
+    void provideConsentCreate() throws Exception {
+        var outcome = create("Consent/transactions/provide-consent-create.json");
+
+        Assertions.assertEquals(true, outcome.getCreated());
+        Assertions.assertNotNull(outcome.getId().getValue());
+    }
+
+    @Disabled("Converter not yet implemented")
+    @Test
+    void provideConsentConditionalUpdate() throws Exception {
+        MethodOutcome outcome;
+
+        outcome = create("Consent/transactions/provide-consent-create.json");
+        var id = outcome.getId();
+
+        outcome = update("Consent/transactions/provide-consent-update.json", "Consent?_id=" + id.getIdPart() + "&patient.identifier=" + PATIENT_ID);
+
+        Assertions.assertEquals(id.getIdPart(), outcome.getId().getIdPart());
+        Assertions.assertEquals(id.getVersionIdPartAsLong() + 1, outcome.getId().getVersionIdPartAsLong());
+
+        var consent = (Consent) outcome.getResource();
+
+        var scope = consent.getScope();
+        Assertions.assertEquals("research", scope.getCodingFirstRep().getCode());
+
+        Assertions.assertEquals(PATIENT_ID, consent.getPatient().getIdentifier().getValue());
+    }
+}
