c6ac1bed3c4a6af06b998f1473527b37c8ee6175
Renaud Subiger <renaud@subiger.com>
2021-05-28 13:01:11 +0200

    Add integration tests for QuestionnaireResponse and update Postman collection

---
+++ src/test/java/org/ehrbase/fhirbridge/fhir/questionnaireresponse/ProvideQuestionnaireResponseTransactionIT.java
@@ -0,0 +1,39 @@
+package org.ehrbase.fhirbridge.fhir.questionnaireresponse;
+
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import org.ehrbase.fhirbridge.fhir.AbstractTransactionIT;
+import org.hl7.fhir.r4.model.QuestionnaireResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Integration Tests that validate "Provide Questionnaire Response" transaction.
+ */
+class ProvideQuestionnaireResponseTransactionIT extends AbstractTransactionIT {
+
+    @Test
+    void provideQuestionnaireResponseCreate() throws Exception {
+        var outcome = create("QuestionnaireResponse/transactions/provide-questionnaire-response-create.json");
+
+        Assertions.assertEquals(true, outcome.getCreated());
+        Assertions.assertNotNull(outcome.getId().getValue());
+    }
+
+    @Test
+    void provideQuestionnaireResponseConditionalUpdate() throws Exception {
+        MethodOutcome outcome;
+
+        outcome = create("QuestionnaireResponse/transactions/provide-questionnaire-response-create.json");
+        var id = outcome.getId();
+
+        outcome = update("QuestionnaireResponse/transactions/provide-questionnaire-response-update.json", "QuestionnaireResponse?_id=" + id.getIdPart() + "&patient.identifier=" + PATIENT_ID);
+
+        Assertions.assertEquals(id.getIdPart(), outcome.getId().getIdPart());
+        Assertions.assertEquals(id.getVersionIdPartAsLong() + 1, outcome.getId().getVersionIdPartAsLong());
+
+        var questionnaireResponse = (QuestionnaireResponse) outcome.getResource();
+
+        Assertions.assertEquals(PATIENT_ID, questionnaireResponse.getSubject().getIdentifier().getValue());
+        Assertions.assertEquals(QuestionnaireResponse.QuestionnaireResponseStatus.AMENDED, questionnaireResponse.getStatus());
+    }
+}
