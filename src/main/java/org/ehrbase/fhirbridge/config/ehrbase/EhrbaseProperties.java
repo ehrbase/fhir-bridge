17d8ad5c88cdf89d4b118aad4405693534030fcb
Renaud Subiger <renaud@subiger.com>
2021-06-01 09:14:02 +0200

    Add a configuration property to enable the modification of existing templates

--- src/main/java/org/ehrbase/fhirbridge/config/ehrbase/EhrbaseProperties.java
+++ src/main/java/org/ehrbase/fhirbridge/config/ehrbase/EhrbaseProperties.java
@@ -1,9 +1,27 @@
-package org.ehrbase.fhirbridge.config;
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ehrbase.fhirbridge.config.ehrbase;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * {@link ConfigurationProperties ConfigurationProperties} to configure EHRbase SDK.
+ * {@link ConfigurationProperties ConfigurationProperties} to configure EHRbase.
+ *
+ * @since 1.0.0
  */
 @ConfigurationProperties(prefix = "fhir-bridge.ehrbase")
 public class EhrbaseProperties {
@@ -87,12 +105,22 @@ public class EhrbaseProperties {
 
         private String prefix;
 
+        private boolean forceUpdate;
+
         public String getPrefix() {
             return prefix;
         }
 
         public void setPrefix(String prefix) {
             this.prefix = prefix;
+        }
+
+        public boolean isForceUpdate() {
+            return forceUpdate;
+        }
+
+        public void setForceUpdate(boolean forceUpdate) {
+            this.forceUpdate = forceUpdate;
         }
     }
 
