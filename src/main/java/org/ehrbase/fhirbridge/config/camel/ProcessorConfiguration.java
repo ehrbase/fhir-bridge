a9165bf9d605ed6158eca8bbbf574bf7597e27f9
SevKohler <kohlerseverin@posteo.de>
2021-06-30 14:15:38 +0200

    added merge

--- src/main/java/org/ehrbase/fhirbridge/config/camel/ProcessorConfiguration.java
+++ src/main/java/org/ehrbase/fhirbridge/config/camel/ProcessorConfiguration.java
@@ -4,7 +4,17 @@ import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import org.ehrbase.fhirbridge.camel.processor.FindResourceProcessor;
 import org.ehrbase.fhirbridge.camel.processor.ProvideResourcePersistenceProcessor;
 import org.ehrbase.fhirbridge.core.repository.ResourceMapRepository;
-import org.hl7.fhir.r4.model.*;
+import org.hl7.fhir.r4.model.AuditEvent;
+import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.Consent;
+import org.hl7.fhir.r4.model.DiagnosticReport;
+import org.hl7.fhir.r4.model.Encounter;
+import org.hl7.fhir.r4.model.Immunization;
+import org.hl7.fhir.r4.model.MedicationStatement;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Procedure;
+import org.hl7.fhir.r4.model.QuestionnaireResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
