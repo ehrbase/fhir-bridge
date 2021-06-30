5c05b4e82bcb131267ca0961788c0ba4be51848a
SevKohler <kohlerseverin@posteo.de>
2021-06-30 12:14:39 +0200

    updated immunization

--- src/main/java/org/ehrbase/fhirbridge/ehr/opt/impfstatuscomposition/definition/ImpfungAction.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/impfstatuscomposition/definition/ImpfungAction.java
@@ -18,7 +18,7 @@ import org.ehrbase.client.classgenerator.shareddefinition.Transition;
 @Archetype("openEHR-EHR-ACTION.medication.v1")
 @Generated(
     value = "org.ehrbase.client.classgenerator.ClassGenerator",
-    date = "2021-05-04T17:09:24.124296+02:00",
+    date = "2021-06-30T11:44:02.752999+02:00",
     comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.4.0"
 )
 public class ImpfungAction implements EntryEntity {
