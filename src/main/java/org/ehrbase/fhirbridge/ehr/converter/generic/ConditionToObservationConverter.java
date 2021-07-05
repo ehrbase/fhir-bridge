93e85a6e7168260f54b5308b6c9df92abf4923d1
Renaud Subiger <renaud@subiger.com>
2021-07-05 15:43:45 +0200

    Fix Blood Gas Panel

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/generic/ConditionToObservationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/generic/ConditionToObservationConverter.java
@@ -2,14 +2,14 @@ package org.ehrbase.fhirbridge.ehr.converter.generic;
 
 import org.ehrbase.client.classgenerator.interfaces.EntryEntity;
 import org.hl7.fhir.r4.model.Condition;
-import org.hl7.fhir.r4.model.DiagnosticReport;
 import org.springframework.lang.NonNull;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.temporal.TemporalAccessor;
 
-public abstract class ConditionToObservationConverter<E extends EntryEntity> extends EntryEntityConverter<Condition, E>  {
+@SuppressWarnings("java:S6212")
+public abstract class ConditionToObservationConverter<E extends EntryEntity> extends EntryEntityConverter<Condition, E> {
 
     @Override
     public E convert(@NonNull Condition resource) {
@@ -23,24 +23,24 @@ public abstract class ConditionToObservationConverter<E extends EntryEntity> ext
         invokeSetTimeValue(entryEntity, resource);
     }
 
-    public void invokeSetTimeValue(E entryEntity, Condition resource){
+    public void invokeSetTimeValue(E entryEntity, Condition resource) {
         try {
             Method setOriginValue = entryEntity.getClass().getMethod("setOriginValue", TemporalAccessor.class);
             setOriginValue.invoke(entryEntity, TimeConverter.convertConditionTime(resource));
         } catch (IllegalAccessException | InvocationTargetException exception) {
             exception.printStackTrace();
-        } catch (NoSuchMethodException ignored){
+        } catch (NoSuchMethodException ignored) {
             //ignored
         }
     }
 
-    public void invokeOriginValue(E entryEntity, Condition resource){
+    public void invokeOriginValue(E entryEntity, Condition resource) {
         try {
             Method setTimeValue = entryEntity.getClass().getMethod("setTimeValue", TemporalAccessor.class);
             setTimeValue.invoke(entryEntity, TimeConverter.convertConditionTime(resource));
-        } catch ( IllegalAccessException | InvocationTargetException exception) {
+        } catch (IllegalAccessException | InvocationTargetException exception) {
             exception.printStackTrace();
-        }catch (NoSuchMethodException ignored){
+        } catch (NoSuchMethodException ignored) {
             //ignored
         }
     }
