226390c1257e91c8dc353d4f5dbbb0edafc3685b
Renaud Subiger <renaud@subiger.com>
2021-07-06 13:29:00 +0200

    Bugfix ArrayIndexOutOfBoundsException in PCRObservationConverter

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/geccovirologischerbefund/PCRObservationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/geccovirologischerbefund/PCRObservationConverter.java
@@ -10,33 +10,45 @@ import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.defini
 import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.NachweisDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.ProAnalytCluster;
 import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.VirusnachweistestDefiningCode;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Reference;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("java:S6212")
 public class PCRObservationConverter extends ObservationToObservationConverter<BefundObservation> {
 
     @Override
     protected BefundObservation convertInternal(Observation resource) {
         BefundObservation befundObservation = new BefundObservation();
-        befundObservation.setFeederAudit(createIdentifierSection(resource));
         befundObservation.setLabortestBezeichnungDefiningCode(LabortestBezeichnungDefiningCode.DETECTION_OF_VIRUS_PROCEDURE);
         befundObservation.setLabortestPanel(createLabortestPanel(resource));
+
+        if (resource.hasIdentifier()) {
+            befundObservation.setFeederAudit(createIdentifierSection(resource));
+        }
         return befundObservation;
     }
 
     private FeederAudit createIdentifierSection(Observation observation) {
         FeederAudit feederAudit = new FeederAudit();
         List<DvIdentifier> identifierList = new ArrayList<>();
-        com.nedap.archie.rm.datavalues.DvIdentifier e =  new com.nedap.archie.rm.datavalues.DvIdentifier();
-        e.setAssigner(observation.getIdentifier().get(0).getAssigner().getReference());
-        e.setType(observation.getIdentifier().get(0).getType().toString().split("@")[0]);
-        e.setId(observation.getIdentifier().get(0).getValue());
+        DvIdentifier e = new DvIdentifier();
+        Identifier identifier = observation.getIdentifierFirstRep();
+        Reference assigner = identifier.getAssigner();
+        if (assigner.hasReference()) {
+            e.setAssigner(assigner.getReference());
+        } else {
+            e.setAssigner(assigner.getIdentifier().getValue());
+        }
+        e.setType(identifier.getType().toString().split("@")[0]);
+        e.setId(identifier.getValue());
         identifierList.add(e);
         feederAudit.setOriginatingSystemItemIds(identifierList);
         com.nedap.archie.rm.archetyped.FeederAuditDetails originatingSystemAudit = new com.nedap.archie.rm.archetyped.FeederAuditDetails();
-        originatingSystemAudit.setSystemId(observation.getIdentifier().get(0).getSystem());
+        originatingSystemAudit.setSystemId(identifier.getSystem());
         feederAudit.setOriginatingSystemAudit(originatingSystemAudit);
         return feederAudit;
     }
@@ -45,7 +57,7 @@ public class PCRObservationConverter extends ObservationToObservationConverter<B
         LabortestPanelCluster labortestPanel = new LabortestPanelCluster();
         ProAnalytCluster analyt = new ProAnalytCluster();
         analyt.setVirusnachweistestDefiningCode(VirusnachweistestDefiningCode.SARS_COV2_COVID19_RNA_PRESENCE_IN_RESPIRATORY_SPECIMEN_BY_NAA_WITH_PROBE_DETECTION);
-        switch(observation.getValueCodeableConcept().getCoding().get(0).getCode()) {
+        switch (observation.getValueCodeableConcept().getCoding().get(0).getCode()) {
             case "260373001":
                 analyt.setNachweisDefiningCode(NachweisDefiningCode.DETECTED_QUALIFIER_VALUE);
                 break;
