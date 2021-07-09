8702bc5687ec6e00ad09bea26e6598f48bb3af98
Renaud Subiger <renaud@subiger.com>
2021-07-09 19:47:48 +0200

    Refactor therapy converter

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/therapy/NichtDurchgefuehrteProzedurEvaluationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/therapy/NichtDurchgefuehrteProzedurEvaluationConverter.java
@@ -1,38 +1,35 @@
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ehrbase.fhirbridge.ehr.converter.specific.therapy;
 
-import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
-import com.nedap.archie.rm.generic.PartySelf;
-import org.ehrbase.client.classgenerator.shareddefinition.Language;
-import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
 import org.ehrbase.fhirbridge.ehr.converter.generic.EntryEntityConverter;
-import org.ehrbase.fhirbridge.ehr.converter.specific.CodeSystem;
-import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.NameDerProzedurDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.NichtDurchgefuehrteProzedurEvaluation;
-import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Procedure;
 
-public class NichtDurchgefuehrteProzedurEvaluationConverter extends EntryEntityConverter<Procedure, NichtDurchgefuehrteProzedurEvaluation> {
+@SuppressWarnings("java:S6212")
+public class NichtDurchgefuehrteProzedurEvaluationConverter
+        extends EntryEntityConverter<Procedure, NichtDurchgefuehrteProzedurEvaluation>
+        implements TherapyConverter {
+
     @Override
-    protected NichtDurchgefuehrteProzedurEvaluation convertInternal(Procedure resource) {
-        NichtDurchgefuehrteProzedurEvaluation nichtDurchgefuehrteProzedur = new NichtDurchgefuehrteProzedurEvaluation();
-        nichtDurchgefuehrteProzedur.setAussageUeberDenAusschlussValue(resource.getStatus().getDisplay());
-        try {
-            nichtDurchgefuehrteProzedur.setEingriffDefiningCode(mapNameDerProzedur(resource));
-        } catch (UnprocessableEntityException e) {
-            throw new ConversionException("Some parts of the not present procedure did not contain the required elements. "
-                    + e.getMessage(), e);
-        }
-        return nichtDurchgefuehrteProzedur;
+    protected NichtDurchgefuehrteProzedurEvaluation convertInternal(Procedure procedure) {
+        NichtDurchgefuehrteProzedurEvaluation result = new NichtDurchgefuehrteProzedurEvaluation();
+        result.setAussageUeberDenAusschlussValue(Procedure.ProcedureStatus.NOTDONE.getDisplay());
+        result.setEingriffDefiningCode(convertCode(procedure));
+        return result;
     }
-
-    private NameDerProzedurDefiningCode mapNameDerProzedur(Procedure procedure) throws UnprocessableEntityException {
-        Coding coding = procedure.getCode().getCoding().get(0);
-
-        if (coding.getSystem().equals(CodeSystem.SNOMED.getUrl()) && NameDerProzedurDefiningCode.getCodesAsMap().containsKey(coding.getCode())) {
-            return NameDerProzedurDefiningCode.getCodesAsMap().get(coding.getCode());
-        } else {
-            throw new UnprocessableEntityException("Invalid name of procedure");
-        }
-    }
-
 }
