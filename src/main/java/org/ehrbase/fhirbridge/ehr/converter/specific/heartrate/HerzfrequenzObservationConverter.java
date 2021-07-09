d67009f33cff0d0d4b3c3aa8777ea64b8697dd5c
SevKohler <kohlerseverin@posteo.de>
2021-07-09 13:57:25 +0200

    refactored heart rate

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/heartrate/HerzfrequenzObservationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/heartrate/HerzfrequenzObservationConverter.java
@@ -7,18 +7,30 @@ import org.ehrbase.fhirbridge.ehr.opt.herzfrequenzcomposition.definition.Herzfre
 import org.hl7.fhir.r4.model.Observation;
 
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 public class HerzfrequenzObservationConverter extends ObservationToObservationConverter<HerzfrequenzObservation> {
 
     @Override
     protected HerzfrequenzObservation convertInternal(Observation resource) {
         HerzfrequenzObservation herzfrequenzObservation = new HerzfrequenzObservation();
-        try {
-            herzfrequenzObservation.setFrequenzMagnitude(resource.getValueQuantity().getValue().doubleValue());
-            herzfrequenzObservation.setFrequenzUnits(resource.getValueQuantity().getCode());//note that the textual value that openEHR template expects as unit is stored in code for this entity
-        } catch (Exception e) {
-            throw new UnprocessableEntityException(e.getMessage());
-        }
+        getValue(resource).ifPresent(herzfrequenzObservation::setFrequenzMagnitude);
         return herzfrequenzObservation;
+    }
+
+    private Optional<Double> getValue(Observation resource) {
+        if(resource.hasValueQuantity() ){
+            return Optional.of(resource.getValueQuantity().getValue().doubleValue());
+        }else{
+            return Optional.empty();
+        }
+    }
+
+    private Optional<String> getUnit(Observation resource) {
+        if(resource.hasValueQuantity() ){
+            return Optional.of(resource.getValueQuantity().getCode());
+        }else{
+            return Optional.empty();
+        }
     }
 }
