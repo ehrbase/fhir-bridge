1219fb3547fb80d6b925aa947201bb42f0f8d98a
Renaud Subiger <renaud@subiger.com>
2021-07-09 14:49:55 +0200

    Refactor symptom

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/symptom/AusgeschlossenesSymptomEvaluationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/symptom/AusgeschlossenesSymptomEvaluationConverter.java
@@ -3,9 +3,9 @@ package org.ehrbase.fhirbridge.ehr.converter.specific.symptom;
 import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
 import org.ehrbase.fhirbridge.ehr.converter.generic.EntryEntityConverter;
 import org.ehrbase.fhirbridge.ehr.converter.specific.CodeSystem;
+import org.ehrbase.fhirbridge.ehr.opt.symptomcomposition.definition.AusgeschlossenesSymptomEvaluation;
 import org.ehrbase.fhirbridge.ehr.opt.symptomcomposition.definition.AussageUberDenAusschlussDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.symptomcomposition.definition.KrankheitsanzeichenCode;
-import org.ehrbase.fhirbridge.ehr.opt.symptomcomposition.definition.AusgeschlossenesSymptomEvaluation;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Condition;
 
@@ -20,11 +20,11 @@ public class AusgeschlossenesSymptomEvaluationConverter extends EntryEntityConve
     }
 
     private void setProblemDiagnose(Condition resource, AusgeschlossenesSymptomEvaluation ausgeschlossenesSymptom) {
-            Coding coding = resource.getCode().getCoding().get(0);
-            if (coding.getSystem().equals(CodeSystem.SNOMED.getUrl()) &&  KrankheitsanzeichenCode.getCodesAsMap().containsKey(coding.getCode())) {
-                ausgeschlossenesSymptom.setProblemDiagnose(KrankheitsanzeichenCode.getCodesAsMap().get(coding.getCode()).toDvCodedText());
-            }else{
-                throw new ConversionException("Unbekanntes Diagnose/Problem.");
-            }
+        Coding coding = resource.getCode().getCoding().get(0);
+        if (coding.getSystem().equals(CodeSystem.SNOMED.getUrl()) && KrankheitsanzeichenCode.getCodesAsMap().containsKey(coding.getCode())) {
+            ausgeschlossenesSymptom.setProblemDiagnose(KrankheitsanzeichenCode.getCodesAsMap().get(coding.getCode()).toDvCodedText());
+        } else {
+            throw new ConversionException("Unbekanntes Diagnose/Problem.");
+        }
     }
 }
