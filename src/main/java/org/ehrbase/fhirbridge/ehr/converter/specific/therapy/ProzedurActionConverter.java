76d5f24b2074c05385e0ddf71a2c486242d91736
Renaud Subiger <renaud@subiger.com>
2021-07-09 19:47:48 +0200

    Refactor therapy converter

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/therapy/ProzedurActionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/therapy/ProzedurActionConverter.java
@@ -1,94 +1,116 @@
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ehrbase.fhirbridge.ehr.converter.specific.therapy;
 
-import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
 import org.ehrbase.fhirbridge.ehr.converter.generic.ProcedureToProcedureActionConverter;
 import org.ehrbase.fhirbridge.ehr.converter.specific.CodeSystem;
 import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.CurrentStateDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.GeraetenameDefiningCode;
+import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.KategorieDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.KoerperstelleDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.MedizingeraetCluster;
-import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.NameDerProzedurDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccoprozedurcomposition.definition.ProzedurAction;
+import org.hl7.fhir.r4.model.Annotation;
 import org.hl7.fhir.r4.model.Coding;
-import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.Procedure;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class ProzedurActionConverter extends ProcedureToProcedureActionConverter<ProzedurAction> {
+@SuppressWarnings("java:S6212")
+public class ProzedurActionConverter extends ProcedureToProcedureActionConverter<ProzedurAction>
+        implements TherapyConverter {
+
     @Override
     protected ProzedurAction convertInternal(Procedure procedure) {
-        ProzedurAction durchgefuehrteProzedur = new ProzedurAction();
-        try {
-
-            durchgefuehrteProzedur.setNameDerProzedurDefiningCode(mapNameDerProzedur(procedure));
-
-            if (durchgefuehrteProzedur.getNameDerProzedurDefiningCode().equals(NameDerProzedurDefiningCode.PLAIN_RADIOGRAPHY) ||
-                    durchgefuehrteProzedur.getNameDerProzedurDefiningCode().equals(NameDerProzedurDefiningCode.DIAGNOSTIC_ULTRASONOGRAPHY_PROCEDURE) ||
-                    durchgefuehrteProzedur.getNameDerProzedurDefiningCode().equals(NameDerProzedurDefiningCode.COMPUTERIZED_AXIAL_TOMOGRAPHY_PROCEDURE)) {
-                mapBodySite(durchgefuehrteProzedur, procedure);
-            } else if (durchgefuehrteProzedur.getNameDerProzedurDefiningCode().equals(NameDerProzedurDefiningCode.ARTIFICIAL_RESPIRATION_PROCEDURE) ||
-                    durchgefuehrteProzedur.getNameDerProzedurDefiningCode().equals(NameDerProzedurDefiningCode.OXYGEN_ADMINISTRATION_BY_NASAL_CANNULA_PROCEDURE) ||
-                    durchgefuehrteProzedur.getNameDerProzedurDefiningCode().equals(NameDerProzedurDefiningCode.RESPIRATORY_THERAPY_PROCEDURE) ||
-                    durchgefuehrteProzedur.getNameDerProzedurDefiningCode().equals(NameDerProzedurDefiningCode.NONINVASIVE_VENTILATION_PROCEDURE)) {
-                mapMedizingerat(durchgefuehrteProzedur, procedure);
-            }
-            durchgefuehrteProzedur.setDurchfuehrungsabsichtValue(mapDurchfuhrungsabsicht(procedure));
-            durchgefuehrteProzedur.setKommentarValue(procedure.getNote().toString());
-
-        } catch (UnprocessableEntityException e) {
-            throw new ConversionException("Some parts of the present procedure did not contain the required elements. "
-                    + e.getMessage(), e);
-        }
-        durchgefuehrteProzedur.setCurrentStateDefiningCode(CurrentStateDefiningCode.PLANNED);
-        return durchgefuehrteProzedur;
+        ProzedurAction result = new ProzedurAction();
+        result.setNameDerProzedurDefiningCode(convertCode(procedure));
+        convertBodySite(procedure).ifPresent(result::setKoerperstelleDefiningCode);
+        result.setMedizingeraet(convertUsedCode(procedure));
+        result.setArtDerProzedurDefiningCode(convertCategory(procedure));
+        convertExtension(procedure).ifPresent(result::setDurchfuehrungsabsichtValue);
+        convertNote(procedure).ifPresent(result::setKommentarValue);
+        result.setCurrentStateDefiningCode(CurrentStateDefiningCode.PLANNED);
+        return result;
     }
 
-    private void mapBodySite(ProzedurAction durchgefuehrteProzedur, Procedure procedure) {
-        Coding bodySiteCoding = procedure.getBodySite().get(0).getCoding().get(0);
-        if (bodySiteCoding.getSystem().equals(CodeSystem.SNOMED.getUrl()) &&
-                KoerperstelleDefiningCode.getCodesAsMap().containsKey(bodySiteCoding.getCode())) {
-            durchgefuehrteProzedur.setKoerperstelleDefiningCode(KoerperstelleDefiningCode.getCodesAsMap().get(bodySiteCoding.getCode()));
-        } else {
-            throw new UnprocessableEntityException("Invalid body site for PLAIN_RADIOGRAPHY");
-        }
+    private Optional<KoerperstelleDefiningCode> convertBodySite(Procedure procedure) {
+        return procedure.getBodySite()
+                .stream()
+                .flatMap(concept -> concept.getCoding().stream())
+                .filter(coding -> coding.getSystem().equals(CodeSystem.SNOMED.getUrl()))
+                .findFirst()
+                .map(coding -> {
+                    KoerperstelleDefiningCode code = KoerperstelleDefiningCode.getCodesAsMap().get(coding.getCode());
+                    if (code == null) {
+                        throw new ConversionException("Invalid body site");
+                    }
+                    return code;
+                });
     }
 
-    private void mapMedizingerat(ProzedurAction durchgefuehrteProzedur, Procedure procedure) {
-        if (procedure.getUsedCode() == null || procedure.getUsedCode().isEmpty()) {
-            return;
-        }
-        Coding usedCodeCoding = procedure.getUsedCode().get(0).getCoding().get(0);
-        if (usedCodeCoding.getSystem().equals(CodeSystem.SNOMED.getUrl()) && GeraetenameDefiningCode.getCodesAsMap().containsKey(usedCodeCoding.getCode())) {
-            MedizingeraetCluster medizingeraetCluster = new MedizingeraetCluster();
-            medizingeraetCluster.setGeraetenameDefiningCode(GeraetenameDefiningCode.getCodesAsMap().get(usedCodeCoding.getCode()));
-            durchgefuehrteProzedur.setMedizingeraet(new ArrayList<>());
-            durchgefuehrteProzedur.getMedizingeraet().add(medizingeraetCluster);
-        } else {
-            throw new UnprocessableEntityException("Invalid medical device code");
-        }
+    private List<MedizingeraetCluster> convertUsedCode(Procedure procedure) {
+        return procedure.getUsedCode()
+                .stream()
+                .flatMap(concept -> concept.getCoding().stream())
+                .filter(coding -> coding.getSystem().equals(CodeSystem.SNOMED.getUrl()))
+                .map(coding -> {
+                    GeraetenameDefiningCode code = GeraetenameDefiningCode.getCodesAsMap().get(coding.getCode());
+                    if (code == null) {
+                        throw new ConversionException("Invalid medical device code");
+                    }
+                    MedizingeraetCluster medizingeraetCluster = new MedizingeraetCluster();
+                    medizingeraetCluster.setGeraetenameDefiningCode(GeraetenameDefiningCode.getCodesAsMap().get(coding.getCode()));
+                    return medizingeraetCluster;
+                })
+                .collect(Collectors.toList());
     }
 
-    private String mapDurchfuhrungsabsicht(Procedure procedure) {
-        if (procedure.getExtension().isEmpty()) {
-            return null;
-        }
-        for (Extension extension : procedure.getExtension()) {
-            if (extension.getValue() instanceof Coding) {
-                return ((Coding) extension.getValue()).getDisplay();
-            }
-        }
-        return null;
+    private KategorieDefiningCode convertCategory(Procedure procedure) {
+        return procedure.getCategory()
+                .getCoding()
+                .stream()
+                .filter(coding -> coding.getSystem().equals(CodeSystem.SNOMED.getUrl()))
+                .map(coding -> KategorieDefiningCode.getCodesAsMap().get(coding.getCode()))
+                .findFirst()
+                .orElseThrow(() -> new ConversionException("Invalid category"));
     }
 
-    private NameDerProzedurDefiningCode mapNameDerProzedur(Procedure procedure) throws UnprocessableEntityException {
-        Coding coding = procedure.getCode().getCoding().get(0);
-
-        if (coding.getSystem().equals(CodeSystem.SNOMED.getUrl()) && NameDerProzedurDefiningCode.getCodesAsMap().containsKey(coding.getCode())) {
-            return NameDerProzedurDefiningCode.getCodesAsMap().get(coding.getCode());
-        } else {
-            throw new UnprocessableEntityException("Invalid name of procedure");
+    private Optional<String> convertExtension(Procedure procedure) {
+        if (!procedure.hasExtension()) {
+            return Optional.empty();
         }
+
+        return procedure.getExtension()
+                .stream()
+                .filter(extension -> extension.getUrl().equals("https://www.medizininformatik-initiative.de/fhir/core/modul-prozedur/StructureDefinition/Durchfuehrungsabsicht"))
+                .findFirst()
+                .map(extension -> ((Coding) extension.getValue()).getCode());
+    }
+
+    private Optional<String> convertNote(Procedure procedure) {
+        if (!procedure.hasNote()) {
+            return Optional.empty();
+        }
+        return Optional.of(procedure.getNote()
+                .stream()
+                .map(Annotation::getText)
+                .collect(Collectors.joining(", ")));
     }
 }
