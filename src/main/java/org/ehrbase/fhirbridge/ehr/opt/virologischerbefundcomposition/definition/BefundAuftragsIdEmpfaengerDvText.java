d4797777ca632276eaabc51d473b036a430b8935
NormanWeinert <norman.weinert@med.uni-goettingen.de>
2021-07-30 09:55:46 +0200

    Apply changes from Feature/NUM-128_Virologischer-Befund

---
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/virologischerbefundcomposition/definition/BefundAuftragsIdEmpfaengerDvText.java
@@ -0,0 +1,32 @@
+package org.ehrbase.fhirbridge.ehr.opt.virologischerbefundcomposition.definition;
+
+import java.lang.String;
+import javax.annotation.processing.Generated;
+import org.ehrbase.client.annotations.Entity;
+import org.ehrbase.client.annotations.OptionFor;
+import org.ehrbase.client.annotations.Path;
+import org.ehrbase.client.classgenerator.interfaces.RMEntity;
+
+@Entity
+@Generated(
+    value = "org.ehrbase.client.classgenerator.ClassGenerator",
+    date = "2021-07-14T14:30:10.479790400+02:00",
+    comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.5.0"
+)
+@OptionFor("DV_TEXT")
+public class BefundAuftragsIdEmpfaengerDvText implements RMEntity, BefundAuftragsIdEmpfaengerChoice {
+  /**
+   * Path: Virologischer Befund/Befund/Details der Testanforderung/Auftrags-ID (Empfänger)/Auftrags-ID (Empfänger)
+   * Description: Lokale Auftrags-ID, die vom auftragsempfangendem System, gewöhnlich dem Laborinformationssystem (LIS) zugewiesen wird.
+   */
+  @Path("|value")
+  private String auftragsIdEmpfaengerValue;
+
+  public void setAuftragsIdEmpfaengerValue(String auftragsIdEmpfaengerValue) {
+     this.auftragsIdEmpfaengerValue = auftragsIdEmpfaengerValue;
+  }
+
+  public String getAuftragsIdEmpfaengerValue() {
+     return this.auftragsIdEmpfaengerValue ;
+  }
+}
---
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/virologischerbefundcomposition/definition/VirusnachweistestDefiningCode.java
@@ -0,0 +1,55 @@
+package org.ehrbase.fhirbridge.ehr.opt.virologischerbefundcomposition.definition;
+
+import java.lang.String;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.ehrbase.client.classgenerator.EnumValueSet;
+
+public enum VirusnachweistestDefiningCode implements EnumValueSet {
+  SARS_COV2_COVID19_RNA_PRESENCE_IN_RESPIRATORY_SPECIMEN_BY_NAA_WITH_PROBE_DETECTION("SARS-CoV-2 (COVID-19) RNA [Presence] in Respiratory specimen by NAA with probe detection", "", "LOINC", "94500-6"),
+
+  SARS_COV2_AG("SARS-CoV-2 Ag", "", "LOINC", "94558-4"),
+
+  SARS_COV2_COVID19_RNA_CYCLE_THRESHOLD_IN_RESPIRATORY_SPECIMEN_BY_NAA_WITH_PROBE_DETECTION("SARS-CoV-2 (COVID-19) RNA [Cycle Threshold #] in Respiratory specimen by NAA with probe detection", "", "LOINC", "94745-7");
+
+  private String value;
+
+  private String description;
+
+  private String terminologyId;
+
+  private String code;
+
+  VirusnachweistestDefiningCode(String value, String description, String terminologyId,
+      String code) {
+    this.value = value;
+    this.description = description;
+    this.terminologyId = terminologyId;
+    this.code = code;
+  }
+
+  public static Map<String, VirusnachweistestDefiningCode> getCodesAsMap(){
+    Map<String, VirusnachweistestDefiningCode> DefiningCodeHashMap = new HashMap<>();
+    for (VirusnachweistestDefiningCode DefiningCode : VirusnachweistestDefiningCode.values()) {
+      DefiningCodeHashMap.put(DefiningCode.getCode(), DefiningCode);
+    }
+    return DefiningCodeHashMap;
+  }
+
+  public String getValue() {
+     return this.value ;
+  }
+
+  public String getDescription() {
+     return this.description ;
+  }
+
+  public String getTerminologyId() {
+     return this.terminologyId ;
+  }
+
+  public String getCode() {
+     return this.code ;
+  }
+}
