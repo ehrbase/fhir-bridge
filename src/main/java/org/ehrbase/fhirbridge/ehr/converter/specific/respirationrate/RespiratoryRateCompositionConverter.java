38800ef52e8078433bc904cf2abec6e106928e02
SevKohler <kohlerseverin@posteo.de>
2021-08-11 16:39:17 +0200

    updated atemfrequenz opt and mapping

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/respirationrate/RespiratoryRateCompositionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/respirationrate/RespiratoryRateCompositionConverter.java
@@ -3,12 +3,14 @@ package org.ehrbase.fhirbridge.ehr.converter.specific.respirationrate;
 import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
 import org.ehrbase.fhirbridge.ehr.converter.generic.ObservationToCompositionConverter;
 import org.ehrbase.fhirbridge.ehr.opt.atemfrequenzcomposition.AtemfrequenzComposition;
+import org.ehrbase.fhirbridge.ehr.opt.atemfrequenzcomposition.definition.RegistereintragKategorieElement;
 import org.ehrbase.fhirbridge.ehr.opt.atemfrequenzcomposition.definition.StatusDefiningCode;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Observation;
 import org.springframework.lang.NonNull;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RespiratoryRateCompositionConverter extends ObservationToCompositionConverter<AtemfrequenzComposition> {
 
@@ -16,19 +18,22 @@ public class RespiratoryRateCompositionConverter extends ObservationToCompositio
     public AtemfrequenzComposition convertInternal(@NonNull Observation resource) {
         AtemfrequenzComposition composition = new AtemfrequenzComposition();
         mapStatus(composition, resource);
-        mapKategorie(resource).ifPresent(composition::setKategorieValue);
+        if (!mapKategorie(resource).isEmpty()) {
+            composition.setKategorie(mapKategorie(resource));
+        }
         composition.setAtemfrequenz(new AtemfrequenzObservationConverter().convert(resource));
         return composition;
     }
 
-    private Optional<String> mapKategorie( Observation resource) {
+    private List<RegistereintragKategorieElement> mapKategorie(Observation resource) {
         return resource.getCategory().get(0).getCoding().stream()
                 .map(Coding::getCode)
-                .findFirst();
-        }
+                .map(RegistereintragKategorieElement::new)
+                .collect(Collectors.toList());
+    }
 
     private void mapStatus(AtemfrequenzComposition composition, Observation resource) {
-        switch(resource.getStatusElement().getCode()){
+        switch (resource.getStatusElement().getCode()) {
             case "final":
                 composition.setStatusDefiningCode(StatusDefiningCode.FINAL);
                 break;
