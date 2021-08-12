e5c6fdd99e206b6968ae2813f7d72e62c2b206ae
SevKohler <kohlerseverin@posteo.de>
2021-08-12 17:37:21 +0200

    merged

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/respirationrate/RespiratoryRateCompositionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/respirationrate/RespiratoryRateCompositionConverter.java
@@ -1,13 +1,14 @@
 package org.ehrbase.fhirbridge.ehr.converter.specific.respirationrate;
 
-import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
 import org.ehrbase.fhirbridge.ehr.converter.generic.ObservationToCompositionConverter;
 import org.ehrbase.fhirbridge.ehr.opt.atemfrequenzcomposition.AtemfrequenzComposition;
-import org.ehrbase.fhirbridge.ehr.opt.atemfrequenzcomposition.definition.StatusDefiningCode;
+import org.ehrbase.fhirbridge.ehr.opt.atemfrequenzcomposition.definition.RegistereintragKategorieElement;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Observation;
 import org.springframework.lang.NonNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class RespiratoryRateCompositionConverter extends ObservationToCompositionConverter<AtemfrequenzComposition> {
