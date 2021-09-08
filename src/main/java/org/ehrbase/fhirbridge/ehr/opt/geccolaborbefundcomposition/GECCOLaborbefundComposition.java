731a14cd3dadf13aac87b503ffe0cbe1930249b7
SevKohler <kohlerseverin@posteo.de>
2021-09-08 15:37:24 +0200

    updated lab

--- src/main/java/org/ehrbase/fhirbridge/ehr/opt/geccolaborbefundcomposition/GECCOLaborbefundComposition.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/geccolaborbefundcomposition/GECCOLaborbefundComposition.java
@@ -29,8 +29,8 @@ import org.ehrbase.fhirbridge.ehr.opt.geccolaborbefundcomposition.definition.Sta
 @Archetype("openEHR-EHR-COMPOSITION.registereintrag.v1")
 @Generated(
     value = "org.ehrbase.client.classgenerator.ClassGenerator",
-    date = "2021-07-05T14:12:01.164754+02:00",
-    comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.4.0"
+    date = "2021-09-08T14:37:10.881862+02:00",
+    comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.5.0"
 )
 @Template("GECCO_Laborbefund")
 public class GECCOLaborbefundComposition implements CompositionEntity {
