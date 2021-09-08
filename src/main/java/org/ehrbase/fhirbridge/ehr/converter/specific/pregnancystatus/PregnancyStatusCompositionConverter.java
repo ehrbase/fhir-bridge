873a018fe16ebed4e1e5caa7fab4aeb4a5f055d2
SevKohler <kohlerseverin@posteo.de>
2021-09-08 16:24:20 +0200

    updated pregnancy status

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/pregnancystatus/PregnancyStatusCompositionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/pregnancystatus/PregnancyStatusCompositionConverter.java
@@ -1,15 +1,15 @@
 package org.ehrbase.fhirbridge.ehr.converter.specific.pregnancystatus;
 
-import org.ehrbase.client.classgenerator.shareddefinition.NullFlavour;
-import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
 import org.ehrbase.fhirbridge.ehr.converter.generic.ObservationToCompositionConverter;
-import org.ehrbase.fhirbridge.ehr.opt.schwangerschaftsstatuscomposition.definition.StatusDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.schwangerschaftsstatuscomposition.SchwangerschaftsstatusComposition;
+import org.ehrbase.fhirbridge.ehr.opt.schwangerschaftsstatuscomposition.definition.SchwangerschaftsstatusKategorieElement;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Observation;
 import org.springframework.lang.NonNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class PregnancyStatusCompositionConverter extends ObservationToCompositionConverter<SchwangerschaftsstatusComposition> {
@@ -18,20 +18,39 @@ public class PregnancyStatusCompositionConverter extends ObservationToCompositio
     public SchwangerschaftsstatusComposition convertInternal(@NonNull Observation resource) {
         SchwangerschaftsstatusComposition composition = new SchwangerschaftsstatusComposition();
         composition.setSchwangerschaftsstatus(new SchwangerschaftsstatusObservationConverter().convert(resource));
-        mapKategorie(resource, composition).ifPresent(composition::setKategorieValue);
+        convertKategorie(resource, composition).ifPresent(composition::setKategorie);
         return composition;
     }
 
-    private Optional<String> mapKategorie(Observation resource, SchwangerschaftsstatusComposition composition) {
-        if (resource.hasCategory()) {
-            return resource.getCategory()
-                    .stream()
-                    .filter(CodeableConcept::hasCoding)
-                    .flatMap(codeableConcept -> codeableConcept.getCoding().stream())
-                    .map(Coding::getCode)
-                    .findFirst();
-        } else {
+    private Optional<List<SchwangerschaftsstatusKategorieElement>> convertKategorie(Observation resource, SchwangerschaftsstatusComposition composition) {
+        List<SchwangerschaftsstatusKategorieElement> schwangerschaftsstatusKategorieElements = mapKategorie(resource);
+        if(schwangerschaftsstatusKategorieElements.isEmpty()){
+            return Optional.of(schwangerschaftsstatusKategorieElements);
+        }else{
             return Optional.empty();
         }
+    }
+
+    private List<SchwangerschaftsstatusKategorieElement> mapKategorie(Observation resource) {
+        List<SchwangerschaftsstatusKategorieElement> schwangerschaftsstatusKategorieElements = new ArrayList<>();
+        if (resource.hasCategory()) {
+            return convertSchwangerschaftsstatusKategoire(resource, schwangerschaftsstatusKategorieElements);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    private List<SchwangerschaftsstatusKategorieElement> convertSchwangerschaftsstatusKategoire(Observation resource, List<SchwangerschaftsstatusKategorieElement> schwangerschaftsstatusKategorieElements) {
+        resource.getCategory()
+                .stream()
+                .filter(CodeableConcept::hasCoding)
+                .flatMap(codeableConcept -> codeableConcept.getCoding().stream())
+                .map(Coding::getCode)
+                .forEach(code -> {
+                    SchwangerschaftsstatusKategorieElement schwangerschaftsstatusKategorieElement = new SchwangerschaftsstatusKategorieElement();
+                    schwangerschaftsstatusKategorieElement.setValue(code);
+                    schwangerschaftsstatusKategorieElements.add(schwangerschaftsstatusKategorieElement);
+                });
+        return schwangerschaftsstatusKategorieElements;
     }
 }
