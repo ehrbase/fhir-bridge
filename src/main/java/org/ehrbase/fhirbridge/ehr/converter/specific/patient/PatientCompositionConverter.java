63573d65ba1ec93ed8e1f4bdcb9374a0fcadd1c2
Renaud Subiger <renaud@subiger.com>
2021-09-10 11:00:00 +0200

    Updates mapping tests

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/patient/PatientCompositionConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/patient/PatientCompositionConverter.java
@@ -1,15 +1,10 @@
 package org.ehrbase.fhirbridge.ehr.converter.specific.patient;
 
-import com.nedap.archie.rm.generic.PartyIdentified;
-import com.nedap.archie.rm.generic.PartyProxy;
-import com.nedap.archie.rm.generic.PartySelf;
 import org.ehrbase.fhirbridge.ehr.converter.generic.PatientToCompositionConverter;
 import org.ehrbase.fhirbridge.ehr.converter.specific.patient.personendaten.PersonenDatenAdminEntryConverter;
 import org.ehrbase.fhirbridge.ehr.opt.geccopersonendatencomposition.GECCOPersonendatenComposition;
 import org.hl7.fhir.r4.model.Patient;
 import org.springframework.lang.NonNull;
-
-import java.util.Optional;
 
 public class PatientCompositionConverter extends PatientToCompositionConverter<GECCOPersonendatenComposition> {
 
@@ -20,15 +15,5 @@ public class PatientCompositionConverter extends PatientToCompositionConverter<G
         composition.setPersonendaten(new PersonenDatenAdminEntryConverter().convert(resource));
         new GeschlechtConverter().convert(resource).ifPresent(composition::setGeschlecht);
         return composition;
-    }
-
-    @Override
-    protected PartyProxy convertComposer(Patient resource) {
-        return new PartySelf();
-    }
-
-    @Override
-    protected Optional<PartyIdentified> convertHealthCareFacility(Patient resource) {
-        return Optional.empty();
     }
 }
