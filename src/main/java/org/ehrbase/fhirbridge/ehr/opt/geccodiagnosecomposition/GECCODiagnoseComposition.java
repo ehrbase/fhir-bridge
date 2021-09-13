9881c639af0ed1b5b8943767730aeb04dc249a7a
SevKohler <kohlerseverin@posteo.de>
2021-09-13 16:14:21 +0200

    updated diagnose

--- src/main/java/org/ehrbase/fhirbridge/ehr/opt/geccodiagnosecomposition/GECCODiagnoseComposition.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/geccodiagnosecomposition/GECCODiagnoseComposition.java
@@ -2,7 +2,6 @@ package org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition;
 
 import com.nedap.archie.rm.archetyped.FeederAudit;
 import com.nedap.archie.rm.datastructures.Cluster;
-import com.nedap.archie.rm.datavalues.DvCodedText;
 import com.nedap.archie.rm.generic.Participation;
 import com.nedap.archie.rm.generic.PartyIdentified;
 import com.nedap.archie.rm.generic.PartyProxy;
@@ -23,6 +22,7 @@ import org.ehrbase.client.classgenerator.shareddefinition.Setting;
 import org.ehrbase.client.classgenerator.shareddefinition.Territory;
 import org.ehrbase.client.openehrclient.VersionUid;
 import org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition.definition.AusgeschlosseneDiagnoseEvaluation;
+import org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition.definition.KategorieDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition.definition.StatusDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition.definition.UnbekannteDiagnoseEvaluation;
 import org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition.definition.VorliegendeDiagnoseEvaluation;
@@ -31,8 +31,8 @@ import org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition.definition.Vorlie
 @Archetype("openEHR-EHR-COMPOSITION.registereintrag.v1")
 @Generated(
     value = "org.ehrbase.client.classgenerator.ClassGenerator",
-    date = "2021-07-12T18:58:20.518470+02:00",
-    comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.4.0"
+    date = "2021-09-13T14:55:50.684644+02:00",
+    comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.5.0"
 )
 @Template("GECCO_Diagnose")
 public class GECCODiagnoseComposition implements CompositionEntity {
@@ -44,7 +44,7 @@ public class GECCODiagnoseComposition implements CompositionEntity {
 
   /**
    * Path: GECCO_Diagnose/context/Erweiterung
-   * Description: Ergänzende Angaben zum Registereintrag. 
+   * Description: Ergänzende Angaben zum Registereintrag.
    */
   @Path("/context/other_context[at0001]/items[at0002]")
   private List<Cluster> erweiterung;
@@ -66,8 +66,8 @@ public class GECCODiagnoseComposition implements CompositionEntity {
    * Path: GECCO_Diagnose/context/Kategorie
    * Description: Die Klassifikation des Registereintrags (z.B. Typ der Observation des FHIR-Profils).
    */
-  @Path("/context/other_context[at0001]/items[at0005]/value")
-  private DvCodedText kategorie;
+  @Path("/context/other_context[at0001]/items[at0005]/value|defining_code")
+  private KategorieDefiningCode kategorieDefiningCode;
 
   /**
    * Path: GECCO_Diagnose/context/Baum/Kategorie/null_flavour
@@ -192,12 +192,12 @@ public class GECCODiagnoseComposition implements CompositionEntity {
      return this.statusNullFlavourDefiningCode ;
   }
 
-  public void setKategorie(DvCodedText kategorie) {
-     this.kategorie = kategorie;
+  public void setKategorieDefiningCode(KategorieDefiningCode kategorieDefiningCode) {
+     this.kategorieDefiningCode = kategorieDefiningCode;
   }
 
-  public DvCodedText getKategorie() {
-     return this.kategorie ;
+  public KategorieDefiningCode getKategorieDefiningCode() {
+     return this.kategorieDefiningCode ;
   }
 
   public void setKategorieNullFlavourDefiningCode(NullFlavour kategorieNullFlavourDefiningCode) {
---
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/geccodiagnosecomposition/definition/NameDesProblemsDerDiagnoseDefiningCode.java
@@ -0,0 +1,193 @@
+package org.ehrbase.fhirbridge.ehr.opt.geccodiagnosecomposition.definition;
+
+import java.lang.String;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.ehrbase.client.classgenerator.EnumValueSet;
+
+public enum NameDesProblemsDerDiagnoseDefiningCode implements EnumValueSet {
+  HISTORY_OF_CORONARY_ARTERY_BYPASS_GRAFTING_SITUATION_ZUSTAND_NACH_REVASKULARISATION("History of coronary artery bypass grafting (situation) Zustand nach Revaskularisation", "", "SNOMED Clinical Terms", "399261000"),
+
+  BONE_TISSUE_STRUCTURE_BODY_STRUCTURE("Bone (tissue) structure (body structure)", "", "SNOMED Clinical Terms", "3138006"),
+
+  DEMENTIA_DISORDER("Dementia (disorder)", "", "SNOMED Clinical Terms", "52448006"),
+
+  CHRONIC_VIRAL_HEPATITIS_DISORDER("Chronic viral hepatitis (disorder)", "", "SNOMED Clinical Terms", "10295004"),
+
+  ENTIRE_SMALL_INTESTINE_BODY_STRUCTURE("Entire small intestine (body structure)", "", "SNOMED Clinical Terms", "181250005"),
+
+  ACUTE_RENAL_FAILURE_SYNDROME_DISORDER("Acute renal failure syndrome (disorder)", "", "SNOMED Clinical Terms", "14669001"),
+
+  EMBOLISM_DISORDER("Embolism (disorder)", "", "SNOMED Clinical Terms", "414086009"),
+
+  MULTIPLE_SCLEROSIS_DISORDER("Multiple sclerosis (disorder)", "", "SNOMED Clinical Terms", "24700007"),
+
+  MALIGNANT_NEOPLASTIC_DISEASE("Malignant neoplastic disease", "", "SNOMED Clinical Terms", "363346000"),
+
+  PULMONARY_HYPERTENSION_DISORDER("Pulmonary hypertension (disorder)", "", "SNOMED Clinical Terms", "70995007"),
+
+  CIRRHOSIS_OF_LIVER_DISORDER("Cirrhosis of liver (disorder)", "", "SNOMED Clinical Terms", "19943007"),
+
+  FIBROSIS_OF_LUNG_DISORDER_LUNGENFIBROSE("Fibrosis of lung (disorder) Lungenfibrose", "", "SNOMED Clinical Terms", "51615001"),
+
+  OBSTRUCTIVE_SLEEP_APNEA_SYNDROME_DISORDER("Obstructive sleep apnea syndrome (disorder)", "", "SNOMED Clinical Terms", "78275009"),
+
+  DIABETES_MELLITUS_TYPE1_DISORDER("Diabetes mellitus type 1 (disorder)", "", "SNOMED Clinical Terms", "46635009"),
+
+  EXTREME_OBESITY_WITH_ALVEOLAR_HYPOVENTILATION_DISORDER("Extreme obesity with alveolar hypoventilation (disorder)", "", "SNOMED Clinical Terms", "190966007"),
+
+  DIABETES_MELLITUS_TYPE2_DISORDER("Diabetes mellitus type 2 (disorder)", "", "SNOMED Clinical Terms", "44054006"),
+
+  ASTHMA_DISORDER("Asthma (disorder)", "", "SNOMED Clinical Terms", "195967001"),
+
+  CHRONIC_KIDNEY_DISEASE_STAGE4_DISORDER("Chronic kidney disease stage 4 (disorder)", "", "SNOMED Clinical Terms", "431857002"),
+
+  EPILEPSY_DISORDER("Epilepsy (disorder)", "", "SNOMED Clinical Terms", "84757009"),
+
+  HISTORY_OF_CEREBROVASCULAR_ACCIDENT_WITHOUT_RESIDUAL_DEFICITS_SITUATION("History of cerebrovascular accident without residual deficits (situation", "", "SNOMED Clinical Terms", "429993008"),
+
+  ENTIRE_LARGE_INTESTINE_BODY_STRUCTURE("Entire large intestine (body structure)", "", "SNOMED Clinical Terms", "181254001"),
+
+  HYPERTENSIVE_DISORDER_SYSTEMIC_ARTERIAL_DISORDER_BLUTHOCHDRUCK("Hypertensive disorder, systemic arterial (disorder) Bluthochdruck", "", "SNOMED Clinical Terms", "38341003"),
+
+  VASCULITIS_DISORDER("Vasculitis (disorder)", "", "SNOMED Clinical Terms", "31996006"),
+
+  ENTIRE_LUNG_BODY_STRUCTURE("Entire lung (body structure)", "", "SNOMED Clinical Terms", "181216001"),
+
+  PSYCHOTIC_DISORDER_DISORDER("Psychotic disorder (disorder)", "", "SNOMED Clinical Terms", "69322001"),
+
+  CHRONIC_KIDNEY_DISEASE_DISORDER("Chronic kidney disease (disorder)", "", "SNOMED Clinical Terms", "709044004"),
+
+  SECONDARY_DIABETES_MELLITUS_DISORDER("Secondary diabetes mellitus (disorder)", "", "SNOMED Clinical Terms", "8801005"),
+
+  MENTAL_DISORDER_DISORDER("Mental disorder (disorder)", "", "SNOMED Clinical Terms", "74732009"),
+
+  HEART_FAILURE_DISORDER_HERZINSUFFIZIENZ("Heart failure (disorder) Herzinsuffizienz", "", "SNOMED Clinical Terms", "84114007"),
+
+  CHRONIC_KIDNEY_DISEASE_STAGE5_ON_DIALYSIS_DISORDER("Chronic kidney disease stage 5 on dialysis (disorder)", "", "SNOMED Clinical Terms", "714152005"),
+
+  CHRONIC_KIDNEY_DISEASE_STAGE1_DISORDER("Chronic kidney disease stage 1 (disorder)", "", "SNOMED Clinical Terms", "431855005"),
+
+  CYSTIC_FIBROSIS_DISORDER_CYSTISCHE_FIBROSE("Cystic fibrosis (disorder) Cystische Fibrose", "", "SNOMED Clinical Terms", "190905008"),
+
+  CHRONIC_KIDNEY_DISEASE_STAGE2_DISORDER("Chronic kidney disease stage 2 (disorder)", "", "SNOMED Clinical Terms", "431856006"),
+
+  PARKINSON_S_DISEASE_DISORDER("Parkinson's disease (disorder)", "", "SNOMED Clinical Terms", "49049000"),
+
+  ENTIRE_KIDNEY_BODY_STRUCTURE("Entire kidney (body structure)", "", "SNOMED Clinical Terms", "181414000"),
+
+  CHRONIC_KIDNEY_DISEASE_STAGE3_DISORDER("Chronic kidney disease stage 3 (disorder)", "", "SNOMED Clinical Terms", "433144002"),
+
+  CORONARY_ARTERIOSCLEROSIS_DISORDER_KORONARE_HERZKRANKHEIT("Coronary arteriosclerosis (disorder) koronare Herzkrankheit", "", "SNOMED Clinical Terms", "53741008"),
+
+  SKIN_PART_BODY_STRUCTURE("Skin part (body structure)", "", "SNOMED Clinical Terms", "119181002"),
+
+  HISTORY_OF_CEREBROVASCULAR_ACCIDENT_WITH_RESIDUAL_DEFICIT_SITUATION("History of cerebrovascular accident with residual deficit (situation)", "", "SNOMED Clinical Terms", "440140008"),
+
+  CAROTID_ARTERY_STENOSIS_DISORDER_CAROTISSTENOSE("Carotid artery stenosis (disorder) Carotisstenose", "", "SNOMED Clinical Terms", "64586002"),
+
+  MIGRAINE_DISORDER("Migraine (disorder)", "", "SNOMED Clinical Terms", "37796009"),
+
+  INFLAMMATORY_BOWEL_DISEASE_DISORDER("Inflammatory bowel disease (disorder)", "", "SNOMED Clinical Terms", "24526004"),
+
+  CONGENITAL_IMMUNODEFICIENCY_DISEASE_DISORDER("Congenital immunodeficiency disease (disorder)", "", "SNOMED Clinical Terms", "36138009"),
+
+  CHRONIC_KIDNEY_DISEASE_STAGE5_DISORDER("Chronic kidney disease stage 5 (disorder)", "", "SNOMED Clinical Terms", "433146000"),
+
+  CHRONIC_NERVOUS_SYSTEM_DISORDER_DISORDER("Chronic nervous system disorder (disorder)", "", "SNOMED Clinical Terms", "128283000"),
+
+  HISTORY_OF_MYOCARDIAL_INFARCTION_SITUATION_ZUSTAND_NACH_HERZINFARKT("History of myocardial infarction (situation) Zustand nach Herzinfarkt", "", "SNOMED Clinical Terms", "399211009"),
+
+  COMBINED_DISORDER_OF_MUSCLE_AND_PERIPHERAL_NERVE_DISORDER("Combined disorder of muscle AND peripheral nerve (disorder)", "", "SNOMED Clinical Terms", "257277002"),
+
+  CARDIAC_ARRHYTHMIA_DISORDER_HERZRHYTHMUSSTOERUNGEN("Cardiac arrhythmia (disorder) Herzrhythmusstörungen", "", "SNOMED Clinical Terms", "698247007"),
+
+  STEATOSIS_OF_LIVER_DISORDER("Steatosis of liver (disorder)", "", "SNOMED Clinical Terms", "197321007"),
+
+  BLOOD_VESSEL_PART_BODY_STRUCTURE("Blood vessel part (body structure)", "", "SNOMED Clinical Terms", "119206002"),
+
+  GASTROINTESTINAL_ULCER("Gastrointestinal ulcer", "", "SNOMED Clinical Terms", "40845000"),
+
+  HUMAN_IMMUNODEFICIENCY_VIRUS_INFECTION("Human immunodeficiency virus infection", "", "SNOMED Clinical Terms", "86406008"),
+
+  CHRONIC_OBSTRUCTIVE_LUNG_DISEASE_DISORDER("Chronic obstructive lung disease (disorder)", "", "SNOMED Clinical Terms", "13645005"),
+
+  ENTIRE_HEART_BODY_STRUCTURE("Entire heart (body structure)", "", "SNOMED Clinical Terms", "302509004"),
+
+  CEREBRAL_MENINGES_STRUCTURE_BODY_STRUCTURE("Cerebral meninges structure (body structure)", "", "SNOMED Clinical Terms", "8935007"),
+
+  DEPRESSIVE_DISORDER_DISORDER("Depressive disorder (disorder)", "", "SNOMED Clinical Terms", "35489007"),
+
+  AUTOIMMUNE_LIVER_DISEASE_DISORDER("Autoimmune liver disease (disorder)", "", "SNOMED Clinical Terms", "235890007"),
+
+  ENTIRE_LIVER_BODY_STRUCTURE("Entire liver (body structure)", "", "SNOMED Clinical Terms", "181268008"),
+
+  ENTIRE_PANCREAS_BODY_STRUCTURE("Entire pancreas (body structure)", "", "SNOMED Clinical Terms", "181277001"),
+
+  TENDON_STRUCTURE_BODY_STRUCTURE("Tendon structure (body structure)", "", "SNOMED Clinical Terms", "13024002"),
+
+  ANXIETY_DISORDER_DISORDER("Anxiety disorder (disorder)", "", "SNOMED Clinical Terms", "197480006"),
+
+  PERIPHERAL_ARTERIAL_OCCLUSIVE_DISEASE_DISORDER_PAVK("Peripheral arterial occlusive disease (disorder) pAVK", "", "SNOMED Clinical Terms", "399957001"),
+
+  SLEEP_APNEA_DISORDER("Sleep apnea (disorder)", "", "SNOMED Clinical Terms", "73430006"),
+
+  DISORDER_OF_CONNECTIVE_TISSUE_DISORDER("Disorder of connective tissue (disorder", "", "SNOMED Clinical Terms", "105969002"),
+
+  THROMBOSIS_DISORDER("Thrombosis (disorder)", "", "SNOMED Clinical Terms", "439127006"),
+
+  INTESTINAL_STRUCTURE_BODY_STRUCTURE("Intestinal structure (body structure)", "", "SNOMED Clinical Terms", "113276009"),
+
+  ENTIRE_HEART_VALVE_BODY_STRUCTURE("Entire heart valve (body structure)", "", "SNOMED Clinical Terms", "181285005"),
+
+  INSULIN_TREATED_TYPE2_DIABETES_MELLITUS_DISORDER("Insulin treated type 2 diabetes mellitus (disorder)", "", "SNOMED Clinical Terms", "237599002"),
+
+  ENTIRE_CORNEA_BODY_STRUCTURE("Entire cornea (body structure)", "", "SNOMED Clinical Terms", "181162001"),
+
+  RHEUMATOID_ARTHRITIS_DISORDER("Rheumatoid arthritis (disorder)", "", "SNOMED Clinical Terms", "69896004"),
+
+  CARTILAGE_TISSUE_BODY_STRUCTURE("Cartilage tissue (body structure)", "", "SNOMED Clinical Terms", "309312004"),
+
+  EAR_OSSICLE_STRUCTURE_BODY_STRUCTURE("Ear ossicle structure (body structure)", "", "SNOMED Clinical Terms", "41845008");
+
+  private String value;
+
+  private String description;
+
+  private String terminologyId;
+
+  private String code;
+
+  NameDesProblemsDerDiagnoseDefiningCode(String value, String description, String terminologyId,
+      String code) {
+    this.value = value;
+    this.description = description;
+    this.terminologyId = terminologyId;
+    this.code = code;
+  }
+
+  public static Map<String, NameDesProblemsDerDiagnoseDefiningCode> getCodesAsMap(){
+    Map<String, NameDesProblemsDerDiagnoseDefiningCode> nameDesProblemsDerDiagnoseDefiningCodeHashMap = new HashMap<>();
+    for (NameDesProblemsDerDiagnoseDefiningCode nameDesProblemsDerDiagnoseDefiningCode : NameDesProblemsDerDiagnoseDefiningCode.values()) {
+      nameDesProblemsDerDiagnoseDefiningCodeHashMap.put(nameDesProblemsDerDiagnoseDefiningCode.getCode(), nameDesProblemsDerDiagnoseDefiningCode);
+    }
+    return nameDesProblemsDerDiagnoseDefiningCodeHashMap;
+  }
+
+  public String getValue() {
+     return this.value ;
+  }
+
+  public String getDescription() {
+     return this.description ;
+  }
+
+  public String getTerminologyId() {
+     return this.terminologyId ;
+  }
+
+  public String getCode() {
+     return this.code ;
+  }
+}
