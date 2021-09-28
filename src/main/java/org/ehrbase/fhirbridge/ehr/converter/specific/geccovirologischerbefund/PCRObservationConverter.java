0c3e2ad68f963560e7c871d3139d33d8702e613d
SevKohler <kohlerseverin@posteo.de>
2021-09-28 15:17:50 +0200

    updated PCR

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/geccovirologischerbefund/PCRObservationConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/geccovirologischerbefund/PCRObservationConverter.java
@@ -1,11 +1,11 @@
 package org.ehrbase.fhirbridge.ehr.converter.specific.geccovirologischerbefund;
 
-import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
+import org.ehrbase.client.classgenerator.shareddefinition.NullFlavour;
 import org.ehrbase.fhirbridge.ehr.converter.generic.ObservationToObservationConverter;
+import org.ehrbase.fhirbridge.ehr.converter.parser.DvCodedTextParser;
 import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.BefundObservation;
 import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.LabortestBezeichnungDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.LabortestPanelCluster;
-import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.NachweisDefiningCode;
 import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.ProAnalytCluster;
 import org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition.VirusnachweistestDefiningCode;
 import org.hl7.fhir.r4.model.Observation;
@@ -25,18 +25,10 @@ public class PCRObservationConverter extends ObservationToObservationConverter<B
         LabortestPanelCluster labortestPanel = new LabortestPanelCluster();
         ProAnalytCluster analyt = new ProAnalytCluster();
         analyt.setVirusnachweistestDefiningCode(VirusnachweistestDefiningCode.SARS_COV2_COVID19_RNA_PRESENCE_IN_RESPIRATORY_SPECIMEN_BY_NAA_WITH_PROBE_DETECTION);
-        switch (observation.getValueCodeableConcept().getCoding().get(0).getCode()) {
-            case "260373001":
-                analyt.setNachweisDefiningCode(NachweisDefiningCode.DETECTED_QUALIFIER_VALUE);
-                break;
-            case "419984006":
-                analyt.setNachweisDefiningCode(NachweisDefiningCode.INCONCLUSIVE_QUALIFIER_VALUE);
-                break;
-            case "260415000":
-                analyt.setNachweisDefiningCode(NachweisDefiningCode.NOT_DETECTED_QUALIFIER_VALUE);
-                break;
-            default:
-                throw new ConversionException("Value code " + observation.getValueCodeableConcept().getCoding().get(0).getCode() + " is not supported");
+        if(observation.hasValueCodeableConcept()){
+            DvCodedTextParser.parseFHIRCoding(observation.getValueCodeableConcept().getCoding().get(0)).ifPresent(analyt::setNachweis);
+        }else{
+            analyt.setNachweisNullFlavourDefiningCode(NullFlavour.UNKNOWN);
         }
         analyt.setErgebnisStatusValue(observation.getStatus().toCode());
         labortestPanel.setProAnalyt(analyt);
---
+++ src/main/java/org/ehrbase/fhirbridge/ehr/opt/geccovirologischerbefundcomposition/definition/ProAnalytTestmethodeDvBoolean.java
@@ -0,0 +1,32 @@
+package org.ehrbase.fhirbridge.ehr.opt.geccovirologischerbefundcomposition.definition;
+
+import java.lang.Boolean;
+import javax.annotation.processing.Generated;
+import org.ehrbase.client.annotations.Entity;
+import org.ehrbase.client.annotations.OptionFor;
+import org.ehrbase.client.annotations.Path;
+import org.ehrbase.client.classgenerator.interfaces.RMEntity;
+
+@Entity
+@Generated(
+    value = "org.ehrbase.client.classgenerator.ClassGenerator",
+    date = "2021-09-28T15:04:42.665788+02:00",
+    comments = "https://github.com/ehrbase/openEHR_SDK Version: 1.5.0"
+)
+@OptionFor("DV_BOOLEAN")
+public class ProAnalytTestmethodeDvBoolean implements RMEntity, ProAnalytTestmethodeChoice {
+  /**
+   * Path: GECCO_Virologischer Befund/Befund/Jedes Ereignis/Labortest-Panel/Pro Analyt/Testmethode/Testmethode
+   * Description: Die Beschreibung der Methode, mit der der Test nur für diesen Analyten durchgeführt wurde.
+   */
+  @Path("|value")
+  private Boolean testmethodeValue;
+
+  public void setTestmethodeValue(Boolean testmethodeValue) {
+     this.testmethodeValue = testmethodeValue;
+  }
+
+  public Boolean isTestmethodeValue() {
+     return this.testmethodeValue ;
+  }
+}
