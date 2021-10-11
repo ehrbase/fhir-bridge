0be03749e2604d2c2e5aa0dca3c09a20c96ac8c9
SevKohler <kohlerseverin@posteo.de>
2021-10-11 18:56:20 +0200

    continue

--- src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/antibodypanel/BefundJedesEreignisPointEventConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/ehr/converter/specific/antibodypanel/BefundJedesEreignisPointEventConverter.java
@@ -4,15 +4,10 @@ import com.nedap.archie.rm.datavalues.DvCodedText;
 import org.ehrbase.fhirbridge.ehr.converter.ConversionException;
 import org.ehrbase.fhirbridge.ehr.converter.generic.ObservationToPointEventConverter;
 import org.ehrbase.fhirbridge.ehr.converter.parser.DvCodedTextParser;
-import org.ehrbase.fhirbridge.ehr.converter.specific.CodeSystem;
-import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.BefundJedesEreignisPointEvent;
-import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.LabortestBezeichnungDefiningCode;
-import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.LabortestPanelCluster;
-import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.NachweisDefiningCode;
-import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.ProAnalytCluster;
-import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.VirusnachweistestDefiningCode;
+import org.ehrbase.fhirbridge.ehr.opt.geccoserologischerbefundcomposition.definition.*;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Observation;
+import org.openjdk.jmh.util.Optional;
 
 import java.util.List;
 
@@ -36,35 +31,26 @@ public class BefundJedesEreignisPointEventConverter extends ObservationToPointEv
         ProAnalytCluster proAnalytCluster = new ProAnalytCluster();
         DvCodedTextParser.parseFHIRCoding(immunoassay.getObservation().getCode().getCoding().get(0)).ifPresent(proAnalytCluster::setVirusnachweistest);
         if(immunoassay.getObservation().hasValueQuantity()){
-            proAnalytCluster.setQuantitativesErgebnisMagnitude(immunoassay.getObservation().getValueQuantity().getValue().doubleValue());
-            proAnalytCluster.setQuantitativesErgebnisUnits(immunoassay.getObservation().getValueQuantity().getCode());
+            ProAnalytQuantitativesErgebnisDvQuantity proAnalytQuantitativesErgebnisDvQuantity = new ProAnalytQuantitativesErgebnisDvQuantity();
+            proAnalytQuantitativesErgebnisDvQuantity.setQuantitativesErgebnisMagnitude(immunoassay.getObservation().getValueQuantity().getValue().doubleValue());
+            proAnalytQuantitativesErgebnisDvQuantity.setQuantitativesErgebnisUnits(immunoassay.getObservation().getValueQuantity().getCode());
+            proAnalytCluster.setQuantitativesErgebnis(proAnalytQuantitativesErgebnisDvQuantity);
         }else{
-            proAnalytCluster.setNachweisDefiningCode(convertNachweisDefiningCode());
+            convertNachweisDefiningCode().ifPresent
+            proAnalytCluster.setNachweis();
         }
         proAnalytCluster.setErgebnisStatusValue(immunoassay.getObservation().getStatusElement().getCode());
         labortestPanelCluster.setProAnalyt(proAnalytCluster);
         return labortestPanelCluster;
     }
 
-    private NachweisDefiningCode convertNachweisDefiningCode() {
+    private Optional<DvCodedText> convertNachweisDefiningCode() {
         if(immunoassay.getObservation().hasValueCodeableConcept() && immunoassay.getObservation().getValueCodeableConcept().hasCoding() && immunoassay.getObservation().getValueCodeableConcept().getCoding().get(0).hasCode()){
             List<Coding> codingList = immunoassay.getObservation().getValueCodeableConcept().getCoding();
             Coding coding = codingList.get(0);
-            return resolveNachweisDefiningCode(coding);
+            return 
         }else{
             throw new ConversionException("ValueCodeableConcept.coding or code is missing");
-        }
-    }
-
-    private NachweisDefiningCode resolveNachweisDefiningCode(Coding coding) {
-        if (coding.getCode().equals(NachweisDefiningCode.DETECTED_QUALIFIER_VALUE.getCode()) && coding.getSystem().equals(CodeSystem.SNOMED.getUrl())){
-            return NachweisDefiningCode.DETECTED_QUALIFIER_VALUE;
-        } else if (coding.getCode().equals(NachweisDefiningCode.INCONCLUSIVE_QUALIFIER_VALUE.getCode()) && coding.getSystem().equals(CodeSystem.SNOMED.getUrl())) {
-            return NachweisDefiningCode.INCONCLUSIVE_QUALIFIER_VALUE;
-        } else if (coding.getCode().equals(NachweisDefiningCode.NOT_DETECTED_QUALIFIER_VALUE.getCode()) && coding.getSystem().equals(CodeSystem.SNOMED.getUrl())) {
-            return NachweisDefiningCode.NOT_DETECTED_QUALIFIER_VALUE;
-        } else {
-            throw new ConversionException("The code in valueCodeableConcept.coding.code is not supported");
         }
     }
 
