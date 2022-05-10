58ffd0a4914261fa077eae1813569cfbd206dba2
SevKohler <kohlerseverin@posteo.de>
2022-05-10 14:40:47 +0200

    changed from of route

--- src/main/java/org/ehrbase/fhirbridge/camel/route/MHDRouteBuilder.java
+++ src/main/java/org/ehrbase/fhirbridge/camel/route/MHDRouteBuilder.java
@@ -3,12 +3,14 @@ package org.ehrbase.fhirbridge.camel.route;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.ehrbase.fhirbridge.camel.processor.MHDDocumentProcessor;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MHDRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         // @formatter:off
-        from("mhd-iti68://service?audit=true&secure=true")
+        from("mhd-iti68://service?audit=true")
                 .process(MHDDocumentProcessor.BEAN_ID);
     }
     // @formatter:on
