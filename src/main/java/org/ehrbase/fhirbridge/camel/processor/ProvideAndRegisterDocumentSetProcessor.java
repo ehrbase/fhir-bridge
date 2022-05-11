db77f4df7b38acae8fa5f729ac6107142c758b44
SevKohler <kohlerseverin@posteo.de>
2022-05-11 17:43:37 +0200

    trying to generate ITI 41

--- src/main/java/org/ehrbase/fhirbridge/camel/processor/ProvideAndRegisterDocumentSetProcessor.java
+++ src/main/java/org/ehrbase/fhirbridge/camel/processor/ProvideAndRegisterDocumentSetProcessor.java
@@ -2,12 +2,22 @@ package org.ehrbase.fhirbridge.camel.processor;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.openehealth.ipf.commons.ihe.xds.core.requests.ProvideAndRegisterDocumentSet;
+import org.openehealth.ipf.commons.ihe.xds.core.metadata.Document;
+import org.openehealth.ipf.commons.ihe.xds.core.metadata.DocumentEntry;
+import org.openehealth.ipf.commons.ihe.xds.core.metadata.SubmissionSet;
+import org.openehealth.ipf.commons.ihe.xds.core.requests.builder.ProvideAndRegisterDocumentSetBuilder;
 
 public class ProvideAndRegisterDocumentSetProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
-        ProvideAndRegisterDocumentSet provideAndRegisterDocumentSet = new ProvideAndRegisterDocumentSet();
+        SubmissionSet submissionSet = new SubmissionSet();
+        ProvideAndRegisterDocumentSetBuilder provideAndRegisterDocumentSetBuilder = new ProvideAndRegisterDocumentSetBuilder(true, submissionSet);
+        Document document = new Document();
+        DocumentEntry documentEntry = new DocumentEntry();
+        document.setDocumentEntry(documentEntry);
+        provideAndRegisterDocumentSetBuilder.withDocument(document);
+        provideAndRegisterDocumentSetBuilder.build();
+       // provideAndRegisterDocumentSet.setSubmissionSet();
 
     }
 }
