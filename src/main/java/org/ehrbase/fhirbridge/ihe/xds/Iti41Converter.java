4fd939d059c318e9eedb2cabb59d8c6c2e5d8510
SevKohler <kohlerseverin@posteo.de>
2022-05-17 16:13:35 +0200

    configured builder

--- src/main/java/org/ehrbase/fhirbridge/ihe/xds/Iti41Converter.java
+++ src/main/java/org/ehrbase/fhirbridge/ihe/xds/Iti41Converter.java
@@ -16,21 +16,57 @@
 
 package org.ehrbase.fhirbridge.ihe.xds;
 
+import org.ehrbase.client.classgenerator.interfaces.CompositionEntity;
+import org.openehealth.ipf.commons.ihe.xds.core.metadata.*;
 import org.openehealth.ipf.commons.ihe.xds.core.requests.ProvideAndRegisterDocumentSet;
+import org.openehealth.ipf.commons.ihe.xds.core.requests.builder.ProvideAndRegisterDocumentSetBuilder;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
+
+import javax.activation.DataHandler;
+import java.util.List;
 
 /**
  * @author Renaud Subiger
  * @since 1.6
  */
 // TODO: Check with @SevKohler, if the Exchange is required here. If yes, use TypeConverter from Camel
-public class Iti41Converter implements Converter<Object, ProvideAndRegisterDocumentSet> {
+public class Iti41Converter implements Converter<CompositionEntity, ProvideAndRegisterDocumentSet> {
 
     @Override
-    public ProvideAndRegisterDocumentSet convert(@NonNull Object source) {
-        var result = new ProvideAndRegisterDocumentSet();
+    public ProvideAndRegisterDocumentSet convert(@NonNull CompositionEntity composition) {
+        SubmissionSet submissionSet = getSumissionSet();
+        List<Folder> folders = getFolders();
+        List<Document > documents = getDocuments(composition);
+        List< Association > associations = getAssociations();
+        ProvideAndRegisterDocumentSetBuilder provideAndRegisterDocumentSetBuilder = new ProvideAndRegisterDocumentSetBuilder(true, new SubmissionSet());
         // TODO: Implement conversion logic
-        return result;
+        return provideAndRegisterDocumentSetBuilder.doBuild(submissionSet, folders, documents, associations);
+
     }
+    private SubmissionSet getSumissionSet() {
+        SubmissionSet submissionSet = new SubmissionSet();
+        return submissionSet;
+    }
+
+    private List<Association> getAssociations() {
+        Association association = new Association();
+        return List.of(association);
+    }
+
+    private List<Document> getDocuments(CompositionEntity source) {
+        Document document = new Document();
+        DataHandler dataHandler = new DataHandler(source, "application/json");
+        DocumentEntry documentEntry = new DocumentEntry();
+        document.setDataHandler(dataHandler);
+      //  document.setDocumentEntry();
+        return List.of(document);
+    }
+
+    private List<Folder> getFolders() {
+        Folder folder = new Folder();
+        return List.of(folder);
+    }
+
+
 }
